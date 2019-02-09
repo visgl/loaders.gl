97882a00f557ade7859e074f46af3ac7d6696027
Ib Green <ib@uber.com>
2019-02-08 16:02:46 -0800

    Remove debugger

--- modules/kml/test/kml-as-geojson-loader.spec.js
+++ modules/kml/test/kml-as-geojson-loader.spec.js
@@ -28,19 +28,9 @@ test('KMLasGeoJsonLoader#parseText', t => {
   if (!KMLasGeoJsonLoader.supported) {
     t.comment('XML parsing not available');
   } else {
-    debugger;
     const data = parseFileSync(KML, KMLasGeoJsonLoader, {log: null});
-    t.equal(data.documents.length, 2, 'Documents were found');
-    t.equal(data.markers.length, 4, 'Markers were found');
-    t.equal(data.lines.length, 6, 'Lines were found');
-    t.equal(data.polygons.length, 9, 'Polygons were found');
-    t.equal(data.overlays.length, 1, 'Overlay was found');
-
-    // for (const key in data) {
-    //   for (const object of data[key]) {
-    //     t.comment(`${key}: ${JSON.stringify(object)}`);
-    //   }
-    // }
+    t.equal(data.type, 'FeatureCollection', 'FeatureCollection found');
+    t.equal(data.features.length, 2, 'Features were found');
   }
 
   t.end();
