36c7311272e04d3e755971f039adee6b34f0228c
Ib Green <ib@uber.com>
2019-02-13 18:29:26 -0800

    Restore tests

--- test/modules.js
+++ test/modules.js
@@ -7,19 +7,19 @@ import '@loaders.gl/core/../test';
 
 import '@loaders.gl/arrow/../test';
 
-// import '@loaders.gl/images/../test';
+import '@loaders.gl/images/../test';
 
-// import '@loaders.gl/draco/../test';
+import '@loaders.gl/draco/../test';
 
-// import '@loaders.gl/obj/../test';
-// import '@loaders.gl/pcd/../test';
+import '@loaders.gl/obj/../test';
+import '@loaders.gl/pcd/../test';
 import '@loaders.gl/ply/../test';
-// import '@loaders.gl/las/../test';
+import '@loaders.gl/las/../test';
 
-// import '@loaders.gl/gltf/../test';
+import '@loaders.gl/gltf/../test';
 
-// import '@loaders.gl/kml/../test';
+import '@loaders.gl/kml/../test';
 
-// import '@loaders.gl/zip/../test';
+import '@loaders.gl/zip/../test';
 
-// import '@loaders.gl/experimental/../test';
+import '@loaders.gl/experimental/../test';
