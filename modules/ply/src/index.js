25096df626d3e5f6b0e115d9c6ca7715a740975d
Ib Green <ibgreen@users.noreply.github.com>
2019-03-03 19:39:02 -0800

    PLY streaming (#105)

--- modules/ply/src/index.js
+++ modules/ply/src/index.js
@@ -1,3 +1,6 @@
 // Import the worker bundled by webpack
 export {default as PLYLoader} from './ply-loader';
 export {default as PLYWorkerLoader} from './ply-worker-loader';
+
+// TODO - PLYStreamLoader should eventually merge with/replace PLYLoader
+export {default as _PLYStreamLoader} from './ply-stream-loader';
