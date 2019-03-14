7bab9f4d219c5b3098b753cccae5e89115f309bf
Ib Green <ib@uber.com>
2019-03-14 15:32:13 -0700

    Fix tests

--- modules/core/src/fetch-file/fetch-file.js
+++ modules/core/src/fetch-file/fetch-file.js
@@ -9,7 +9,7 @@ import {
 } from './fetch-file-browser';
 
 // read-file-node is excluded from build under browser so don't do indivdual imports
-import * as node from './read-file-node';
+import * as node from './fetch-file-node';
 
 export async function fetchFile(url, options) {
   const func = isBrowser ? browserFetchFile : node.fetchFile;
