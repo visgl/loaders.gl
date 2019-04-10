54c485282eaff5f758a4b1bc872a7eb566d86ae8
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-04-10 10:00:47 -0700

    Port three.js obj loader (#150)

--- modules/obj/test/obj-loader.spec.js
+++ modules/obj/test/obj-loader.spec.js
@@ -7,25 +7,39 @@ import {OBJLoader, OBJWorkerLoader} from '@loaders.gl/obj';
 import {validateLoadedData} from 'test/common/conformance';
 
 const OBJ_ASCII_URL = '@loaders.gl/obj/test/data/bunny.obj';
+const OBJ_NORMALS_URL = '@loaders.gl/obj/test/data/cube.obj';
+const OBJ_MULTI_PART_URL = '@loaders.gl/obj/test/data/magnolia.obj';
 
 test('OBJLoader#parseText', async t => {
   const data = await load(OBJ_ASCII_URL, OBJLoader);
   validateLoadedData(t, data);
 
   t.equal(data.mode, 4, 'mode is TRIANGLES (4)');
-  t.equal(data.indices.value.length, 14904, 'INDICES attribute was found');
 
-  t.equal(data.attributes.POSITION.value.length, 7509, 'POSITION attribute was found');
+  t.equal(data.attributes.POSITION.value.length, 14904 * 3, 'POSITION attribute was found');
   t.equal(data.attributes.POSITION.size, 3, 'POSITION attribute was found');
 
-  // TODO - need OBJ test model with normals and uvs
-  // const NORMAL = data.glTFAttributeMap.NORMAL;
-  // const TEXCOORD_0 = data.glTFAttributeMap.TEXCOORD_0;
-  // t.equal(data.attributes[NORMAL].value.length, 7509, 'NORMAL attribute was found');
-  // t.equal(data.attributes[NORMAL].size, 3, 'NORMAL attribute was found');
-  // t.equal(data.attributes[TEXCOORD_0].value.length, 7509, 'TEXCOORD_0 attribute was found');
-  // t.equal(data.attributes[TEXCOORD_0].size, 3, 'TEXCOORD_0 attribute was found');
+  t.end();
+});
 
+test('OBJLoader#parseText - object with normals', async t => {
+  const data = await load(OBJ_NORMALS_URL, OBJLoader);
+  validateLoadedData(t, data);
+
+  t.equal(data.attributes.POSITION.value.length, 108, 'POSITION attribute was found');
+  t.equal(data.attributes.POSITION.size, 3, 'POSITION attribute was found');
+  t.equal(data.attributes.NORMAL.value.length, 108, 'NORMAL attribute was found');
+  t.equal(data.attributes.NORMAL.size, 3, 'NORMAL attribute was found');
+  t.equal(data.attributes.TEXCOORD_0.value.length, 72, 'TEXCOORD_0 attribute was found');
+  t.equal(data.attributes.TEXCOORD_0.size, 2, 'TEXCOORD_0 attribute was found');
+  t.end();
+});
+
+test('OBJLoader#parseText - multi-part object', async t => {
+  const data = await load(OBJ_MULTI_PART_URL, OBJLoader);
+  validateLoadedData(t, data);
+
+  t.equal(data.header.vertexCount, 1372 * 3, 'Vertices are loaded');
   t.end();
 });
 
@@ -41,9 +55,8 @@ test('OBJWorkerLoader#parse(text)', async t => {
   validateLoadedData(t, data);
 
   t.equal(data.mode, 4, 'mode is TRIANGLES (4)');
-  t.equal(data.indices.value.length, 14904, 'INDICES attribute was found');
 
-  t.equal(data.attributes.POSITION.value.length, 7509, 'POSITION attribute was found');
+  t.equal(data.attributes.POSITION.value.length, 14904 * 3, 'POSITION attribute was found');
   t.equal(data.attributes.POSITION.size, 3, 'POSITION attribute was found');
   t.end();
 });
