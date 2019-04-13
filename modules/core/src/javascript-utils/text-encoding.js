3cab443e5bc1ae9d4ae39ff277df438918a808dc
Ib Green <ib@uber.com>
2019-04-13 07:39:30 -0700

    Move the no longer maintained text-encoder module into core

--- modules/core/src/javascript-utils/text-encoding.js
+++ modules/core/src/javascript-utils/text-encoding.js
@@ -1,7 +1,8 @@
-/* global TextEncoder,TextDecoder */
+// Node 11 introduces these classes, for lower versions we use these polyfills
 
+/* global TextEncoder,TextDecoder */
 if (typeof TextDecoder === 'undefined') {
-  module.exports = require('text-encoding');
+  module.exports = require('../node/text-encoding/encoding');
 } else {
   module.exports = {TextEncoder, TextDecoder};
 }
