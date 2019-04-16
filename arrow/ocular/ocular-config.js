305de49dad3d727ba5d61ff832de680842e382c0
Ib Green <ib@uber.com>
2019-04-16 16:19:20 -0700

    Switch to publishing gh-pages from master/docs

--- arrow/ocular/ocular-config.js
+++ arrow/ocular/ocular-config.js
@@ -2,8 +2,8 @@ const DOCS = require('../docs/table-of-contents.json');
 
 module.exports = {
   DIR_NAME: `${__dirname}`,
-  DOC_FOLDER: `${__dirname}/../arrow-docs/`,
-  ROOT_FOLDER: `${__dirname}/../`,
+  DOC_FOLDER: `${__dirname}/../docs/`,
+  ROOT_FOLDER: `${__dirname}/../../`,
 
   EXAMPLES: [],
   DOCS,
@@ -15,7 +15,6 @@ module.exports = {
   PROJECT_ORG: 'uber-web',
   PROJECT_URL: 'https://github.com/uber-web/loaders.gl',
   PROJECT_DESC: 'Apache Arrow JavaScript Bindings',
-  WEBSITE_PATH: '/arrow-website/',
   PATH_PREFIX: '/loaders.gl/arrowjs',
 
   FOOTER_LOGO: '',
@@ -28,8 +27,7 @@ module.exports = {
 
   HOME_RIGHT: null,
 
-  HOME_BULLETS: [
-  ],
+  HOME_BULLETS: [],
 
   ADDITIONAL_LINKS: [],
 
@@ -38,20 +36,4 @@ module.exports = {
   // For showing star counts and contributors.
   // Should be like btoa('YourUsername:YourKey') and should be readonly.
   GITHUB_KEY: null,
-
-  // TODO/ib - from gatsby starter, clean up
-  // Site title.
-  siteTitle: 'loaders.gl',
-  // Alternative site title for SEO.
-  siteTitleAlt: 'loaders.gl',
-  // Logo used for SEO and manifest.
-  siteLogo: '/logos/logo-1024.png',
-  // Domain of your website without pathPrefix.
-  siteUrl: 'https://ocular',
-  // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  pathPrefix: '/luma',
-  // Website description used for RSS feeds/meta description tag.
-  siteDescription: 'WebGL2 Components',
-  // Path to the RSS file.
-  siteRss: '/rss.xml'
 };
