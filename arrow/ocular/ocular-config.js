9cf77dd34eac4a6233ed225fa67e44a16a3c540e
Ib Green <ibgreen@users.noreply.github.com>
2019-04-16 16:20:27 -0700

    Switch to publishing gh-pages from master/docs (#172)

---
+++ arrow/ocular/ocular-config.js
@@ -0,0 +1,39 @@
+const DOCS = require('../docs/table-of-contents.json');
+
+module.exports = {
+  DIR_NAME: `${__dirname}`,
+  DOC_FOLDER: `${__dirname}/../docs/`,
+  ROOT_FOLDER: `${__dirname}/../../`,
+
+  EXAMPLES: [],
+  DOCS,
+
+  // TODO/ib - from ocular, deduplicate with above settings
+  PROJECT_TYPE: 'github',
+
+  PROJECT_NAME: 'arrow/js',
+  PROJECT_ORG: 'uber-web',
+  PROJECT_URL: 'https://github.com/uber-web/loaders.gl',
+  PROJECT_DESC: 'Apache Arrow JavaScript Bindings',
+  PATH_PREFIX: '/loaders.gl/arrowjs',
+
+  FOOTER_LOGO: '',
+
+  PROJECTS: [],
+
+  HOME_PATH: '/',
+
+  HOME_HEADING: 'Framework agnostic loaders for visualization assets',
+
+  HOME_RIGHT: null,
+
+  HOME_BULLETS: [],
+
+  ADDITIONAL_LINKS: [],
+
+  GA_TRACKING: null,
+
+  // For showing star counts and contributors.
+  // Should be like btoa('YourUsername:YourKey') and should be readonly.
+  GITHUB_KEY: null,
+};
