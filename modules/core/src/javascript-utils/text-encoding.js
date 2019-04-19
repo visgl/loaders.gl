5aaf6013380b8389c507027e46677f5b49f7a76d
Ib Green <ib@uber.com>
2019-04-19 13:58:21 -0700

    Remove polyfill dependency from core

--- modules/core/src/javascript-utils/text-encoding.js
+++ modules/core/src/javascript-utils/text-encoding.js
@@ -1,8 +1,7 @@
-// Node 11 introduces these classes, for lower versions we use these polyfills
-
 /* global TextEncoder,TextDecoder */
 if (typeof TextDecoder === 'undefined') {
-  require('@loaders.gl/polyfills');
+  module.exports = {};
+} else {
+  module.exports = {TextEncoder, TextDecoder};
 }
 
-module.exports = {TextEncoder, TextDecoder};
