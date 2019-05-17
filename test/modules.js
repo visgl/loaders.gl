c4c308ccfe1aac29f78149a1b02a26a99ca3522d
Ib Green <ib@uber.com>
2019-05-17 09:23:26 -0700

    restore tests

--- test/modules.js
+++ test/modules.js
@@ -5,32 +5,32 @@ require('./aliases');
 require('@loaders.gl/polyfills');
 
 // Core
-// require('@loaders.gl/polyfills/test');
-// require('@loaders.gl/core/test');
-// require('@loaders.gl/loader-utils/test');
-// require('@loaders.gl/images/test');
+require('@loaders.gl/polyfills/test');
+require('@loaders.gl/core/test');
+require('@loaders.gl/loader-utils/test');
+require('@loaders.gl/images/test');
 
 // Pointcloud/Mesh Formats
-// require('@loaders.gl/draco/test');
-// require('@loaders.gl/las/test');
-// require('@loaders.gl/obj/test');
-// require('@loaders.gl/pcd/test');
-// require('@loaders.gl/ply/test');
+require('@loaders.gl/draco/test');
+require('@loaders.gl/las/test');
+require('@loaders.gl/obj/test');
+require('@loaders.gl/pcd/test');
+require('@loaders.gl/ply/test');
 
 // Scenegraph Formats
-// require('@loaders.gl/gltf/test');
+require('@loaders.gl/gltf/test');
 
 // 3D Tile Formats
 require('@loaders.gl/3d-tiles/test');
 // require('@loaders.gl/math/test');
 
 // Geospatial Formats
-// require('@loaders.gl/kml/test');
+require('@loaders.gl/kml/test');
 
 // Table Formats
-// require('@loaders.gl/arrow/test');
-// require('@loaders.gl/csv/test');
-// require('@loaders.gl/experimental/test');
+require('@loaders.gl/arrow/test');
+require('@loaders.gl/csv/test');
+require('@loaders.gl/experimental/test');
 
 // Archive Formats
-// require('@loaders.gl/zip/test');
+require('@loaders.gl/zip/test');
