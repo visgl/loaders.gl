91dbe3b98bc84819dd932783d2dad88411517507
Ib Green <ib@uber.com>
2019-05-31 17:12:40 -0700

    Make Tile3DLoading async

--- modules/3d-tiles/src/parsers/parse-3d-tile.js
+++ modules/3d-tiles/src/parsers/parse-3d-tile.js
@@ -1,37 +1,31 @@
 import {TILE3D_TYPE} from '../constants';
 import {getMagicString} from './helpers/parse-utils';
 
-import parsePointCloud3DTileSync from './parse-3d-tile-point-cloud';
-import parseBatchedModel3DTileSync from './parse-3d-tile-batched-model';
-import parseInstancedModel3DTileSync from './parse-3d-tile-instanced-model';
-import parseComposite3DTileSync from './parse-3d-tile-composite';
+import parsePointCloud3DTile from './parse-3d-tile-point-cloud';
+import parseBatchedModel3DTile from './parse-3d-tile-batched-model';
+import parseInstancedModel3DTile from './parse-3d-tile-instanced-model';
+import parseComposite3DTile from './parse-3d-tile-composite';
 
 // Extracts
-export default function parse3DTileSync(arrayBuffer, byteOffset = 0, options = {}, tile = {}) {
+export default async function parse3DTile(arrayBuffer, byteOffset = 0, options = {}, tile = {}) {
   tile.byteOffset = byteOffset;
   tile.type = getMagicString(arrayBuffer, byteOffset);
 
   switch (tile.type) {
     case TILE3D_TYPE.COMPOSITE:
       // Note: We pass this function as argument so that embedded tiles can be parsed recursively
-      parseComposite3DTileSync(tile, arrayBuffer, byteOffset, options, parse3DTileSync);
-      break;
+      return parseComposite3DTile(tile, arrayBuffer, byteOffset, options, parse3DTile);
 
     case TILE3D_TYPE.BATCHED_3D_MODEL:
-      parseBatchedModel3DTileSync(tile, arrayBuffer, byteOffset, options);
-      break;
+      return parseBatchedModel3DTile(tile, arrayBuffer, byteOffset, options);
 
     case TILE3D_TYPE.INSTANCED_3D_MODEL:
-      parseInstancedModel3DTileSync(tile, arrayBuffer, byteOffset, options);
-      break;
+      return parseInstancedModel3DTile(tile, arrayBuffer, byteOffset, options);
 
     case TILE3D_TYPE.POINT_CLOUD:
-      parsePointCloud3DTileSync(tile, arrayBuffer, byteOffset, options);
-      break;
+      return parsePointCloud3DTile(tile, arrayBuffer, byteOffset, options);
 
     default:
       throw new Error(`3DTileLoader: unknown type ${tile.type}`); // eslint-disable-line
   }
-
-  return tile;
 }
