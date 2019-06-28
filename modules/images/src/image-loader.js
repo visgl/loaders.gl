71b3f53b39bcc14b8148f2fe2e0d6694d2b9a140
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-06-27 18:18:58 -0700

    images: Specify extensions for image loaders (#276)

--- modules/images/src/image-loader.js
+++ modules/images/src/image-loader.js
@@ -6,10 +6,12 @@ import {
   loadToHTMLImage
 } from './lib/parse-image';
 
+const EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp', 'ico', 'svg'];
+
 // Loads a platform-specific image type that can be used as input data to WebGL textures
 export default {
   name: 'Images',
-  extension: [],
+  extensions: EXTENSIONS,
   parse: canParseImage && parseImage,
   loadAndParse: !canParseImage && loadImage
 };
@@ -18,10 +20,12 @@ export default {
 
 // Specifically loads an ImageBitmap (works on newer browsers, on both main and worker threads)
 export const ImageBitmapLoader = {
+  extensions: EXTENSIONS,
   parse: parseToImageBitmap
 };
 
 // Specifically loads an HTMLImage (works on all browsers' main thread but not on worker threads)
 export const HTMLImageLoader = {
+  extensions: EXTENSIONS,
   loadAndParse: loadToHTMLImage
 };
