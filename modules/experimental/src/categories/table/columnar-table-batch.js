4a8c26d800053bed4e737db3ea1db8ef0a234b97
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-08-07 17:38:36 -0700

    [csv] use header option in batch loader (#304)

--- modules/experimental/src/categories/table/columnar-table-batch.js
+++ modules/experimental/src/categories/table/columnar-table-batch.js
@@ -14,7 +14,7 @@ export default class ColumnarTableBatch {
     // If user keeps pushing rows beyond batch size, reallocate
     this.reallocateColumns();
     for (const fieldName in row) {
-      this.columns[fieldName] = row[fieldName];
+      this.columns[fieldName][this.length] = row[fieldName];
     }
     this.length++;
   }
@@ -25,10 +25,21 @@ export default class ColumnarTableBatch {
   }
 
   getNormalizedBatch() {
-    const columns = this.columns;
+    this.pruneColumns();
+    const columns = Array.isArray(this.schema) ? this.columns : {};
+
+    // schema is an array if there're no headers
+    // object if there are headers
+    // columns should match schema format
+    if (!Array.isArray(this.schema)) {
+      for (const fieldName in this.schema) {
+        const field = this.schema[fieldName];
+        columns[field.name] = this.columns[field.index];
+      }
+    }
+
     this.columns = null;
-    // TODO - Ensure column lengths are set to the actual loaded size
-    // this.pruneColumns();
+
     return {data: columns, schema: this.schema, length: this.length};
   }
 
@@ -40,22 +51,30 @@ export default class ColumnarTableBatch {
     }
 
     this.allocated = this.allocated > 0 ? (this.allocated *= 2) : this.batchSize;
-    this.columns = {};
+    this.columns = [];
 
     for (const fieldName in this.schema) {
       const field = this.schema[fieldName];
       const ArrayType = field.type || Float32Array;
-      // const oldColumn = this.columns[fieldName];
-      this.columns[fieldName] = new ArrayType(this.allocated);
+      const oldColumn = this.columns[field.index];
 
-      // Copy the old data to the new array
-      // if (oldColumn) {
-      //   copy(this.columns[fieldName], oldColumn);
-      // }
+      if (oldColumn && ArrayBuffer.isView(oldColumn)) {
+        // Copy the old data to the new array
+        const typedArray = new ArrayType(this.allocated);
+        typedArray.set(oldColumn);
+        this.columns[field.index] = typedArray;
+      } else if (oldColumn) {
+        // Plain array
+        oldColumn.length = this.allocated;
+        this.columns[field.index] = oldColumn;
+      } else {
+        // Create new
+        this.columns[field.index] = new ArrayType(this.allocated);
+      }
     }
   }
 
   pruneColumns() {
-    return this.columns;
+    this.columns = this.columns.map(column => column.slice(0, this.length));
   }
 }
--- modules/experimental/src/categories/table/row-table-batch.js
+++ modules/experimental/src/categories/table/row-table-batch.js
@@ -4,6 +4,15 @@ export default class RowTableBatch {
     this.batchSize = batchSize;
     this.rows = null;
     this.length = 0;
+
+    // schema is an array if there're no headers
+    // object if there are headers
+    if (!Array.isArray(schema)) {
+      this._headers = [];
+      for (const key in schema) {
+        this._headers[schema[key].index] = schema[key].name;
+      }
+    }
   }
 
   addRow(row) {
@@ -11,7 +20,7 @@ export default class RowTableBatch {
       this.rows = new Array(this.batchSize);
       this.length = 0;
     }
-    this.rows[this.length] = row;
+    this.rows[this.length] = convertRowToObject(row, this._headers);
     this.length++;
   }
 
@@ -27,4 +36,15 @@ export default class RowTableBatch {
     }
     return null;
   }
+}
+
+function convertRowToObject(row, headers) {
+  if (!headers) {
+    return row;
+  }
+  const result = {};
+  for (let i = 0; i < headers.length; i++) {
+    result[headers[i]] = row[i];
+  }
+  return result;
 }
