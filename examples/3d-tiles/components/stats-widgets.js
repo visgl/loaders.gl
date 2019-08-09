bb96efc919a7c39ea8c32acc2fe58fe485b7bdf4
Xintong Xia <xintong@uber.com>
2019-08-08 19:55:26 -0700

    Bump deck.gl and fix 3d-tile layer (#309)

--- examples/3d-tiles/components/stats-widgets.js
+++ examples/3d-tiles/components/stats-widgets.js
@@ -1,29 +1,9 @@
 import {StatsWidget} from '@probe.gl/stats-widget';
 import {lumaStats} from '@luma.gl/core';
 
-// const timeWidget = new StatsWidget(this.stats, {
-//   title: 'Render Time',
-//   framesPerUpdate: 60,
-//   formatters: {
-//     'CPU Time': 'averageTime',
-//     'GPU Time': 'averageTime',
-//     'Frame Rate': 'fps'
-//   },
-//   resetOnUpdate: {
-//     'CPU Time': true,
-//     'GPU Time': true,
-//     'Frame Rate': true
-//   },
-//   css: {
-//     position: 'absolute',
-//     top: '20px',
-//     left: '20px'
-//   }
-// });
-
 export function getStatsWidget(container) {
   return new StatsWidget(lumaStats.get('Memory Usage'), {
-    framesPerUpdate: 60,
+    framesPerUpdate: 1,
     formatters: {
       'GPU Memory': 'memory',
       'Buffer Memory': 'memory',
