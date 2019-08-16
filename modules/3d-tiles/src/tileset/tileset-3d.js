f7d6f45de03eccadc21e558bde2b694b726c4e08
Ib Green <ibgreen@users.noreply.github.com>
2019-08-16 07:48:05 -0700

    3d-tiles: traverser no longer knows about tileset (#328)

--- modules/3d-tiles/src/tileset/tileset-3d.js
+++ modules/3d-tiles/src/tileset/tileset-3d.js
@@ -312,7 +312,10 @@ export default class Tileset3D {
     this._updatedVisibilityFrame++; // TODO: only update when camera or culling volume from last update moves (could be render camera change or prefetch camera)
     this._cache.reset();
 
-    this._traverser.traverse(this, frameState);
+    // eslint-disable-next-line consistent-this
+    const options = this; // TODO: hack, create separate options field
+    this._traverser.traverse(this.root, frameState, options);
+    Object.assign(this, this._traverser.result); // Hack during refactor
 
     const requestedTiles = this._requestedTiles;
     // Sort requests by priority before making any requests.
