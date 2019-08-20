0c963cbcf4ac3c0754f0d155e30dbf1a1a6acf8f
Xintong Xia <xintong@uber.com>
2019-08-20 15:37:35 -0700

    remove unused

--- modules/draco/src/index.js
+++ modules/draco/src/index.js
@@ -4,23 +4,3 @@ export {default as DracoWriter} from './draco-writer';
 
 export {default as DracoParser} from './draco-parser';
 export {default as DracoBuilder} from './draco-builder';
-
-// // DEPRECATED
-//
-// // eslint-disable-next-line import/first
-// import DracoParser from './draco-parser';
-//
-// function parseSync(arrayBuffer, options) {
-//   const dracoParser = new DracoParser();
-//   try {
-//     return dracoParser.parseSync(arrayBuffer, options);
-//   } finally {
-//     dracoParser.destroy();
-//   }
-// }
-//
-// export const DracoWorkerLoader = {
-//   name: 'DRACO',
-//   extensions: ['drc'],
-//   parseSync
-// };
