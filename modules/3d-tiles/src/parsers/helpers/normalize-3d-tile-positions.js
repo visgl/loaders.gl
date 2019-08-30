9efd9f2e3b484bfeb64b026a93cfbf6a8f2660c8
Ib Green <ib@uber.com>
2019-08-30 08:27:25 -0700

    Address commnents

--- modules/3d-tiles/src/parsers/helpers/normalize-3d-tile-positions.js
+++ modules/3d-tiles/src/parsers/helpers/normalize-3d-tile-positions.js
@@ -2,21 +2,21 @@ import {Vector3} from 'math.gl';
 import {GL} from '@loaders.gl/math';
 
 // Prepare attribute for positions
-// For quantized posititions, either expand to Float32Array or return custom accessor
-// https://github.com/AnalyticalGraphicsInc/3d-tiles/blob/master/specification/TileFormats/Instanced3DModel/README.md#quantized-positions
-
 export function normalize3DTilePositionAttribute(tile, positions, options) {
   if (!tile.isQuantized) {
     return positions;
   }
 
-  // Optionally decode quantized positions on GPU, for renderers that can't work with normalized attributes
+  // For quantized posititions, either expand to Float32Array or return custom accessor
+  // https://github.com/AnalyticalGraphicsInc/3d-tiles/blob/master/specification/TileFormats/Instanced3DModel/README.md#quantized-positions
+
+  // Optionally decodes quantized positions on GPU, for simpler renderers that don't accept normalized attributes
   if (options.decodeQuantizedPositions) {
     tile.isQuantized = false;
     return decodeQuantizedPositions(tile, positions);
   }
 
-  // Use normalized shorts directly, no copying/processing.
+  // Default: Use normalized shorts directly, no copying/processing.
   // NOTE: The "missing" offset/scaling operations are automatically added to modelMatrix if `tile.isQuantized === true`
   return {
     type: GL.UNSIGNED_SHORT,
@@ -32,15 +32,13 @@ function decodeQuantizedPositions(tile, positions) {
   const decodedArray = new Float32Array(tile.pointCount * 3);
 
   for (let i = 0; i < tile.pointCount; i++) {
-    // POSITION_QUANTIZED * QUANTIZED_VOLUME_SCALE / 65535.0 + QUANTIZED_VOLUME_OFFSET
+    // POSITION = POSITION_QUANTIZED / 65535.0 * QUANTIZED_VOLUME_SCALE + QUANTIZED_VOLUME_OFFSET
     scratchPosition
       .set(positions[i * 3], positions[i * 3 + 1], positions[i * 3 + 2])
-      // TODO - Remove this when deck.gl allows `normalized` flag to be set on `positions`
       .scale(1 / tile.quantizedRange)
       .multiply(tile.quantizedVolumeScale)
-      .add(tile.quantizedVolumeOffset);
-
-    scratchPosition.toArray(decodedArray, i * 3);
+      .add(tile.quantizedVolumeOffset)
+      .toArray(decodedArray, i * 3);
   }
 
   return decodedArray;
