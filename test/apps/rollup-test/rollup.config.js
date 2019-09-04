11c2117ad185b09d623a5e0c9643c913579d2a61
Ib Green <ib@uber.com>
2019-09-03 19:53:20 -0700

    yarn lint fix

--- test/apps/rollup-test/rollup.config.js
+++ test/apps/rollup-test/rollup.config.js
@@ -1,19 +1,18 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-
 export default {
-	input: 'src/main.js',
-	output: {
-		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
-	},
-	plugins: [
-		resolve({
-			browser: true,
-			preferBuiltins: false
-		}),
-		commonjs()
-	]
+  input: 'src/main.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true
+  },
+  plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: false
+    }),
+    commonjs()
+  ]
 };
--- test/apps/rollup-test/src/main.js
+++ test/apps/rollup-test/src/main.js
@@ -1,9 +1,14 @@
-import { parse } from '@loaders.gl/core';
-import { OBJLoader } from "@loaders.gl/obj";
+import {parse} from '@loaders.gl/core';
+import {OBJLoader} from '@loaders.gl/obj';
 
 async function test() {
-    const data = await parse(fetch('https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/obj/test/data/cube.obj'), OBJLoader);
-    console.log(data)
+  const data = await parse(
+    fetch(
+      'https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/obj/test/data/cube.obj'
+    ),
+    OBJLoader
+  );
+  console.log(data);
 }
 
 test();
