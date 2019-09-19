116ef8f076f50c941c837b36396d1099c93ca58f
Ib Green <ibgreen@users.noreply.github.com>
2019-09-19 12:07:17 -0700

    glTF deck.gl example (#416)

---
+++ examples/gltf-deck/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+
+const CONFIG = {
+  mode: 'development',
+
+  entry: {
+    app: './app.js'
+  },
+
+  plugins: [new HtmlWebpackPlugin({title: 'glTF in deck.gl'})],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+};
+
+// This line enables bundling against src in this repo rather than installed module
+module.exports = env => (env ? require('../webpack.config.local')(CONFIG)(env) : CONFIG);
