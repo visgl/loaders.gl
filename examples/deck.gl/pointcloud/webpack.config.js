e459bf816d5dbbde27566524fcdf9ef457eaa2fc
Ib Green <ib@uber.com>
2019-09-22 07:08:48 -0700

    Reorganize examples

--- examples/deck.gl/pointcloud/webpack.config.js
+++ examples/deck.gl/pointcloud/webpack.config.js
@@ -22,8 +22,11 @@ const CONFIG = {
         }
       }
     ]
+  },
+  node: {
+    fs: 'empty'
   }
 };
 
 // This line enables bundling against src in this repo rather than installed module
-module.exports = env => (env ? require('../webpack.config.local')(CONFIG)(env) : CONFIG);
+module.exports = env => (env ? require('../../webpack.config.local')(CONFIG)(env) : CONFIG);
--- website/templates/example-gltf.jsx
+++ website/templates/example-gltf.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import AnimationLoopRunner from '../src/components/animation-loop-runner';
-import AnimationLoop from '../../examples/gltf/app';
+import AnimationLoop from '../../examples/luma.gl/gltf/app';
 
 export default class Example extends React.Component {
   render() {
