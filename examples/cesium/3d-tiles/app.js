d12b40749c687dea32cb70e1dba2fceeb7817d4b
Ib Green <ib@uber.com>
2019-09-27 08:22:19 -0700

    lint

--- examples/cesium/3d-tiles/app.js
+++ examples/cesium/3d-tiles/app.js
@@ -1,16 +1,15 @@
 /* global Cesium */
 
 Cesium.Ion.defaultAccessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4Y2RlNjczZi02MThlLTQxNGQtYmIxZC0wNjFiOWFhNDY2MzEiLCJpZCI6OTYxOSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1NDQ4ODE1OH0.XIVcgLjGcA1HkJlUL-HMaiQfCoaSOrMPmnR7i1ScTyI";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4Y2RlNjczZi02MThlLTQxNGQtYmIxZC0wNjFiOWFhNDY2MzEiLCJpZCI6OTYxOSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1NDQ4ODE1OH0.XIVcgLjGcA1HkJlUL-HMaiQfCoaSOrMPmnR7i1ScTyI';
 
-const viewer = new Cesium.Viewer("cesiumContainer");
+const viewer = new Cesium.Viewer('cesiumContainer');
 
 const tileset = viewer.scene.primitives.add(
   new Cesium.Cesium3DTileset({
     url:
-      " https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/3d-tiles/test/data/Batched/BatchedColorsMix/tileset.json"
+      ' https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/3d-tiles/test/data/Batched/BatchedColorsMix/tileset.json'
   })
 );
 
 viewer.zoomTo(tileset);
-
