5d51bcc195b31bf88d478e208186eb1d94004de7
Ib Green <ib@uber.com>
2019-09-30 12:33:21 -0700

    lint

--- modules/csv/src/papaparse/async-iterator-streamer.js
+++ modules/csv/src/papaparse/async-iterator-streamer.js
@@ -3,9 +3,7 @@
 // Or papaparse can expose Streamer API so we can extend without forking.
 
 /* eslint-disable no-invalid-this */
-
-// TODO - Not found by Node.js - how to deal with this without loaders.gl?
-import {TextDecoder} from '@loaders.gl/core';
+/* global TextDecoder */
 
 // Note: papaparse is not an ES6 module
 import Papa from './papaparse.transpiled';
