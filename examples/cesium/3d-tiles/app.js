f6043c61d037037b3187ca9cb8ec135a586c046c
Xintong Xia <xintong@uber.com>
2019-10-02 14:56:01 -0700

    update cesium example

--- examples/cesium/3d-tiles/app.js
+++ examples/cesium/3d-tiles/app.js
@@ -1,15 +1,99 @@
-/* global Cesium */
+/* global Cesium, fetch */
+
+import {Vector3} from 'math.gl';
+import {registerLoaders} from '@loaders.gl/core';
+import {DracoWorkerLoader} from '@loaders.gl/draco';
+import {Tileset3D, _getIonTilesetMetadata} from '@loaders.gl/3d-tiles';
+
+registerLoaders([DracoWorkerLoader]);
 
 Cesium.Ion.defaultAccessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4Y2RlNjczZi02MThlLTQxNGQtYmIxZC0wNjFiOWFhNDY2MzEiLCJpZCI6OTYxOSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1NDQ4ODE1OH0.XIVcgLjGcA1HkJlUL-HMaiQfCoaSOrMPmnR7i1ScTyI';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWMxMzcyYy0zZjJkLTQwODctODNlNi01MDRkZmMzMjIxOWIiLCJpZCI6OTYyMCwic2NvcGVzIjpbImFzbCIsImFzciIsImdjIl0sImlhdCI6MTU2Mjg2NjI3M30.1FNiClUyk00YH_nWfSGpiQAjR5V2OvREDq1PJ5QMjWQ';
+
+// eslint-disable-next-line
+const TILESET_URL =
+  'https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/3d-tiles/test/data/Batched/BatchedColorsMix/tileset.json';
+const NELBOURNE_ION_ASSET_ID = 43978;
 
 const viewer = new Cesium.Viewer('cesiumContainer');
 
 const tileset = viewer.scene.primitives.add(
   new Cesium.Cesium3DTileset({
-    url:
-      ' https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/3d-tiles/test/data/Batched/BatchedColorsMix/tileset.json'
+    url: Cesium.IonResource.fromAssetId(NELBOURNE_ION_ASSET_ID)
   })
 );
 
 viewer.zoomTo(tileset);
+
+function convertCesiumFrameState(frameState, height) {
+  let cameraPosition = frameState.camera.position;
+  cameraPosition = new Vector3([cameraPosition.x, cameraPosition.y, cameraPosition.z]);
+
+  let cameraDirection = frameState.camera.direction;
+  cameraDirection = new Vector3([cameraDirection.x, cameraDirection.y, cameraDirection.z]);
+
+  let cameraUp = frameState.camera.up;
+  cameraUp = new Vector3([cameraUp.x, cameraUp.y, cameraUp.z]);
+
+  const cullingVolume = frameState.cullingVolume;
+  const planes = cullingVolume.planes.map(plane => ({
+    normal: new Vector3(plane.x, plane.y, plane.z),
+    distance: plane.w
+  }));
+
+  return {
+    camera: {
+      position: cameraPosition,
+      direction: cameraDirection,
+      up: cameraUp
+    },
+    height,
+    cullingVolume: {
+      ...cullingVolume,
+      planes
+    },
+    frameNumber: frameState.frameNumber, // TODO: This can be the same between updates, what number is unique for between updates?
+    sseDenominator: frameState.camera.frustum.sseDenominator
+  };
+}
+
+fetchTilesetJson({
+  ionAssetId: NELBOURNE_ION_ASSET_ID,
+  ionAccessToken: Cesium.Ion.defaultAccessToken
+})
+  .then(({tilesetJson, tilesetUrl, ionMetadata}) => {
+    const tileset3d = new Tileset3D(tilesetJson, tilesetUrl, {
+      onTileLoad: tileHeader => {
+        // eslint-disable-next-line
+        console.log(tileHeader.uri);
+      },
+      ...ionMetadata,
+      throttleRequests: true
+    });
+    return tileset3d;
+  })
+  .then(tileset3d => {
+    viewer.scene.preRender.addEventListener(scene => {
+      const frameState = convertCesiumFrameState(scene.frameState, scene.canvas.height);
+      tileset3d.update(frameState);
+    });
+  });
+
+async function fetchTilesetJson({tilesetUrl, ionAssetId, ionAccessToken}) {
+  let fetchOptions = null;
+  let ionMetadata = {};
+  if (ionAssetId && ionAccessToken) {
+    ionMetadata = await _getIonTilesetMetadata(ionAccessToken, ionAssetId);
+    const {url, headers} = ionMetadata;
+    tilesetUrl = url;
+    fetchOptions = {headers};
+  }
+
+  const response = await fetch(tilesetUrl, fetchOptions);
+  const tilesetJson = await response.json();
+  return {
+    tilesetJson,
+    tilesetUrl,
+    ionMetadata
+  };
+}
