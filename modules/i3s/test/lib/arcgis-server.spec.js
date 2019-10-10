d347f1016cffcd9f6a01565b0e9bdecef4ee5048
Ib Green <ib@uber.com>
2019-10-10 13:56:09 -0700

    wip

--- modules/i3s/test/lib/arcgis-server.spec.js
+++ modules/i3s/test/lib/arcgis-server.spec.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
-const ARCGIS_URL =
-  '@loaders.gl/i3s/test/data/SanFrancisco_Bldgs/SceneServer/layers/0';
+const ARCGIS_URL = `${__dirname}/../data/SanFrancisco_Bldgs/SceneServer/layers/0`;
+// '@loaders.gl/i3s/test/data/SanFrancisco_Bldgs/SceneServer/layers/0';
 // 'https://tiles.arcgis.com/tiles/z2tnIkrLQ2BRzr6P/arcgis/rest/services/SanFrancisco_Bldgs/SceneServer/layers/0?p=fjson';
 
 import test from 'tape-promise/tape';
@@ -9,6 +9,6 @@ import {fetchFile} from '@loaders.gl/core';
 test('getArcgisAssetMetadata#Download layer data', async t => {
   const response = await fetchFile(ARCGIS_URL);
   const tileset = await response.json();
-  t.ok(tileset);
+  t.equal(tileset.id, 0);
   t.end();
 });
