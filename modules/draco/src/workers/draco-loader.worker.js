0f452d4e3dd03524b633ffaff1cf0246b87a0a64
Ib Green <ib@unfolded.ai>
2019-12-12 09:50:59 -0800

    remove unused constant

--- modules/draco/src/workers/draco-loader.worker.js
+++ modules/draco/src/workers/draco-loader.worker.js
@@ -5,8 +5,6 @@ import {createWorker} from '@loaders.gl/loader-utils';
 import {DracoLoader} from '../draco-loader';
 import draco3d from 'draco3d';
 
-const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
-
 // Define a loader object that bundles the draco3d module
 // (instead of loading draco3d WASM modules dynamically, as is done on the main thread)
 const BundledDracoLoader = {
