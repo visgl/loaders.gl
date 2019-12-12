6d2497dfd1fecc9d4f374f557e84a323fb7aaee0
Xintong Xia <xintong@uber.com>
2019-12-12 13:02:26 -0800

    Revert "build speed 2x: Avoid transpiling libs folder (#554)"
    This reverts commit 940deb70732c6c8d9ecf4fff3e8aed27f8687adc.

--- babel.config.js
+++ babel.config.js
@@ -6,16 +6,25 @@ module.exports = api => {
   config.plugins = config.plugins || [];
   config.plugins.push('version-inline');
 
+  // https://babeljs.io/docs/en/options#overrides
+  const overrides = config.overrides || [];
+  // TEST to prevent compilation of already transpiled files
+  // TODO: Ideally these files should be copied to the right dist folder without any further modification
+  // It still seems they are being parsed by babel which does take a relatively long time
+  overrides.push({
+    test: /src\/libs/,
+    compact: false,
+    sourceMaps: false
+  });
+  // Default babel config (env, plugin) only apply to the rest of the files
+  overrides.push({
+    exclude: /src\/libs/,
+    ...config
+  });
+
   return {
-    ...config,
-    ignore: [
-      // Don't transpile workers, they are transpiled separately
-      '**/*.worker.js',
-      '**/workers/*.js',
-      // Don't transpile files in libs, we use this folder to store external,
-      // already transpiled and minified libraries and scripts.
-      // e.g. draco, basis, las-perf etc.
-      /src\/libs/
-    ]
+    // Don't transpile workers, they are transpiled separately
+    ignore: ['**/*.worker.js', '**/workers/*.js'],
+    overrides
   };
 };
