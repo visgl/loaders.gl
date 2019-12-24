6b179f201f169e1a0fece4225e2b785373b94249
Ib Green <ib@unfolded.ai>
2019-12-24 09:25:29 -0800

    core: `load` and `parse` from `Stream`s (#588)

--- modules/core/test/lib/load.spec.js
+++ modules/core/test/lib/load.spec.js
@@ -1,16 +1,11 @@
 /* global Blob */
-import {isBrowser, load, registerLoaders} from '@loaders.gl/core';
-
 import test from 'tape-promise/tape';
+import {isBrowser, load, fetchFile, registerLoaders, resolvePath} from '@loaders.gl/core';
+import {JSONLoader} from '@loaders.gl/json';
+
+const JSON_URL = '@loaders.gl/core/test/data/files/basic.json';
 
 const JSON_DATA = [{col1: 22, col2: 'abc'}];
-
-const JSONLoader = {
-  name: 'JSON',
-  extensions: ['json'],
-  testText: null,
-  parseTextSync: JSON.parse
-};
 
 test('load#load', async t => {
   t.ok(load, 'load defined');
@@ -33,7 +28,7 @@ test('load#auto detect loader', t => {
   load('package.json', {JSON: {option: true}});
 });
 
-test('load#Blob (text)', async t => {
+test('load#Blob(text) - BROWSER ONLY', async t => {
   if (!isBrowser) {
     t.comment('Skipping load(Blob) tests in Node.js');
     t.end();
@@ -46,6 +41,29 @@ test('load#Blob (text)', async t => {
   const data = await load(blob, JSONLoader);
 
   t.deepEquals(data, JSON_DATA, 'load(Blob) returned data');
+
+  t.end();
+});
+
+test('load#stream', async t => {
+  const response = await fetchFile(JSON_URL);
+  const stream = response.body;
+  const data = await load(stream, JSONLoader);
+  t.equals(typeof data, 'object', 'load(stream) returned data');
+  t.end();
+});
+
+test('load#Node stream - NODE ONLY', async t => {
+  if (isBrowser) {
+    t.comment('Skipping load(Node stream) tests in Node.js');
+    t.end();
+    return;
+  }
+
+  const fs = require('fs');
+  const stream = fs.createReadStream(resolvePath(JSON_URL));
+  const data = await load(stream, JSONLoader);
+  t.equals(typeof data, 'object', 'load(Node stream) returned data');
 
   t.end();
 });
