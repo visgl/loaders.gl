f724f45a05d2f0ce6a8a384a9e544807b645fb58
Xintong Xia <xintong@uber.com>
2020-01-03 11:56:04 -0800

    use getDefaultImageType

--- modules/images/src/index.js
+++ modules/images/src/index.js
@@ -2,7 +2,7 @@ export {default as ImageLoader} from './image-loader';
 export {default as ImageWriter} from './image-writer';
 
 // Parsed Image API
-export {isImageTypeSupported, getSupportedImageType} from './lib/parsed-image-api/image-type';
+export {isImageTypeSupported} from './lib/parsed-image-api/image-type';
 export {
   isImage,
   getImageType,
@@ -34,3 +34,9 @@ export {
 
 // DEPRECATED
 export {default as HTMLImageLoader} from './image-loader';
+
+import {getDefaultImageType} from './lib/parsed-image-api/image-type';
+
+export function getSupportedImageType(imageType) {
+  return getDefaultImageType(imageType);
+}
