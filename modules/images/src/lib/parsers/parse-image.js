9f0f44839f5e712062fe7a2ee4d9d63575389301
Xiaoji Chen <xiaoji@uber.com>
2020-01-10 15:11:05 -0800

    comments

--- modules/images/src/lib/parsers/parse-image.js
+++ modules/images/src/lib/parsers/parse-image.js
@@ -6,12 +6,15 @@ import parseToImage from './parse-to-image';
 import parseToImageBitmap from './parse-to-image-bitmap';
 import parseSVG from './parse-svg';
 
+const SVG_DATA_URL_PATTERN = /^data:image\/svg\+xml/;
+const SVG_URL_PATTERN = /\.svg((\?|#).*)?$/;
+
 // Parse to platform defined image type (ndarray on node, ImageBitmap or HTMLImage on browser)
 export default async function parseImage(arrayBuffer, options, context) {
   options = options || {};
 
   const {url} = context || {};
-  if (url && /^data:image\/svg\+xml|\.svg((\?|#).*)?$/.test(url)) {
+  if (url && (SVG_DATA_URL_PATTERN.test(url) || SVG_URL_PATTERN.test(url))) {
     return await parseSVG(arrayBuffer, options);
   }
 
