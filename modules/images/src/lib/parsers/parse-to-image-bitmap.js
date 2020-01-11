5eccf86ad37779867b636c4cf6dbe58c7f834fb7
Ib Green <ib@unfolded.ai>
2020-01-11 06:39:50 -0800

    image: Fix options.imagebitmap on Firefox

--- modules/images/src/lib/parsers/parse-to-image-bitmap.js
+++ modules/images/src/lib/parsers/parse-to-image-bitmap.js
@@ -4,9 +4,33 @@
 // Supported on worker threads, not supported on Edge, IE11 and Safari
 // https://developer.mozilla.org/en-US/docs/Web/API/ImageBitmap#Browser_compatibility
 
+let imagebitmapOptionsSupported = true;
+
 export default function parseToImageBitmap(arrayBuffer, options) {
   // NOTE: In some cases unnecessary conversion to blob (response, blob, file input)
   const blob = new Blob([new Uint8Array(arrayBuffer)]); // MIME type not needed...
-  const imagebitmapOptions = options && options.imagebitmap;
-  return imagebitmapOptions ? createImageBitmap(blob) : createImageBitmap(blob, imagebitmapOptions);
+  let imagebitmapOptions = options && options.imagebitmap;
+
+  // Firefox crashes if imagebitmapOptions is supplied
+  // Avoid supplying if not provided, track if not supported
+  if (isEmptyObject(imagebitmapOptions) || !imagebitmapOptionsSupported) {
+    imagebitmapOptions = null;
+  }
+
+  if (imagebitmapOptions) {
+    try {
+      return createImageBitmap(blob, imagebitmapOptions);
+    } catch (error) {
+      console.warn(error); // eslint-disable-line
+      imagebitmapOptionsSupported = false;
+    }
+  }
+  return createImageBitmap(blob);
+}
+
+function isEmptyObject(object) {
+  for (const key in object) {
+    return true;
+  }
+  return false;
 }
