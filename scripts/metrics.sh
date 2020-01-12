cb734d193a0a1b2ac091bdb92b0e5838f0709508
Ib Green <ib@unfolded.ai>
2020-01-12 08:57:26 -0800

    build: Better metrics (#621)

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
 echo '# Minified Scripts'
-find modules -name dist.min.js | while read file; do
-    ls -S -lh "$file" | grep -v node_modules | awk '{print $9, $5}' | sort
+find modules -name dist.min.js | sort | while read file; do
+    ls -S -lh "$file" | grep -v node_modules | awk '{print $9, $5}'
 done
 
 echo '# Worker sizes'
-find modules -name "*-loader.worker.js" | grep -v src | while read file; do
+find modules -name "*-loader.worker.js" | sort | grep -v 'src\|esm\|es5\|es6' | while read file; do
     ls -S -lh "$file" | grep -v test | awk '{print $9, $5}'
 done
