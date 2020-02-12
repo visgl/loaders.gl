71d6edf7bfde243f34111fdaf25afd44a213033f
Chris Gervang <chr@uber.com>
2020-02-12 14:27:32 -0800

    Revert ImageLoader change due to Worker test failure

--- modules/terrain/docs/api-reference/terrain-loader.md
+++ modules/terrain/docs/api-reference/terrain-loader.md
@@ -15,8 +15,11 @@ The `TerrainLoader` reconstructs mesh surfaces from height map images, e.g. [Map
 ## Usage
 
 ```js
+import {ImageLoader} from '@loaders.gl/images';
 import {TerrainLoader} from '@loaders.gl/terrain';
-import {load} from '@loaders.gl/core';
+import {load, registerLoaders} from '@loaders.gl/core';
+
+registerLoaders(ImageLoader);
 
 const data = await load(url, TerrainLoader, options);
 ```
--- modules/terrain/src/lib/parse-terrain.js
+++ modules/terrain/src/lib/parse-terrain.js
@@ -1,5 +1,4 @@
 import Martini from '@mapbox/martini';
-import {ImageLoader} from '@loaders.gl/images';
 
 function getTerrain(imageData, tileSize, elevationDecoder) {
   const {rScaler, bScaler, gScaler, offset} = elevationDecoder;
@@ -92,7 +91,7 @@ function getMartiniTileMesh(terrainImage, terrainOptions) {
 export default async function loadTerrain(arrayBuffer, options, context) {
   options.image = options.image || {};
   options.image.type = 'data';
-  const image = await context.parse(arrayBuffer, ImageLoader, options, options.baseUri);
+  const image = await context.parse(arrayBuffer, options, options.baseUri);
   // Extend function to support additional mesh generation options (square grid or delatin)
   return getMartiniTileMesh(image, options.terrain);
 }
