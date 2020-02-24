028846dbdeb0c6663d21feedbd4804041091fbda
xintong <xintong@uber.com>
2020-02-24 14:23:55 -0800

    rebase and lint fix

--- modules/i3s/src/lib/utils/lod.js
+++ modules/i3s/src/lib/utils/lod.js
@@ -117,5 +117,6 @@ function calculateScreenSizeFactor(tile, frameState) {
 
   const screenCircleFactor = Math.sqrt(height * height + width * width) / tanOfHalfVFAngle;
 
-  return screenCircleFactor * Math.PI / 2; //emperically derived bias factor:
+  // emperically derived bias factor
+  return (screenCircleFactor * Math.PI) / 2;
 }
