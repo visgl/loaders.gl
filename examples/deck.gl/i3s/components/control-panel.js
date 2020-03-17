8e5356d11f9b27140d1b0ca3fce5b0665dc23212
xintong <xintong@uber.com>
2020-03-17 10:57:47 -0700

    address comments

--- examples/deck.gl/i3s/components/control-panel.js
+++ examples/deck.gl/i3s/components/control-panel.js
@@ -17,7 +17,7 @@ const Container = styled.div`
   position: absolute;
   top: 0;
   right: 0;
-  width: 340px;
+  width: 300px;
   background: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
   padding: 12px 18px;
@@ -39,10 +39,31 @@ const TilesetDropDown = styled.select`
 `;
 
 const FrameWrap = styled.div`
-  width: 320px;
-  height: 200px;
+  height: fit-content;
   padding: 0;
   overflow: hidden;
+`;
+
+const FrameControl = styled.div`
+  margin: ${props => (props.showFullInfo ? '12px 0' : 0)};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const FrameButton = styled.div`
+  display: flex;
+  padding: 6px 12px;
+  color: white;
+  background: rgb(52, 152, 219);
+  align-items: center;
+  height: 20px;
+  &:hover {
+    background: rgb(0, 152, 219);
+    cursor: pointer;
+  }
 `;
 
 const propTypes = {
@@ -64,6 +85,7 @@ export default class ControlPanel extends PureComponent {
     super(props);
     this._renderMapStyles = this._renderMapStyles.bind(this);
     this.state = {
+      showFullInfo: false,
       selectedMapStyle: INITIAL_MAP_STYLE
     };
   }
@@ -125,20 +147,32 @@ export default class ControlPanel extends PureComponent {
 
   _renderInfo() {
     const {metadata} = this.props;
+    const {showFullInfo} = this.state;
     if (!metadata) {
       return null;
     }
+
     const url = `https://www.arcgis.com/home/item.html?id=${metadata.serviceItemId}`;
     return (
       <FrameWrap>
         <iframe
-          style={{border: '1px solid rgba(200, 200, 200, 100)'}}
+          style={{
+            display: showFullInfo ? 'inherit' : 'none',
+            height: 500,
+            width: '99%',
+            border: '1px solid rgba(200, 200, 200, 100)'
+          }}
           id={'tileset-info'}
           src={url}
         />
-        <div>
-          <a href={url}>Go to ArcGIS</a>
-        </div>
+        <FrameControl showFullInfo={showFullInfo}>
+          <FrameButton onClick={() => this.setState({showFullInfo: !showFullInfo})}>
+            Show {showFullInfo ? `Less` : `More`}
+          </FrameButton>
+          <a target="_blank" rel="noopener noreferrer" href={url}>
+            Go to ArcGIS
+          </a>
+        </FrameControl>
       </FrameWrap>
     );
   }
