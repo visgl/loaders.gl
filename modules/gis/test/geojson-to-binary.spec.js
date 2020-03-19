1e92d60d3a49a79401b6dd29f8fded01ea26cb21
Kyle Barron <barronk@mit.edu>
2020-03-18 19:52:00 -0600

    Add 3D coordinates test

--- modules/gis/test/geojson-to-binary.spec.js
+++ modules/gis/test/geojson-to-binary.spec.js
@@ -5,6 +5,7 @@ import {geojsonToBinary} from '@loaders.gl/gis';
 // Sample GeoJSON Files
 // All features have 2D coordinates
 const FEATURES_2D = '@loaders.gl/gis/test/data/2d_features.json';
+const FEATURES_3D = '@loaders.gl/gis/test/data/3d_features.json';
 
 test('gis#geojson-to-binary 2D features', async t => {
   const response = await fetchFile(FEATURES_2D);
@@ -129,6 +130,150 @@ test('gis#geojson-to-binary 2D features', async t => {
     6,
     6,
     6
+  ]);
+  t.end();
+});
+
+test('gis#geojson-to-binary 3D features', async t => {
+  const response = await fetchFile(FEATURES_3D);
+  const {features} = await response.json();
+  const {points, lines, polygons} = geojsonToBinary(features);
+
+  // 3D size
+  t.equal(points.positions.size, 3);
+  t.equal(lines.positions.size, 3);
+  t.equal(polygons.positions.size, 3);
+
+  // Other arrays have coordinate size 1
+  t.equal(points.objectIds.size, 1);
+  t.equal(lines.pathIndices.size, 1);
+  t.equal(lines.objectIds.size, 1);
+  t.equal(polygons.polygonIndices.size, 1);
+  t.equal(polygons.primitivePolygonIndices.size, 1);
+  t.equal(polygons.objectIds.size, 1);
+
+  // Point value equality
+  t.deepEqual(points.positions.value, [100, 0, 1, 100, 0, 2, 101, 1, 3]);
+  t.deepEqual(points.objectIds.value, [0, 1, 1]);
+
+  // LineString value equality
+  t.deepEqual(lines.pathIndices.value, [0, 2, 4]);
+  t.deepEqual(lines.positions.value, [
+    100,
+    0,
+    4,
+    101,
+    1,
+    5,
+    100,
+    0,
+    6,
+    101,
+    1,
+    7,
+    102,
+    2,
+    8,
+    103,
+    3,
+    9
+  ]);
+  t.deepEqual(lines.objectIds.value, [2, 2, 3, 3, 3, 3]);
+
+  // Polygon value equality
+  t.deepEqual(polygons.polygonIndices.value, [0, 5, 15, 20]);
+  t.deepEqual(polygons.primitivePolygonIndices.value, [0, 5, 10, 15, 20, 25]);
+  t.deepEqual(polygons.positions.value, [
+    100,
+    0,
+    10,
+    101,
+    0,
+    11,
+    101,
+    1,
+    12,
+    100,
+    1,
+    13,
+    100,
+    0,
+    14,
+    100,
+    0,
+    15,
+    101,
+    0,
+    16,
+    101,
+    1,
+    17,
+    100,
+    1,
+    18,
+    100,
+    0,
+    19,
+    100.8,
+    0.8,
+    20,
+    100.8,
+    0.2,
+    21,
+    100.2,
+    0.2,
+    22,
+    100.2,
+    0.8,
+    23,
+    100.8,
+    0.8,
+    24,
+    102,
+    2,
+    25,
+    103,
+    2,
+    26,
+    103,
+    3,
+    27,
+    102,
+    3,
+    28,
+    102,
+    2,
+    29,
+    100,
+    0,
+    30,
+    101,
+    0,
+    31,
+    101,
+    1,
+    32,
+    100,
+    1,
+    33,
+    100,
+    0,
+    34,
+    100.2,
+    0.2,
+    35,
+    100.2,
+    0.8,
+    36,
+    100.8,
+    0.8,
+    37,
+    100.8,
+    0.2,
+    38,
+    100.2,
+    0.2,
+    39
   ]);
   t.end();
 });
