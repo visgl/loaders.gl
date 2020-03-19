5d7fbd1d9e4134d05fc0ecedbe2f2922b5d00561
Kyle Barron <barronk@mit.edu>
2020-03-18 19:13:44 -0600

    Add tests

--- modules/gis/test/geojson-to-binary.spec.js
+++ modules/gis/test/geojson-to-binary.spec.js
@@ -1,6 +1,5 @@
 import test from 'tape-promise/tape';
 import {fetchFile} from '@loaders.gl/core';
-
 import {geojsonToBinary} from '@loaders.gl/gis';
 
 // Sample GeoJSON Files
@@ -24,5 +23,112 @@ test('gis#geojson-to-binary 2D features', async t => {
   t.equal(polygons.polygonIndices.size, 1);
   t.equal(polygons.primitivePolygonIndices.size, 1);
   t.equal(polygons.objectIds.size, 1);
+
+  // Point value equality
+  t.deepEqual(points.positions.value, [100, 0, 100, 0, 101, 1]);
+  t.deepEqual(points.objectIds.value, [0, 1, 1]);
+
+  // LineString value equality
+  t.deepEqual(lines.pathIndices.value, [0, 2, 4]);
+  t.deepEqual(lines.positions.value, [100, 0, 101, 1, 100, 0, 101, 1, 102, 2, 103, 3]);
+  t.deepEqual(lines.objectIds.value, [2, 2, 3, 3, 3, 3]);
+
+  // Polygon value equality
+  t.deepEqual(polygons.polygonIndices.value, [0, 5, 15, 20]);
+  t.deepEqual(polygons.primitivePolygonIndices.value, [0, 5, 10, 15, 20, 25]);
+  t.deepEqual(polygons.positions.value, [
+    100,
+    0,
+    101,
+    0,
+    101,
+    1,
+    100,
+    1,
+    100,
+    0,
+    100,
+    0,
+    101,
+    0,
+    101,
+    1,
+    100,
+    1,
+    100,
+    0,
+    100.8,
+    0.8,
+    100.8,
+    0.2,
+    100.2,
+    0.2,
+    100.2,
+    0.8,
+    100.8,
+    0.8,
+    102,
+    2,
+    103,
+    2,
+    103,
+    3,
+    102,
+    3,
+    102,
+    2,
+    100,
+    0,
+    101,
+    0,
+    101,
+    1,
+    100,
+    1,
+    100,
+    0,
+    100.2,
+    0.2,
+    100.2,
+    0.8,
+    100.8,
+    0.8,
+    100.8,
+    0.2,
+    100.2,
+    0.2
+  ]);
+  t.deepEqual(polygons.objectIds.value, [
+    4,
+    4,
+    4,
+    4,
+    4,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6,
+    6
+  ]);
   t.end();
 });
