e7984fc17c5c269f4c3b7bfa39e1ee64ac38ed5a
Ib Green <ib@unfolded.ai>
2020-06-01 20:05:02 -0700

    Update gifshot example to use piublished script

--- modules/video/test/gif-builder.spec.js
+++ modules/video/test/gif-builder.spec.js
@@ -1,8 +1,7 @@
 import test from 'tape-promise/tape';
 
-import {isBrowser} from '@loaders.gl/core';
-// import {load, isBrowser} from '@loaders.gl/core';
-// import {ImageLoader} from '@loaders.gl/images';
+import {load, isBrowser} from '@loaders.gl/core';
+import {ImageLoader} from '@loaders.gl/images';
 import {GIFBuilder} from '@loaders.gl/video';
 
 const IMAGE_URLS = [
@@ -10,8 +9,6 @@ const IMAGE_URLS = [
   'http://i.imgur.com/qOwVaSN.png',
   'http://i.imgur.com/Vo5mFZJ.gif'
 ];
-
-// const IMAGES = Promise.all(IMAGE_URLS.map(url => load(url, ImageLoader, {images: {type: 'image'}})));
 
 test('GIFBuilder#imports', t => {
   t.ok(GIFBuilder, 'GIFBuilder defined');
@@ -27,6 +24,28 @@ test('GIFBuilder#load(URL)', async t => {
   const gifBuilder = new GIFBuilder({source: 'images', width: 400, height: 400});
 
   for (const image of IMAGE_URLS) {
+    await gifBuilder.add(image);
+  }
+
+  const gifDataUrl = await gifBuilder.build();
+  t.ok(gifDataUrl.startsWith('data:image/gif;'), 'build() returns GIF image encoded as data URL');
+
+  t.end();
+});
+
+test('GIFBuilder#load(Image)', async t => {
+  if (!isBrowser) {
+    t.end();
+    return;
+  }
+
+  const IMAGES = await Promise.all(
+    IMAGE_URLS.map(url => load(url, ImageLoader, {image: {type: 'image'}}))
+  );
+
+  const gifBuilder = new GIFBuilder({source: 'images', width: 400, height: 400});
+
+  for (const image of IMAGES) {
     await gifBuilder.add(image);
   }
 
