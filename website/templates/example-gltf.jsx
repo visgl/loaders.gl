4c7c7f33c27fe8866607e07f9aa867c06a16294c
Ib Green <ib@unfolded.ai>
2020-06-07 20:18:28 -0700

    examples: Move to website /experimental folders (#771)

--- website/templates/example-gltf.jsx
+++ website/templates/example-gltf.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import AnimationLoopRunner from '../src/components/animation-loop-runner';
-import AnimationLoop from '../../examples/luma.gl/gltf/app';
+import AnimationLoop from '../../examples/website/gltf/app';
 
 export default class Example extends React.Component {
   render() {
