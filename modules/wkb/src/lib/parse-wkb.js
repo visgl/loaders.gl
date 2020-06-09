ba0d0c2c3b1fb2cd0a340565533cdb5760b023c1
Kyle Barron <kylebarron2@gmail.com>
2020-06-09 13:22:46 -0600

    Remove endianness handling in favor of DataView

--- modules/wkb/src/lib/parse-wkb.js
+++ modules/wkb/src/lib/parse-wkb.js
@@ -4,6 +4,7 @@
 function parseWKB(buffer) {
   var view = new DataView(buffer)
   var offset = 0;
+
   // Check endianness of data
   var littleEndian = view.getUint8(offset) === 1
   offset++
@@ -41,40 +42,15 @@ function parseWKB(buffer) {
       console.error(`Unsupported geometry type: ${geometryType}`)
   }
 
-  if (littleEndian === isHostLittleEndian()) {
-    // Possible to make typedarray without copy?
-    // Yes, you can just do new Float64Array(buffer, offset)
-    var point = new Float64Array(buffer.slice(offset))
-  } else {
-    var point = new Float64Array(reverseEndianness(buffer.slice(offset)));
+
+
+}
+
+
   }
 
-
-}
-
-// TODO don't copy, just use DataView with offset
-function reverseEndianness(buffer, dataLittleEndian) {
-  var buf = buffer.slice(offset);
-  var view = new DataView(buf);
-
-  // Reverse endianness of each double in buffer
-  for (var i = 0; i < view.byteLength; i+=8) {
-    view.setFloat64(i, view.getFloat64(i, dataLittleEndian), !dataLittleEndian)
-  }
-
-  return buf;
 }
 
 
-// https://gist.github.com/TooTallNate/4750953
-function isHostLittleEndian() {
-  var b = new ArrayBuffer(4);
-  var a = new Uint32Array(b);
-  var c = new Uint8Array(b);
-  a[0] = 0xdeadbeef;
-  if (c[0] == 0xef) return true;
-  if (c[0] == 0xde) return false;
-  throw new Error('unknown endianness');
-}
 
 
