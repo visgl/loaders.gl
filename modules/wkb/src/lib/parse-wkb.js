84891877eb1ecbf938f7c95b587339a183253ba3
Kyle Barron <kylebarron2@gmail.com>
2020-06-09 18:02:21 -0600

    Define primitives first

--- modules/wkb/src/lib/parse-wkb.js
+++ modules/wkb/src/lib/parse-wkb.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 
 var NUM_DIMENSIONS = {
-  0: 2,
-  1: 3,
-  2: 3,
-  3: 4
+  0: 2, // 2D
+  1: 3, // 3D (Z)
+  2: 3, // 3D (M)
+  3: 4 // 4D (ZM)
 };
 
 function parseWKB(buffer) {
@@ -24,29 +24,54 @@ function parseWKB(buffer) {
 
   switch (geometryType) {
     case 1:
-      parsePoint();
+      parsePoint(view, offset, dimension, littleEndian);
       break;
     case 2:
-      parseLineString();
+      parseLineString(view, offset, dimension, littleEndian);
       break;
     case 3:
       parsePolygon(view, offset, dimension, littleEndian);
       break;
     case 4:
-      parseMultiPoint();
+      parseMultiPoint(view, offset, dimension, littleEndian);
       break;
     case 5:
-      parseMultiLineString();
+      parseMultiLineString(view, offset, dimension, littleEndian);
       break;
     case 6:
-      parseMultiPolygon();
+      parseMultiPolygon(view, offset, dimension, littleEndian);
       break;
     case 7:
-      parseGeometryCollection();
+      parseGeometryCollection(view, offset, dimension, littleEndian);
       break;
     default:
       console.error(`Unsupported geometry type: ${geometryType}`);
   }
+}
+
+// Primitives; parse point and linear ring
+function parsePoint(view, offset, dimension, littleEndian) {
+  var positions = new Float64Array(dimension);
+  for (var i = 0; i < dimension; i++) {
+    positions[i] = view.getFloat64(offset, littleEndian);
+    offset += 8;
+  }
+
+  return {positions, offset};
+}
+
+function parseLinearRing(view, offset, dimension, littleEndian) {
+  var nPoints = view.getUint32(offset, littleEndian);
+  offset += 4;
+
+  // Instantiate array
+  var coords = new Float64Array(nPoints * dimension);
+  for (var i = 0; i < nPoints * dimension; i++) {
+    coords[i] = view.getFloat64(offset, littleEndian);
+    offset += 8;
+  }
+
+  return {coords, offset};
 }
 
 function parsePolygon(view, offset, dimension, littleEndian) {
@@ -71,20 +96,6 @@ function parsePolygon(view, offset, dimension, littleEndian) {
     positions: {value: positions, size: dimension},
     primitivePolygonIndices: {value: primitivePolygonIndices, size: 1}
   };
-}
-
-function parseLinearRing(view, offset, dimension, littleEndian) {
-  var nPoints = view.getUint32(offset, littleEndian);
-  offset += 4;
-
-  // Instantiate array
-  var coords = new Float64Array(nPoints * dimension);
-  for (var i = 0; i < nPoints * dimension; i++) {
-    coords[i] = view.getFloat64(offset, littleEndian);
-    offset += 8;
-  }
-
-  return {coords, offset};
 }
 
 // TODO: remove copy; import from typed-array-utils
