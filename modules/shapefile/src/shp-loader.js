66a7f446a876b30a495f4aa7614bb0a88f11cb18
Kyle Barron <kylebarron2@gmail.com>
2020-06-19 13:30:08 -0600

    Add src scaffolding

---
+++ modules/shapefile/src/shp-loader.js
@@ -0,0 +1,30 @@
+// import parseSHP from './lib/parse-shp';
+const parseSHP = (arrayBuffer) => null;
+
+/** @typedef {import('@loaders.gl/loader-utils').LoaderObject} LoaderObject */
+
+// __VERSION__ is injected by babel-plugin-version-inline
+// @ts-ignore TS2304: Cannot find name '__VERSION__'.
+const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
+
+/** @type {LoaderObject} */
+export const SHPWorkerLoader = {
+  id: 'shp',
+  name: 'SHP',
+  category: 'geometry',
+  version: VERSION,
+  extensions: ['shp'],
+  mimeTypes: [],
+  options: {
+    shp: {
+      workerUrl: `https://unpkg.com/@loaders.gl/shp@${VERSION}/dist/shp-loader.worker.js`
+    }
+  }
+};
+
+/** @type {LoaderObject} */
+export const SHPLoader = {
+  ...SHPWorkerLoader,
+  parse: async (arrayBuffer, options) => parseSHP(arrayBuffer),
+  parseSync: parseSHP
+};
