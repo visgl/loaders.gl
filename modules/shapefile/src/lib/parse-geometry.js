16a8db118d71d0b8d5c16cb79c0f732d206b0608
Kyle Barron <kylebarron2@gmail.com>
2020-06-19 17:54:12 -0600

    Parse Z/M MultiPoint

--- modules/shapefile/src/lib/parse-geometry.js
+++ modules/shapefile/src/lib/parse-geometry.js
@@ -25,16 +25,16 @@ export function parseRecord(view) {
     //   return parsePoly(view, offset, 4); // PolyLineZ
     // case 15:
     //   return parsePoly(view, offset, 4); // PolygonZ
-    // case 18:
-    //   return parseMultiPoint(view, offset, 4); // MultiPointZ
+    case 18:
+      return parseMultiPoint(view, offset, 4); // MultiPointZ
     case 21:
       return parsePoint(view, offset, 3); // PointM
     // case 23:
     //   return parsePoly(view, offset, 3); // PolyLineM
     // case 25:
     //   return parsePoly(view, offset, 3); // PolygonM
-    // case 28:
-    //   return parseMultiPoint(view, offset, 3); // MultiPointM
+    case 28:
+      return parseMultiPoint(view, offset, 3); // MultiPointM
     default:
       throw new Error(`unsupported shape type: ${type}`);
   }
@@ -62,12 +62,28 @@ function parseMultiPoint(view, offset, dim) {
   offset += Int32Array.BYTES_PER_ELEMENT;
 
   let xyPositions;
+  let mPositions;
+  let zPositions;
   [xyPositions, offset] = parsePositions(view, offset, nPoints, 2);
 
-  // TODO: parse 3/4D, concat and interleave
+  // Parse Z coordinates
+  if (dim === 4) {
+    // skip parsing range
+    offset += 2 * Float64Array.BYTES_PER_ELEMENT;
+    [zPositions, offset] = parsePositions(view, offset, nPoints, 1);
+  }
+
+  // Parse M coordinates
+  if (dim >= 3) {
+    // skip parsing range
+    offset += 2 * Float64Array.BYTES_PER_ELEMENT;
+    [mPositions, offset] = parsePositions(view, offset, nPoints, 1);
+  }
+
+  const positions = concatPositions(xyPositions, mPositions, zPositions);
 
   return {
-    positions: {value: xyPositions, size: dim},
+    positions: {value: positions, size: dim},
     type: 'Point'
   };
 }
@@ -106,5 +122,51 @@ function parsePoly(view, offset, dim) {
 function parsePositions(view, offset, nPoints, dim) {
   const bufferOffset = view.byteOffset + offset;
   const bufferLength = nPoints * dim * Float64Array.BYTES_PER_ELEMENT;
-  return [new Float64Array(view.buffer.slice(bufferOffset, bufferOffset + bufferLength)), offset];
+  return [
+    new Float64Array(view.buffer.slice(bufferOffset, bufferOffset + bufferLength)),
+    offset + bufferLength
+  ];
+}
+
+// Concatenate and interleave positions arrays
+// xy positions are interleaved; mPositions, zPositions are their own arrays
+function concatPositions(xyPositions, mPositions, zPositions) {
+  if (!(mPositions || zPositions)) {
+    return xyPositions;
+  }
+
+  let arrayLength = xyPositions.length;
+  let nDim = 2;
+
+  if (zPositions && zPositions.length) {
+    arrayLength += zPositions.length;
+    nDim++;
+  }
+
+  if (mPositions && mPositions.length) {
+    arrayLength += mPositions.length;
+    nDim++;
+  }
+
+  const positions = new Float64Array(arrayLength);
+  for (let i = 0; i < xyPositions.length / 2; i++) {
+    positions[nDim * i] = xyPositions[i * 2];
+    positions[nDim * i + 1] = xyPositions[i * 2 + 1];
+  }
+
+  if (zPositions && zPositions.length) {
+    for (let i = 0; i < zPositions.length; i++) {
+      // If Z coordinates exist; used as third coord in positions array
+      positions[nDim * i + 2] = zPositions[i];
+    }
+  }
+
+  if (mPositions && mPositions.length) {
+    for (let i = 0; i < mPositions.length; i++) {
+      // M is always last, either 3rd or 4th depending on if Z exists
+      positions[nDim * i + (nDim - 1)] = mPositions[i];
+    }
+  }
+
+  return positions;
 }
