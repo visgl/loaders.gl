226f46929dcde51cc7e1ec5ae8331cde4128b20b
Kyle Barron <kylebarron2@gmail.com>
2020-06-24 15:47:56 -0600

    Cleanup

--- modules/shapefile/src/lib/parse-dbf.js
+++ modules/shapefile/src/lib/parse-dbf.js
@@ -1,44 +1,41 @@
-/* eslint-disable */
+/* global TextDecoder */
 
-var LITTLE_ENDIAN = true;
+const LITTLE_ENDIAN = true;
 
-function parseDbf(arrayBuffer) {
+export function parseDbf(arrayBuffer) {
   // Global header
-  var globalHeaderView = new DataView(arrayBuffer, 0, 32);
-  var header = parseHeader(globalHeaderView);
-  var {headerLength, recordLength, nRecords} = header;
+  const globalHeaderView = new DataView(arrayBuffer, 0, 32);
+  const header = parseHeader(globalHeaderView);
+  const {headerLength, recordLength, nRecords} = header;
 
   // Row headers
   // TODO: use code page (.cpg) for encoding. Default to ASCII or UTF-8?
-  var encoding = 'utf-8';
+  const encoding = 'utf-8';
   // Requires Node 11+
-  var textDecoder = new TextDecoder(encoding);
-  var colHeaderView = new DataView(arrayBuffer, 32, headerLength - 32);
-  var colHeaders = parseColumnHeaders(colHeaderView, textDecoder);
+  const textDecoder = new TextDecoder(encoding);
+  const colHeaderView = new DataView(arrayBuffer, 32, headerLength - 32);
+  const colHeaders = parseColumnHeaders(colHeaderView, textDecoder);
 
   // Not exactly sure why start offset is +1?
   // parsedbf uses ((colHeaders.length + 1) << 5) + 2;
-  var recordsView = new DataView(arrayBuffer, headerLength + 1);
+  const recordsView = new DataView(arrayBuffer, headerLength + 1);
   return parseRows(recordsView, colHeaders, nRecords, recordLength, textDecoder);
 }
 
 function parseHeader(headerView) {
   // Last updated date
-  var year = headerView.getUint8(1) + 1900;
-  var month = headerView.getUint8(2);
-  var day = headerView.getUint8(3);
+  const year = headerView.getUint8(1) + 1900;
+  const month = headerView.getUint8(2);
+  const day = headerView.getUint8(3);
 
   // Number of records in data file
-  var nRecords = headerView.getUint32(4, LITTLE_ENDIAN);
-
+  const nRecords = headerView.getUint32(4, LITTLE_ENDIAN);
   // Length of header in bytes
-  var headerLength = headerView.getUint16(8, LITTLE_ENDIAN);
-
+  const headerLength = headerView.getUint16(8, LITTLE_ENDIAN);
   // Length of each record
-  var recordLength = headerView.getUint16(10, LITTLE_ENDIAN);
-
+  const recordLength = headerView.getUint16(10, LITTLE_ENDIAN);
   // Not sure if this is usually set
-  var languageDriver = headerView.getUint8(29);
+  const languageDriver = headerView.getUint8(29);
 
   return {
     year,
@@ -46,32 +43,29 @@ function parseHeader(headerView) {
     day,
     nRecords,
     headerLength,
-    recordLength
+    recordLength,
+    languageDriver
   };
 }
 
 function parseColumnHeaders(view, textDecoder) {
   // NOTE: this might overestimate the number of fields if the "Database
   // Container" container exists and is included in the headerLength
-  var nFields = (view.byteLength - 1) / 32;
-  var offset = 0;
-
-  var fields = [];
-  for (var i = 0; i < nFields; i++) {
+  const nFields = (view.byteLength - 1) / 32;
+  const fields = [];
+  let offset = 0;
+  for (let i = 0; i < nFields; i++) {
     // Better way to read text?
-    var name = textDecoder
+    const name = textDecoder
       .decode(view.buffer.slice(view.byteOffset + offset, view.byteOffset + offset + 11))
+      // eslint-disable-next-line no-control-regex
       .replace(/\u0000/g, '');
-
-    var dataType = String.fromCharCode(view.getUint8(offset + 11));
-    var fieldLength = view.getUint8(offset + 16);
-    var decimal = view.getUint8(offset + 17);
 
     fields.push({
       name,
-      dataType,
-      fieldLength,
-      decimal
+      dataType: String.fromCharCode(view.getUint8(offset + 11)),
+      fieldLength: view.getUint8(offset + 16),
+      decimal: view.getUint8(offset + 17)
     });
     offset += 32;
   }
@@ -79,20 +73,20 @@ function parseColumnHeaders(view, textDecoder) {
 }
 
 function parseRows(view, fields, nRecords, recordLength, textDecoder) {
-  var rows = [];
-  for (var i = 0; i < nRecords; i++) {
-    var offset = i * recordLength;
-    var recordView = new DataView(view.buffer, view.byteOffset + offset, recordLength);
+  const rows = [];
+  for (let i = 0; i < nRecords; i++) {
+    const offset = i * recordLength;
+    const recordView = new DataView(view.buffer, view.byteOffset + offset, recordLength);
     rows.push(parseRow(recordView, fields, textDecoder));
   }
   return rows;
 }
 
 function parseRow(view, fields, textDecoder) {
-  var offset = 0;
-  var out = {};
-  for (var field of fields) {
-    var text = textDecoder.decode(
+  const out = {};
+  let offset = 0;
+  for (const field of fields) {
+    const text = textDecoder.decode(
       view.buffer.slice(view.byteOffset + offset, view.byteOffset + offset + field.fieldLength)
     );
     out[field.name] = parseField(text, field.dataType);
@@ -123,10 +117,15 @@ function parseField(text, dataType) {
   }
 }
 
+// Parse YYYYMMDD to date in milliseconds
 function parseDate(str) {
   return new Date(str.slice(0, 4), parseInt(str.slice(4, 6), 10) - 1, str.slice(6, 8)).getTime();
 }
 
+// Read boolean value
+// any of Y, y, T, t coerce to true
+// any of N, n, F, f coerce to false
+// otherwise null
 function readBoolean(value) {
   return /^[nf]$/i.test(value) ? false : /^[yt]$/i.test(value) ? true : null;
 }
