a277437eb6b0c71706ca74f7549947871ed9bcda
Kyle Barron <kylebarron2@gmail.com>
2020-06-25 15:46:52 -0600

    Return ArrayBuffer, not view from concatenateArrayBuffers (#820)

---
+++ modules/loader-utils/src/lib/iterator-utils/async-iteration.d.ts
@@ -0,0 +1,4 @@
+export function forEach(iterator, visitor);
+
+export function concatenateChunksAsync(asyncIterator: AsyncIterable<ArrayBuffer>): ArrayBuffer;
+export function concatenateChunksAsync(asyncIterator: AsyncIterable<string>): string;
