178847fbd8d95de5a974a031b4d03a58ce09a0c3
Kyle Barron <kylebarron2@gmail.com>
2020-06-25 15:24:29 -0600

    Fix typescript

--- modules/loader-utils/src/lib/iterator-utils/async-iteration.d.ts
+++ modules/loader-utils/src/lib/iterator-utils/async-iteration.d.ts
@@ -1,5 +1,4 @@
 export function forEach(iterator, visitor);
 
-export function concatenateChunksAsync(
-  asyncIterator: AsyncIterator<string | ArrayBuffer>
-): string | ArrayBuffer;
+export function concatenateChunksAsync(asyncIterator: AsyncIterable<ArrayBuffer>): ArrayBuffer;
+export function concatenateChunksAsync(asyncIterator: AsyncIterable<string>): string;
