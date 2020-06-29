439eaaace93dcc597deb3472a3cdae56ab347eb4
Kyle Barron <kylebarron2@gmail.com>
2020-06-29 11:58:41 -0600

    Switch to custom tests for binary to geojson

--- modules/gis/test/binary-to-geojson.spec.js
+++ modules/gis/test/binary-to-geojson.spec.js
@@ -1,16 +1,18 @@
+/* eslint-disable max-depth */
 import test from 'tape-promise/tape';
 import {fetchFile} from '@loaders.gl/core';
 import {binaryToGeoJson} from '@loaders.gl/gis';
 
-const WKB_2D_TEST_CASES = '@loaders.gl/wkt/test/data/wkb-testdata2d.json';
+const FEATURE_COLLECTION_TEST_CASES = '@loaders.gl/gis/test/data/featurecollection.json';
 
-test('binary-to-geojson 2D', async t => {
-  const response = await fetchFile(WKB_2D_TEST_CASES);
-  const TEST_CASES = parseTestCases(await response.json());
+test('binary-to-geojson feature collections', async t => {
+  const response = await fetchFile(FEATURE_COLLECTION_TEST_CASES);
+  const json = await response.json();
+  const TEST_CASES = parseTestCases(json);
 
   for (const testCase of Object.values(TEST_CASES)) {
     if (testCase.geoJSON && testCase.binary) {
-      t.deepEqual(binaryToGeoJson(testCase.binary), testCase.geoJSON);
+      t.deepEqual(binaryToGeoJson(testCase.binary), testCase.geoJSON.features);
     }
   }
 
@@ -19,23 +21,23 @@ test('binary-to-geojson 2D', async t => {
 
 // Mutates input object
 function parseTestCases(testCases) {
-  for (const [key, value] of Object.entries(testCases)) {
+  for (const value of Object.values(testCases)) {
     // Convert `binary`, an object with typed arrays output, into typed arrays
     // from regular arrays
     if (value.binary) {
-      if (value.binary.positions) {
-        value.binary.positions.value = new Float32Array(value.binary.positions.value);
-      }
-      if (value.binary.pathIndices) {
-        value.binary.pathIndices.value = new Uint16Array(value.binary.pathIndices.value);
-      }
-      if (value.binary.primitivePolygonIndices) {
-        value.binary.primitivePolygonIndices.value = new Uint16Array(
-          value.binary.primitivePolygonIndices.value
-        );
-      }
-      if (value.binary.polygonIndices) {
-        value.binary.polygonIndices.value = new Uint16Array(value.binary.polygonIndices.value);
+      for (const data of Object.values(value.binary)) {
+        if (data.positions) {
+          data.positions.value = new Float32Array(data.positions.value);
+        }
+        if (data.pathIndices) {
+          data.pathIndices.value = new Uint16Array(data.pathIndices.value);
+        }
+        if (data.primitivePolygonIndices) {
+          data.primitivePolygonIndices.value = new Uint16Array(data.primitivePolygonIndices.value);
+        }
+        if (data.polygonIndices) {
+          data.polygonIndices.value = new Uint16Array(data.polygonIndices.value);
+        }
       }
     }
   }
