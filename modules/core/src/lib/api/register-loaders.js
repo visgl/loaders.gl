3c78a591e535676409f0ce84be0b6864cecdc046
Ib Green <ib@unfolded.ai>
2020-06-29 08:57:32 -0700

    core: truly global loaders and options (#828)

--- modules/core/src/lib/api/register-loaders.js
+++ modules/core/src/lib/api/register-loaders.js
@@ -1,24 +1,34 @@
 import {normalizeLoader} from '../loader-utils/normalize-loader';
+import {getGlobalLoaderState} from '../loader-utils/merge-options';
 
-let registeredLoaders = [];
+// Store global registered loaders on the global object to increase chances of cross loaders-version interoperability
+// This use case is not reliable but can help when testing new versions of loaders.gl with existing frameworks
+const getGlobalLoaderRegistry = () => {
+  const state = getGlobalLoaderState();
+  state.loaderRegistry = state.loaderRegistry || [];
+  return state.loaderRegistry;
+};
 
 export function registerLoaders(loaders) {
+  const loaderRegistry = getGlobalLoaderRegistry();
+
   loaders = Array.isArray(loaders) ? loaders : [loaders];
 
   for (const loader of loaders) {
     const normalizedLoader = normalizeLoader(loader);
-    if (!registeredLoaders.find(registeredLoader => normalizedLoader === registeredLoader)) {
-      // add to the beginning of the registeredLoaders, so the last registeredLoader get picked
-      registeredLoaders.unshift(normalizedLoader);
+    if (!loaderRegistry.find(registeredLoader => normalizedLoader === registeredLoader)) {
+      // add to the beginning of the loaderRegistry, so the last registeredLoader get picked
+      loaderRegistry.unshift(normalizedLoader);
     }
   }
 }
 
 export function getRegisteredLoaders() {
-  return registeredLoaders;
+  return getGlobalLoaderRegistry();
 }
 
 // For testing
 export function _unregisterLoaders() {
-  registeredLoaders = [];
+  const state = getGlobalLoaderState();
+  state.loaderRegistry = [];
 }
