abbd5b7cc80bcf9c8a27ef3f18e1095215f6a23f
Ib Green <ib@unfolded.ai>
2020-07-01 21:51:45 -0700

    wip

--- modules/core/test/lib/load-in-batches.spec.js
+++ modules/core/test/lib/load-in-batches.spec.js
@@ -5,6 +5,23 @@ import {OBJLoader} from '@loaders.gl/obj';
 
 const OBJ_ASCII_URL = '@loaders.gl/obj/test/data/bunny.obj';
 
+test('loadInBatches#FileList', async t => {
+  if (isBrowser) {
+    const response = await fetchFile(OBJ_ASCII_URL);
+    const blob = await response.blob();
+
+    const iteratorPromises = await loadInBatches([blob, blob], OBJLoader);
+    for await (const iterator of iteratorPromises) {
+      for await (const batch of iterator) {
+        // Just the one batch...
+        t.equal(batch.mode, 4, 'mode is TRIANGLES (4)');
+      }
+    }
+  }
+
+  t.end();
+});
+
 test('loadInBatches#non-batched loader', async t => {
   const batches = await loadInBatches(OBJ_ASCII_URL, OBJLoader);
   for await (const batch of batches) {
--- modules/core/test/lib/parse-in-batches.spec.js
+++ modules/core/test/lib/parse-in-batches.spec.js
@@ -42,29 +42,3 @@ test('parseInBatches', async t => {
 
   t.end();
 });
-
-test('loadInBatches#non-batched loader', async t => {
-  const batches = await loadInBatches(OBJ_ASCII_URL, OBJLoader);
-  for await (const batch of batches) {
-    // Just the one batch...
-    t.equal(batch.mode, 4, 'mode is TRIANGLES (4)');
-  }
-  t.end();
-});
-
-test('loadInBatches#FileList', async t => {
-  if (isBrowser) {
-    const response = await fetchFile(OBJ_ASCII_URL);
-    const blob = await response.blob();
-
-    const iteratorPromises = await loadInBatches([blob, blob], OBJLoader);
-    for await (const iterator of iteratorPromises) {
-      for await (const batch of iterator) {
-        // Just the one batch...
-        t.equal(batch.mode, 4, 'mode is TRIANGLES (4)');
-      }
-    }
-  }
-
-  t.end();
-});
