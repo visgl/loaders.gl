03d2d1f6c614e2a5f10c07b51c7d8a774909eef2
Kyle Barron <kylebarron2@gmail.com>
2020-07-02 12:04:50 -0600

    Default dbf loader to windows-1252 (#838)

--- modules/shapefile/test/dbf/parse-dbf.spec.js
+++ modules/shapefile/test/dbf/parse-dbf.spec.js
@@ -23,10 +23,10 @@ test('Shapefile JS DBF tests', async t => {
   for (const testFileName of SHAPEFILE_JS_TEST_FILES) {
     let response = await fetchFile(`${SHAPEFILE_JS_DATA_FOLDER}/${testFileName}.dbf`);
     const body = await response.arrayBuffer();
-    const options = {worker: false, dbf: {encoding: 'utf8'}};
+    const options = {worker: false, dbf: {encoding: 'windows-1252'}};
 
-    if (testFileName === 'latin1-property') {
-      options.dbf.encoding = 'latin1';
+    if (testFileName === 'utf8-property') {
+      options.dbf.encoding = 'utf8';
     }
     const output = await parse(body, DBFLoader, options);
 
