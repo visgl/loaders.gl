bda06b28b4a940f9ca258013348ad3b1300f6eb2
Ib Green <ib@unfolded.ai>
2020-07-02 17:32:19 -0700

    shapefile: multifile loading

--- modules/shapefile/src/index.js
+++ modules/shapefile/src/index.js
@@ -1,2 +1,7 @@
+export {ShapefileLoader} from './shapefile-loader';
 export {DBFLoader, DBFWorkerLoader} from './dbf-loader';
 export {SHPLoader, SHPWorkerLoader} from './shp-loader';
+
+// EXPERIMENTAL
+export {default as _BrowserFileSystem} from './lib/filesystems/browser-filesystem';
+export {default as _NodeFileSystem} from './lib/filesystems/node-filesystem';
