72af35d6eaeba72bc53b09757ea1de799b8e0d9c
Ib Green <ib@unfolded.ai>
2020-07-06 16:48:38 -0700

    images: improved ImageWriter (#840)

--- modules/images/src/image-writer.js
+++ modules/images/src/image-writer.js
@@ -3,9 +3,11 @@ import {encodeImage} from './lib/encoders/encode-image';
 export default {
   name: 'Images',
   extensions: ['jpeg'],
-  // TODO encode is broken, fix...
-  encode: async (image, options) => await encodeImage(image, options.type),
   options: {
-    type: 'png'
-  }
+    image: {
+      mimeType: 'image/png',
+      jpegQuality: null
+    }
+  },
+  encode: encodeImage
 };
--- modules/images/src/lib/encoders/encode-image.d.ts
+++ modules/images/src/lib/encoders/encode-image.d.ts
@@ -2,8 +2,8 @@
  * Returns data bytes representing a compressed image in PNG or JPG format,
  * This data can be saved using file system (f) methods or used in a request.
  * @param image - ImageBitmap Image or Canvas
- * @param options 
+ * @param options
  * param opt.type='png' - png, jpg or image/png, image/jpg are valid
  * param mimeType= - Whether to include a data URI header
  */
-export function encodeImage(image: any, type?: string): string;
+export function encodeImage(image: any, options?: object): Promise<ArrayBuffer>;
