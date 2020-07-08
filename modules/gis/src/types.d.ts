54bfe148b249a6f2f0ac1cb1e2c55e99776cb862
Ib Green <ib@unfolded.ai>
2020-07-08 08:44:31 -0700

    shapefile/gis: integrate binaryToGeoJson (#846)

---
+++ modules/gis/src/types.d.ts
@@ -0,0 +1,22 @@
+type BinaryAttribute = {value: any[], size: number}
+
+export type BinaryGeometryData = {
+  points?: {
+    positions: BinaryAttribute;
+    featureIds: BinaryAttribute;
+    globalFeatureIds: BinaryAttribute;
+  };
+  lines?: {
+    positions: BinaryAttribute;
+    pathIndices: BinaryAttribute;
+    featureIds: BinaryAttribute;
+    globalFeatureIds: BinaryAttribute;
+  };
+  polygons?: {
+    positions: BinaryAttribute;
+    polygonIndices: BinaryAttribute;
+    primitivePolygonIndices: BinaryAttribute;
+    featureIds: BinaryAttribute;
+    globalFeatureIds: BinaryAttribute;
+  };
+};
