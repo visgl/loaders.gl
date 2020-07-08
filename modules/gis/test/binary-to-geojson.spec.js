af017b960ec21d0bedee0e460c01dd03179669f9
Kyle Barron <kylebarron2@gmail.com>
2020-07-08 08:05:36 -0600

    gis: Binary to geojson converter (#824)

---
+++ modules/gis/test/binary-to-geojson.spec.js
@@ -0,0 +1,51 @@
+/* eslint-disable max-depth */
+import test from 'tape-promise/tape';
+import {fetchFile} from '@loaders.gl/core';
+import {binaryToGeoJson} from '@loaders.gl/gis';
+
+const FEATURE_COLLECTION_TEST_CASES = '@loaders.gl/gis/test/data/featurecollection.json';
+
+test('binary-to-geojson feature collections', async t => {
+  const response = await fetchFile(FEATURE_COLLECTION_TEST_CASES);
+  const json = await response.json();
+  const TEST_CASES = parseTestCases(json);
+
+  for (const [key, testCase] of Object.entries(TEST_CASES)) {
+    // Skip polygons test until working MultiPolygon parsing
+    if (key === 'polygons') {
+      // eslint-disable-next-line
+      continue;
+    }
+
+    if (testCase.geoJSON && testCase.binary) {
+      t.deepEqual(binaryToGeoJson(testCase.binary), testCase.geoJSON.features);
+    }
+  }
+
+  t.end();
+});
+
+// Mutates input object
+function parseTestCases(testCases) {
+  for (const value of Object.values(testCases)) {
+    // Convert `binary`, an object with typed arrays output, into typed arrays
+    // from regular arrays
+    if (value.binary) {
+      for (const data of Object.values(value.binary)) {
+        if (data.positions) {
+          data.positions.value = new Float32Array(data.positions.value);
+        }
+        if (data.pathIndices) {
+          data.pathIndices.value = new Uint16Array(data.pathIndices.value);
+        }
+        if (data.primitivePolygonIndices) {
+          data.primitivePolygonIndices.value = new Uint16Array(data.primitivePolygonIndices.value);
+        }
+        if (data.polygonIndices) {
+          data.polygonIndices.value = new Uint16Array(data.polygonIndices.value);
+        }
+      }
+    }
+  }
+  return testCases;
+}
