b594d3a196645717ac333324df8850c4995c41d5
Ib Green <ib@unfolded.ai>
2020-07-09 08:08:23 -0700

    Reorganize exports

--- modules/core/src/index.js
+++ modules/core/src/index.js
@@ -20,7 +20,35 @@ export {loadInBatches} from './lib/api/load-in-batches';
 export {encode, encodeSync, encodeInBatches} from './lib/api/encode';
 export {save, saveSync} from './lib/api/save';
 
-// "JAVASCRIPT" UTILS
+// ITERATOR UTILS
+export {makeIterator} from './iterator-utils/make-iterator/make-iterator';
+
+// CORE UTILS SHARED WITH LOADERS (RE-EXPORTED FROM LOADER-UTILS)
+export {setPathPrefix, getPathPrefix, resolvePath} from '@loaders.gl/loader-utils';
+export {RequestScheduler} from '@loaders.gl/loader-utils';
+
+// EXPERIMENTAL
+export {default as _fetchProgress} from './lib/progress/fetch-progress';
+
+// FOR TESTING
+export {_unregisterLoaders} from './lib/api/register-loaders';
+
+//
+// TODO - MOVE TO LOADER-UTILS AND DEPRECATE IN CORE?
+//
+
+export {isBrowser, isWorker, self, window, global, document} from '@loaders.gl/loader-utils';
+export {assert} from '@loaders.gl/loader-utils';
+export {forEach, concatenateChunksAsync} from '@loaders.gl/loader-utils';
+
+export {
+  makeTextDecoderIterator,
+  makeTextEncoderIterator,
+  makeLineIterator,
+  makeNumberedLineIterator
+} from '@loaders.gl/loader-utils';
+
+// "JAVASCRIPT" UTILS - move to loader-utils?
 export {
   isPromise,
   isIterable,
@@ -30,26 +58,3 @@ export {
   isReadableStream,
   isWritableStream
 } from './javascript-utils/is-type';
-
-// ITERATOR UTILS
-export {makeIterator} from './iterator-utils/make-iterator/make-iterator';
-
-export {
-  makeTextDecoderIterator,
-  makeTextEncoderIterator,
-  makeLineIterator,
-  makeNumberedLineIterator
-} from '@loaders.gl/loader-utils';
-
-// CORE UTILS SHARED WITH LOADERS (RE-EXPORTED FROM LOADER-UTILS)
-export {isBrowser, isWorker, self, window, global, document} from '@loaders.gl/loader-utils';
-export {assert} from '@loaders.gl/loader-utils';
-export {setPathPrefix, getPathPrefix, resolvePath} from '@loaders.gl/loader-utils';
-export {RequestScheduler} from '@loaders.gl/loader-utils';
-export {forEach, concatenateChunksAsync} from '@loaders.gl/loader-utils';
-
-// EXPERIMENTAL
-export {default as _fetchProgress} from './lib/progress/fetch-progress';
-
-// FOR TESTING
-export {_unregisterLoaders} from './lib/api/register-loaders';
