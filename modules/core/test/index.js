6441166b3d4597e25dc145fe17c49e0aab395697
Ib Green <ib@unfolded.ai>
2020-07-09 11:38:46 -0700

    core: Reorganize exports (#850)

--- modules/core/test/index.js
+++ modules/core/test/index.js
@@ -4,14 +4,18 @@ import './javascript-utils/text-encoder.spec';
 import './lib/utils/mime-type-utils.spec';
 import './lib/utils/resource-utils.spec';
 
-import './lib/fetch';
 import './lib/loader-utils';
-import './lib/progress/fetch-progress.spec';
 
-import './lib/set-loader-options.spec';
-import './lib/register-loaders.spec';
-import './lib/select-loader.spec';
-import './lib/parse.spec';
-import './lib/load.spec';
-import './lib/parse-in-batches.spec';
-import './lib/load-in-batches.spec';
+import './lib/fetch';
+
+import './lib/api/set-loader-options.spec';
+import './lib/api/register-loaders.spec';
+import './lib/api/select-loader.spec';
+import './lib/api/parse.spec';
+import './lib/api/load.spec';
+import './lib/api/parse-in-batches.spec';
+import './lib/api/load-in-batches.spec';
+
+// EXPERIMENTAL
+
+import './lib/progress/fetch-progress.spec';
