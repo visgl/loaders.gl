6004b5afb6740f653c52675aaa921fb609c7ae19
Victor Belomestnov <belom88@yandex.ru>
2020-07-18 20:56:59 +0300

    cli: i3s-server

---
+++ modules/cli/src/i3s-server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
