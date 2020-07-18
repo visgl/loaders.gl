35818f12dfd6f20ca57c32c3257ec130b26f3cb8
Victor Belomestnov <belom88@yandex.ru>
2020-07-19 00:29:23 +0300

    cli: i3s-server cors support

--- modules/cli/src/i3s-server/app.js
+++ modules/cli/src/i3s-server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -10,6 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
