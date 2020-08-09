cb0ac43d23bd72a69780d555500e7e01cf92f384
Ib Green <ib@unfolded.ai>
2020-08-09 08:15:43 -0700

    Add draco compression tests

--- modules/cli/test/index.js
+++ modules/cli/test/index.js
@@ -1,3 +1,5 @@
+import './i3s-converter/helpers/draco-compression.spec';
 import './i3s-converter/helpers/node-pages.spec';
 import './i3s-converter/helpers/json-templates.spec';
+
 import './i3s-converter/i3s-converter.spec';
