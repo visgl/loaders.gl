6eccc22a7e729554f80c0ac2a864150f07d7fe44
Ib Green <ib@unfolded.ai>
2020-09-13 09:34:18 -0700

    fixes

--- modules/core/src/lib/api/load.d.ts
+++ modules/core/src/lib/api/load.d.ts
@@ -1,4 +1,4 @@
-import {DataType, LoaderObject} from '@loaders.gl/loader-utils';
+import {DataType, WorkerLoaderObject} from '@loaders.gl/loader-utils';
 /**
  * Parses `data` using a specified loader
  * @param data
@@ -6,4 +6,4 @@ import {DataType, LoaderObject} from '@loaders.gl/loader-utils';
  * @param options
  * @param context
  */
-export function load(url: string | DataType, loaders: LoaderObject | LoaderObject[], options?: object, context?: object): Promise<any>;
+export function load(url: string | DataType, loaders: WorkerLoaderObject | WorkerLoaderObject[], options?: object, context?: object): Promise<any>;
