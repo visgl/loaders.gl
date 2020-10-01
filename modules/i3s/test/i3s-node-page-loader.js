acec62a54886809d323eb721c37ce83989c9f461
Victor Belomestnov <belom88@yandex.ru>
2020-10-01 12:33:13 +0300

    i3s: handle nodepages
    - fixes PR comments

---
+++ modules/i3s/test/i3s-node-page-loader.js
@@ -0,0 +1,16 @@
+import test from 'tape-promise/tape';
+import {parse, fetchFile} from '@loaders.gl/core';
+import I3SNodePageLoader from '../src/i3s-node-page-loader';
+
+const NODEPAGE_URL =
+  '@loaders.gl/i3s/test/data/SanFrancisco_3DObjects_1_7/SceneServer/layers/0/nodepages/0';
+
+test('I3SNodePageLoader#Load node page', async t => {
+  const response = await fetchFile(NODEPAGE_URL);
+  const nodePage = await parse(response, I3SNodePageLoader);
+  t.ok(nodePage);
+  t.ok(nodePage.nodes);
+  t.equal(nodePage.nodes.length, 16);
+  t.equal(nodePage.nodes[2].lodThreshold, 870638.071285568);
+  t.end();
+});
--- modules/i3s/test/index.js
+++ modules/i3s/test/index.js
@@ -1,1 +1,3 @@
 import './helpers/i3s-nodepages-tiles.spec';
+
+import './i3s-node-page-loader';
