5f73043603919d6b4b93deec68f6b40689a86fe2
Victor Belomestnov <belom88@yandex.ru>
2020-10-13 19:23:40 +0300

    cli: i3s obb to 3d-tiles obb - JDoc comments (#110)

--- modules/cli/src/3d-tiles-converter/helpers/i3s-obb-to-3d-tiles-obb.d.ts
+++ modules/cli/src/3d-tiles-converter/helpers/i3s-obb-to-3d-tiles-obb.d.ts
@@ -1,6 +1,10 @@
-
+/**
+ * Convert quaternion-based OBB to half-axes-based OBB
+ * @param i3SObb quaternion based OBB
+ * @returns number[12] 3DTiles OBB https://github.com/CesiumGS/3d-tiles/tree/master/specification#box
+ */
 export function i3sObbTo3dTilesObb(i3SObb: {
   center: number[];
   halfSize: number[];
   quaternion: number[];
-});
+}): number[];
