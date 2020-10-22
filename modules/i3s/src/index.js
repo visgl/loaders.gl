2501328cf53896fe913f390471c669b2d59b18e7
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2020-10-22 14:43:22 +0300

    Changed attribute loader logic (#984)
    * Changed attribute loader logic
    
    * Fixed comments

--- modules/i3s/src/index.js
+++ modules/i3s/src/index.js
@@ -1,1 +1,2 @@
 export {default as I3SLoader} from './i3s-loader';
+export {default as I3SAttributeLoader} from './i3s-attribute-loader';
--- modules/i3s/test/index.js
+++ modules/i3s/test/index.js
@@ -2,4 +2,4 @@ import './helpers/i3s-nodepages-tiles.spec';
 
 import './i3s-loader.spec';
 import './i3s-node-page-loader.spec';
-import './i3s-attributes-loader.spec';
+import './i3s-attribute-loader.spec';
