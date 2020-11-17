4c045b945857ae06ac7d8752ce167d71c99cebd5
Kyle Barron <kylebarron2@gmail.com>
2020-11-16 17:58:43 -0700

    Fix shapefile batch parsing when options.metadata: true

--- modules/shapefile/src/lib/parsers/parse-shapefile.js
+++ modules/shapefile/src/lib/parsers/parse-shapefile.js
@@ -5,26 +5,44 @@ import {zipBatchIterators} from '../streaming/zip-batch-iterators';
 import {SHPLoader} from '../../shp-loader';
 import {DBFLoader} from '../../dbf-loader';
 
+// eslint-disable-next-line max-statements
 export async function* parseShapefileInBatches(asyncIterator, options, context) {
+  const {metadata} = options;
   const {reproject = false, _targetCrs = 'WGS84'} = (options && options.gis) || {};
   const {parseInBatches, fetch, url} = context;
   const {shx, cpg, prj} = await loadShapefileSidecarFiles(options, context);
 
   // parse geometries
-  const shapeIterator = await parseInBatches(asyncIterator, SHPLoader, options); // {shp: shx}
-  const shapeHeader = (await shapeIterator.next()).value;
+  const shapeIterator = await parseInBatches(asyncIterator, SHPLoader, options);
 
   // parse properties
   let propertyIterator;
-  let iterator;
   const dbfResponse = await fetch(replaceExtension(url, 'dbf'));
   if (dbfResponse.ok) {
     propertyIterator = await parseInBatches(dbfResponse, DBFLoader, {
+      ...options,
       dbf: {encoding: cpg || 'latin1'}
     });
+  }
+
+  // When `options.metadata` is `true`, there's an extra initial `metadata`
+  // object before the iterator starts. zipBatchIterators expects to receive
+  // batches of Array objects, and will fail with non-iterable batches, so it's
+  // important to skip over the first batch.
+  if (metadata) {
+    await shapeIterator.next();
+    if (propertyIterator) {
+      await propertyIterator.next();
+    }
+  }
+
+  // Shapefile has an extra metadata batch when loaded in batches
+  const shapeHeader = (await shapeIterator.next()).value;
+
+  let iterator;
+  if (propertyIterator) {
     iterator = await zipBatchIterators(shapeIterator, propertyIterator);
   } else {
-    // Ignore properties
     iterator = shapeIterator;
   }
 
