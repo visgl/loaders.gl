ad7ddc0ccfbbf456a37db4ea1ce774fc3b375286
Kyle Barron <kylebarron2@gmail.com>
2020-11-24 18:51:25 -0700

    Update flatgeobuf tests

--- modules/flatgeobuf/test/flatgeobuf-loader.spec.js
+++ modules/flatgeobuf/test/flatgeobuf-loader.spec.js
@@ -1,6 +1,6 @@
 import test from 'tape-promise/tape';
 import {FlatGeobufLoader} from '@loaders.gl/flatgeobuf';
-import {setLoaderOptions, fetchFile, parse, parseInBatches} from '@loaders.gl/core';
+import {setLoaderOptions, load, loadInBatches} from '@loaders.gl/core';
 
 const FLATGEOBUF_COUNTRIES_DATA_URL = '@loaders.gl/flatgeobuf/test/data/countries.fgb';
 
@@ -10,22 +10,24 @@ setLoaderOptions({
   }
 });
 
-test('Load FlatGeobuf file', async t => {
-  const response = await fetchFile(FLATGEOBUF_COUNTRIES_DATA_URL);
-  const fgbBuffer = await response.arrayBuffer();
-  const features = await parse(fgbBuffer, FlatGeobufLoader);
+test('FlatGeobufLoader#load', async t => {
+  const features = await load(FLATGEOBUF_COUNTRIES_DATA_URL, FlatGeobufLoader, {worker: false});
   t.equal(features.length, 179);
   t.end();
 });
 
-test('Load FlatGeobuf file streaming', async t => {
-  const response = await fetchFile(FLATGEOBUF_COUNTRIES_DATA_URL);
-  const iterator = await parseInBatches(response, FlatGeobufLoader);
+test('FlatGeobufLoader#loadInBatches', async t => {
+  const iterator = await loadInBatches(FLATGEOBUF_COUNTRIES_DATA_URL, FlatGeobufLoader, {
+    worker: false
+  });
   t.ok(iterator);
+
   const features = [];
   for await (const feature of iterator) {
     features.push(feature);
   }
+
   t.ok(features.length, 179);
   t.end();
 });
+
