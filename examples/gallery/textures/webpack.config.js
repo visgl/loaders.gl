a398ede42ea2a2d21f4b6f60d9c662c3c0f599b9
Daniil <daniil.ryabinin@actionengine.com>
2020-12-18 14:13:19 +0300

    Added texture testing tool

---
+++ examples/gallery/textures/webpack.config.js
@@ -0,0 +1,19 @@
+const {resolve} = require('path');
+
+const CONFIG = {
+  mode: 'development',
+
+  entry: {
+    app: resolve('./app.js')
+  },
+
+  node: {
+    fs: 'empty'
+  },
+  devServer: {
+    contentBase: [resolve('.'), resolve('../../'), resolve('../../../')]
+  }
+};
+
+// This line enables bundling against src in this repo rather than installed module
+module.exports = env => (env ? require('../../webpack.config.local')(CONFIG)(env) : CONFIG);
