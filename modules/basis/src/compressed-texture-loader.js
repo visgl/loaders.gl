55f107db92330d3c3febd0a87ce528519fb8abcb
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2020-12-24 14:50:27 +0300

    Refactoring of texture-tester app (#1068)
    * Refactoring of texture-tester app
    
    * Fixed comments

--- modules/basis/src/compressed-texture-loader.js
+++ modules/basis/src/compressed-texture-loader.js
@@ -1,6 +1,8 @@
 /** @typedef {import('@loaders.gl/loader-utils').LoaderObject} LoaderObject */
 /** @typedef {import('@loaders.gl/loader-utils').WorkerLoaderObject} WorkerLoaderObject */
 
+// eslint-disable-next-line import/no-unresolved
+import {parseCompressedTexture} from '@loaders.gl/basis/lib/parsers/parse-compressed-texture';
 // __VERSION__ is injected by babel-plugin-version-inline
 // @ts-ignore TS2304: Cannot find name '__VERSION__'.
 const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
@@ -30,5 +32,5 @@ export const CompressedTextureWorkerLoader = {
 /** @type {LoaderObject} */
 export const CompressedTextureLoader = {
   ...CompressedTextureWorkerLoader,
-  parse: async (arrayBuffer, options) => arrayBuffer
+  parse: async (arrayBuffer, options) => parseCompressedTexture(arrayBuffer)
 };
