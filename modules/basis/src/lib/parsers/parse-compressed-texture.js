8a58c78cd6097f423743f8195eab5cc5534d644a
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2020-12-28 17:07:30 +0300

    Added WEBGL_compressed_texture_etc1 support for for compressed texture loader (#1072)

--- modules/basis/src/lib/parsers/parse-compressed-texture.js
+++ modules/basis/src/lib/parsers/parse-compressed-texture.js
@@ -1,5 +1,7 @@
 import {parsePvr} from './parse-pvr';
 
+const HEADER_LENGTH = 13;
+
 export function parseCompressedTexture(data) {
   if (isPvr(data)) {
     return parsePvr(data);
@@ -7,8 +9,9 @@ export function parseCompressedTexture(data) {
   return null;
 }
 function isPvr(data) {
-  const array = new Uint32Array(data);
-  const version = array[0];
+  const header = new Uint32Array(data, 0, HEADER_LENGTH);
+  const version = header[0];
+
   if (version === 0x03525650 || version === 0x50565203) {
     return true;
   }
