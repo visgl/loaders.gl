6e67c459752363276981e1d484793896e22fd78e
Victor Belomestnov <belom88@yandex.ru>
2020-12-29 12:56:14 +0300

    textures: dds parser

--- modules/basis/src/lib/parsers/parse-compressed-texture.js
+++ modules/basis/src/lib/parsers/parse-compressed-texture.js
@@ -1,19 +1,25 @@
-import {parsePvr} from './parse-pvr';
-
-const HEADER_LENGTH = 13;
+import {parsePvr, PVR_CONSTANTS} from './parse-pvr';
+import {parseDds, DDS_CONSTANTS} from './parse-dds';
 
 export function parseCompressedTexture(data) {
   if (isPvr(data)) {
     return parsePvr(data);
+  } else if (isDds(data)) {
+    return parseDds(data);
   }
   return null;
 }
-function isPvr(data) {
-  const header = new Uint32Array(data, 0, HEADER_LENGTH);
-  const version = header[0];
 
-  if (version === 0x03525650 || version === 0x50565203) {
-    return true;
-  }
-  return false;
+function isPvr(data) {
+  const header = new Uint32Array(data, 0, PVR_CONSTANTS.HEADER_LENGTH);
+  const version = header[PVR_CONSTANTS.MAGIC_NUMBER_INDEX];
+
+  return version === PVR_CONSTANTS.MAGIC_NUMBER || version === PVR_CONSTANTS.MAGIC_NUMBER_EXTRA;
+}
+
+function isDds(data) {
+  const header = new Uint32Array(data, 0, DDS_CONSTANTS.HEADER_LENGTH);
+  const magic = header[DDS_CONSTANTS.MAGIC_NUMBER_INDEX];
+
+  return magic === DDS_CONSTANTS.MAGIC_NUMBER;
 }
