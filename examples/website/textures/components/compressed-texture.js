0beaf90f8293a0bf3085724991bd5e55a86d0437
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2021-01-15 10:54:48 +0300

    FIx for uncompressed image textures (#1102)

--- examples/website/textures/components/compressed-texture.js
+++ examples/website/textures/components/compressed-texture.js
@@ -246,7 +246,7 @@ export default class CompressedTexture extends PureComponent {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 8);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
     const uploadTime = Date.now() - startTime;
 
