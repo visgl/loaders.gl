d737c695d13e51567fb75d414a66d3e0f75ccb63
Kyle Barron <kylebarron2@gmail.com>
2021-01-18 13:56:48 -0700

    Fix tests

--- modules/textures/test/npy-loader.spec.js
+++ modules/textures/test/npy-loader.spec.js
@@ -1,7 +1,7 @@
 import test from 'tape-promise/tape';
 import {validateLoader} from 'test/common/conformance';
 
-import {_NPYLoader, _NPYWorkerLoader} from '@loaders.gl/textures';
+import {NPYLoader, NPYWorkerLoader} from '@loaders.gl/textures';
 import {setLoaderOptions, load} from '@loaders.gl/core';
 
 const NPY_UINT8_URL = '@loaders.gl/textures/test/data/uint8.npy';
@@ -13,14 +13,14 @@ setLoaderOptions({
 });
 
 test('NPYLoader#loader objects', async t => {
-  validateLoader(t, _NPYLoader, 'NPYLoader');
-  validateLoader(t, _NPYWorkerLoader, 'NPYWorkerLoader');
+  validateLoader(t, NPYLoader, 'NPYLoader');
+  validateLoader(t, NPYWorkerLoader, 'NPYWorkerLoader');
   t.end();
 });
 
 test('NPYLoader#parse', async t => {
   const options = {};
-  const {data, header} = await load(NPY_UINT8_URL, _NPYLoader, options);
+  const {data, header} = await load(NPY_UINT8_URL, NPYLoader, options);
 
   const expectedData = new Uint8Array([1, 2, 3, 4]);
   // eslint-disable-next-line camelcase
@@ -39,7 +39,7 @@ test('NPYWorkerLoader#parse', async t => {
   }
 
   const options = {};
-  const {data, header} = await load(NPY_UINT8_URL, _NPYWorkerLoader, options);
+  const {data, header} = await load(NPY_UINT8_URL, NPYWorkerLoader, options);
 
   const expectedData = new Uint8Array([1, 2, 3, 4]);
   // eslint-disable-next-line camelcase
