1a88f16a242e47f87a04aa7feee77a895056b655
Ib Green <ib@unfolded.ai>
2021-02-03 17:40:25 -0800

    scripts

--- modules/compression/src/index.d.ts
+++ modules/compression/src/index.d.ts
@@ -1,5 +1,3 @@
-import {WorkerObject} from '@loaders.gl/loader-utils';
-
 // Zlib (via Pako)
 export {default as ZlibDeflateTransform} from './lib/zlib/zlib-deflate-transform';
 export {default as ZlibInflateTransform} from './lib/zlib/zlib-inflate-transform';
@@ -11,5 +9,3 @@ export {default as LZ4InflateTransform} from './lib/lz4/lz4-inflate-transform';
 // Zstd
 // export {default as ZstdDeflateTransform} from './lib/zstd/zstd-deflate-transform';
 // export {default as ZstdInflateTransform} from './lib/zstd/zstd-inflate-transform';
-
-export const ZlibWorker: WorkerObject;
--- modules/compression/src/index.js
+++ modules/compression/src/index.js
@@ -1,5 +1,3 @@
-/** @typedef {import('@loaders.gl/loader-utils').WorkerObject} WorkerObject */
-
 export {default as ZlibDeflateTransform} from './lib/zlib/zlib-deflate-transform';
 export {default as ZlibInflateTransform} from './lib/zlib/zlib-inflate-transform';
 
@@ -9,19 +7,3 @@ export {default as LZ4InflateTransform} from './lib/lz4/lz4-inflate-transform';
 // Move to WIP due to enormous dependency size
 // export {default as ZstdDeflateTransform} from './lib/zstd/zstd-deflate-transform';
 // export {default as ZstdInflateTransform} from './lib/zstd/zstd-inflate-transform';
-
-// __VERSION__ is injected by babel-plugin-version-inline
-// @ts-ignore TS2304: Cannot find name '__VERSION__'.
-const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
-
-export const ZlibWorker = {
-  id: 'zlib',
-  name: 'zlib',
-  version: VERSION,
-  options: {
-    zlib: {
-      localWorkerUrl: 'modules/compression/dist/zlib.worker.js',
-      workerUrl: `https://unpkg.com/@loaders.gl/compression@${VERSION}/dist/zlip.worker.js`
-    }
-  }
-};
