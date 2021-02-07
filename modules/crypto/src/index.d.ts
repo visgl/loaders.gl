63d661251b55e810adb1fc48eb0e3e503186336b
Ib Green <ib@unfolded.ai>
2021-02-07 11:28:17 -0800

    fixes

--- modules/crypto/src/index.d.ts
+++ modules/crypto/src/index.d.ts
@@ -1,5 +1,3 @@
-import {WorkerObject} from '@loaders.gl/loader-utils';
-
 // Cryptographic hash
 export {default as CryptoHashTransform} from './lib/crypto/crypto-hash-transform';
 export {default as CRC32HashTransform} from './lib/crc32/crc32-hash-transform';
@@ -7,5 +5,3 @@ export {default as CRC32CHashTransform} from './lib/crc32c/crc32c-hash-transform
 export {default as MD5HashTransform} from './lib/md5-wasm/md5-hash-transform';
 
 export {hexToBase64 as _hexToBase64, toHex as _toHex} from './lib/utils/digest-utils';
-
-export const CRC32Worker: WorkerObject;
