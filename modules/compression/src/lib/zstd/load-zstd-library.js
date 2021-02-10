8e5203eac094fab3e246d72e55d042122d12f124
Ib Green <ib@unfolded.ai>
2021-02-10 10:02:10 -0800

    fix

--- modules/compression/src/lib/zstd/load-zstd-library.js
+++ modules/compression/src/lib/zstd/load-zstd-library.js
@@ -1,6 +1,6 @@
 import {assert} from '@loaders.gl/loader-utils';
 
-const ERR_ZSTD_LIBRARY_NOT_SUPPLIED = 'zstd-codec lib must be supplied in options.module.CryptoJS';
+const ERR_ZSTD_LIBRARY_NOT_SUPPLIED = `zstd-codec lib must be supplied in options.module['zstd-codec']`;
 
 /**
  * zstd-codec is an injectable dependency due to big size
