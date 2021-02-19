89dc803772a49a12c52413c7d93ea3015860cf0f
Ib Green <ib@unfolded.ai>
2021-02-19 08:32:09 -0800

    chore(examples): Add shader overrides to i3s-picking

--- examples/experimental/i3s-picking/mesh-layer/mesh-layer.js
+++ examples/experimental/i3s-picking/mesh-layer/mesh-layer.js
@@ -1,3 +1,11 @@
 import {SimpleMeshLayer} from '@deck.gl/mesh-layers';
 
-export default class MeshLayer extends SimpleMeshLayer {}
+import vs from './simple-mesh-layer-vertex.glsl';
+import fs from './simple-mesh-layer-fragment.glsl';
+
+export default class MeshLayer extends SimpleMeshLayer {
+  getShaders() {
+    const shaders = super.getShaders();
+    return {...shaders, vs, fs};
+  }
+}
