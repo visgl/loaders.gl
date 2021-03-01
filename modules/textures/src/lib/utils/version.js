7d1564f3e74784bae3c93c39fc6aa0339f908d99
Victor Belomestnov <belom88@yandex.ru>
2021-03-01 20:36:14 +0300

    texture: load worker from "beta" (#1223)

--- modules/textures/src/lib/utils/version.js
+++ modules/textures/src/lib/utils/version.js
@@ -1,4 +1,5 @@
 // Version constant cannot be imported, it needs to correspond to the build version of **this** module.
 // __VERSION__ is injected by babel-plugin-version-inline
+// TODO: use 'latest' instead of 'beta' when 3.0.0 version is released as 'latest'
 // @ts-ignore TS2304: Cannot find name '__VERSION__'.
-export const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
+export const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'beta';
