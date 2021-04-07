2a87e7049e37471fbc6fdecf2f69f44678d83b2a
Victor Belomestnov <belom88@yandex.ru>
2021-04-07 14:41:53 +0300

    i3s: support texture atlas (#1282)

--- examples/experimental/i3s-17-and-debug/tile-layer/tile-layer.js
+++ examples/experimental/i3s-17-and-debug/tile-layer/tile-layer.js
@@ -108,6 +108,9 @@ function getMeshGeometry(contentAttributes) {
   if (contentAttributes.texCoords) {
     attributes.texCoords = contentAttributes.texCoords;
   }
+  if (contentAttributes.uvRegions) {
+    attributes.uvRegions = contentAttributes.uvRegions;
+  }
   if (contentAttributes.colors) {
     attributes.colors = contentAttributes.colors;
   }
