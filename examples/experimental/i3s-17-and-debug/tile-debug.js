e79908271803c02fdece212577f311d11d286adb
Victor Belomestnov <belom88@yandex.ru>
2021-04-13 19:03:34 +0300

    i3s-debug: LOD validation (#1312)

--- examples/experimental/i3s-17-and-debug/tile-debug.js
+++ examples/experimental/i3s-17-and-debug/tile-debug.js
@@ -1,6 +1,6 @@
 import {OrientedBoundingBox, BoundingSphere} from '@math.gl/culling';
 import {CubeGeometry} from '@luma.gl/engine';
-import {BOUNDING_VOLUME_WARNING_TYPE, LOD_WARNING_TYPE} from './constants';
+import {BOUNDING_VOLUME_WARNING_TYPE, LOD_WARNING_TYPE, PARENT_LOD_WARNING_TYPE} from './constants';
 import {Vector3} from 'math.gl';
 import {Ellipsoid} from '@math.gl/geospatial';
 
@@ -224,16 +224,32 @@ function createBoundingBoxFromTileObb(obb) {
 
 // LOD spec https://github.com/Esri/i3s-spec/blob/master/format/LevelofDetail.md
 function checkLOD(tile, tileWarnings) {
-  const parentLod = tile.parent && tile.parent.lodMetricValue;
+  const divergence = 0.05;
+  const tileLodRatio = tile.lodMetricValue / tile.boundingVolume.radius;
+  const parentLodRatio = tile.parent.lodMetricValue / tile.parent.boundingVolume.radius;
+  const lodRatios = tile.parent.children.map(child => {
+    return child.lodMetricValue / child.boundingVolume.radius;
+  });
+  const meanRatio = lodRatios.reduce((accum, current) => accum + current, 0) / lodRatios.length;
 
-  if (!parentLod || tile.lodMetricValue > parentLod) {
-    return;
+  if (
+    meanRatio < parentLodRatio &&
+    !tileWarnings.find(
+      warning => warning.tileId === tile.parent.id && warning.type === PARENT_LOD_WARNING_TYPE
+    )
+  ) {
+    const title = `Tile (${
+      tile.parent.id
+    }) LOD/Radius ratio "${parentLodRatio}" > mean child LOD/Radius ratio "${meanRatio}"`;
+    tileWarnings.push({type: PARENT_LOD_WARNING_TYPE, title, tileId: tile.parent.id});
   }
 
-  const title = `Tile (${tile.id}) LOD = "${tile.lodMetricValue}" < Parent (${
-    tile.parent.id
-  }) tile LOD = "${tile.parent.lodMetricValue}"`;
-  tileWarnings.push({type: LOD_WARNING_TYPE, title});
+  if (Math.abs(tileLodRatio - meanRatio) > divergence) {
+    const title = `Tile (${
+      tile.id
+    }) LOD/Radius ratio "${tileLodRatio}" has large deviation from mean LOD/Radius ratio of neighbors "${meanRatio}"`;
+    tileWarnings.push({type: LOD_WARNING_TYPE, title, tileId: tile.parent.id});
+  }
 }
 
 function getTextureSize(tile) {
