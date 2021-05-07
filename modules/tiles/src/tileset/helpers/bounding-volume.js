34261b3636a9660a902dd6bc306e56541499428f
atomicra <atomicra@ya.ru>
2021-05-07 17:42:18 +0300

    tiles: obb feature has been added in BV logic for compute Intersections with frustum. comment is fixed

--- modules/tiles/src/tileset/helpers/bounding-volume.js
+++ modules/tiles/src/tileset/helpers/bounding-volume.js
@@ -77,7 +77,7 @@ function createBox(box, transform, result) {
   // The next three elements (indices 6, 7, and 8) define the y axis direction and half-length.
   // The last three elements (indices 9, 10, and 11) define the z axis direction and half-length.
   // 2. A half-size-quaternion based representation.
-  // box: An array of 9 numbers that define an oriented bounding box.
+  // box: An array of 10 numbers that define an oriented bounding box.
   // The first three elements define the x, y, and z values for the center of the box in a right-handed 3-axis (x, y, z) Cartesian coordinate system where the z-axis is up.
   // The next three elements (with indices 3, 4, and 5) define the halfSize.
   // The last four elements (indices 6, 7, 8 and 10) define the quaternion.
