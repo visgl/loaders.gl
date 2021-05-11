02ca687398b9a98828c770b9956eb9d3ccc6edc9
laijackylai <66239914+laijackylai@users.noreply.github.com>
2021-05-11 11:36:56 +0800

    Update terrain-loader.js
    add tesselector option

--- modules/terrain/src/terrain-loader.js
+++ modules/terrain/src/terrain-loader.js
@@ -1,32 +1,33 @@
 /** @typedef {import('@loaders.gl/loader-utils').WorkerLoaderObject} WorkerLoaderObject */
 /** @typedef {import('@loaders.gl/loader-utils').LoaderObject} LoaderObject */
-import {VERSION} from './lib/utils/version';
 import loadTerrain from './lib/parse-terrain';
+import { VERSION } from './lib/utils/version';
 
 /**
  * Worker loader for quantized meshes
  * @type {WorkerLoaderObject}
  */
 export const TerrainWorkerLoader = {
-  name: 'Terrain',
-  id: 'terrain',
-  module: 'terrain',
-  version: VERSION,
-  worker: true,
-  extensions: ['png', 'pngraw'],
-  mimeTypes: ['image/png'],
-  options: {
-    terrain: {
-      bounds: null,
-      meshMaxError: 10,
-      elevationDecoder: {
-        rScaler: 1,
-        gScaler: 0,
-        bScaler: 0,
-        offset: 0
-      }
-    }
-  }
+	name: 'Terrain',
+	id: 'terrain',
+	module: 'terrain',
+	version: VERSION,
+	worker: true,
+	extensions: ['png', 'pngraw'],
+	mimeTypes: ['image/png'],
+	options: {
+		terrain: {
+			tesselector: 'martini',
+			bounds: null,
+			meshMaxError: 10,
+			elevationDecoder: {
+				rScaler: 1,
+				gScaler: 0,
+				bScaler: 0,
+				offset: 0
+			}
+		}
+	}
 };
 
 /**
@@ -34,6 +35,6 @@ export const TerrainWorkerLoader = {
  * @type {LoaderObject}
  */
 export const TerrainLoader = {
-  ...TerrainWorkerLoader,
-  parse: loadTerrain
+	...TerrainWorkerLoader,
+	parse: loadTerrain
 };
