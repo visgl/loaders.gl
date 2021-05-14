7f062bb1407b828a2fd2b25fc444516fba487d4b
Daniil <daniil.ryabinin@actionengine.com>
2021-05-14 14:46:13 +0300

    B3dm - Support legacy b3dm data format

--- modules/3d-tiles/src/lib/parsers/helpers/parse-3d-tile-tables.js
+++ modules/3d-tiles/src/lib/parsers/helpers/parse-3d-tile-tables.js
@@ -8,20 +8,55 @@ const SIZEOF_UINT32 = 4;
 // eslint-disable-next-line max-statements
 export function parse3DTileTablesHeaderSync(tile, arrayBuffer, byteOffset) {
   const view = new DataView(arrayBuffer);
+  let batchLength;
 
   tile.header = tile.header || {};
 
-  tile.header.featureTableJsonByteLength = view.getUint32(byteOffset, true);
+  let featureTableJsonByteLength = view.getUint32(byteOffset, true);
   byteOffset += SIZEOF_UINT32;
 
-  tile.header.featureTableBinaryByteLength = view.getUint32(byteOffset, true);
+  let featureTableBinaryByteLength = view.getUint32(byteOffset, true);
   byteOffset += SIZEOF_UINT32;
 
-  tile.header.batchTableJsonByteLength = view.getUint32(byteOffset, true);
+  let batchTableJsonByteLength = view.getUint32(byteOffset, true);
   byteOffset += SIZEOF_UINT32;
 
-  tile.header.batchTableBinaryByteLength = view.getUint32(byteOffset, true);
+  let batchTableBinaryByteLength = view.getUint32(byteOffset, true);
   byteOffset += SIZEOF_UINT32;
+
+  // First legacy header format - [batchLength] [batchTableByteLength] ('batchTableJsonByteLength': JSON starts with a quotation mark or the glTF magic)
+  // Second legacy format - [batchTableJsonByteLength] [batchTableBinaryByteLength] [batchLength] (Second legacy format is similar as first but here we check 'batchTableBinaryByteLength' instead)
+  // Current header format - [featureTableJsonByteLength] [featureTableBinaryByteLength] [batchTableJsonByteLength] [batchTableBinaryByteLength]
+  // First byte will be 0x22 or 0x67. The minimum uint32 expected is 0x22000000 = 570425344 = 570MB.
+  if (batchTableJsonByteLength >= 570425344) {
+    byteOffset -= SIZEOF_UINT32 * 2;
+    batchLength = featureTableJsonByteLength;
+    batchTableJsonByteLength = featureTableBinaryByteLength;
+    batchTableBinaryByteLength = 0;
+    featureTableJsonByteLength = 0;
+    featureTableBinaryByteLength = 0;
+
+    const deprecationWarning = `This b3dm header is using the legacy format [batchLength] [batchTableByteLength].
+      The new format spec: https://github.com/CesiumGS/3d-tiles/tree/master/specification/TileFormats/Batched3DModel.`;
+    console.warn(deprecationWarning); // eslint-disable-line
+  } else if (batchTableBinaryByteLength >= 570425344) {
+    byteOffset -= SIZEOF_UINT32;
+    batchLength = batchTableJsonByteLength;
+    batchTableJsonByteLength = featureTableJsonByteLength;
+    batchTableBinaryByteLength = featureTableBinaryByteLength;
+    featureTableJsonByteLength = 0;
+    featureTableBinaryByteLength = 0;
+
+    const deprecationWarning = `This b3dm header is using the legacy format [batchTableJsonByteLength] [batchTableBinaryByteLength] [batchLength].
+      The new format spec: https://github.com/CesiumGS/3d-tiles/tree/master/specification/TileFormats/Batched3DModel.`;
+    console.warn(deprecationWarning); // eslint-disable-line
+  }
+
+  tile.header.featureTableJsonByteLength = featureTableJsonByteLength;
+  tile.header.featureTableBinaryByteLength = featureTableBinaryByteLength;
+  tile.header.batchTableJsonByteLength = batchTableJsonByteLength;
+  tile.header.batchTableBinaryByteLength = batchTableBinaryByteLength;
+  tile.header.batchLength = batchLength;
 
   return byteOffset;
 }
@@ -33,10 +68,10 @@ export function parse3DTileTablesSync(tile, arrayBuffer, byteOffset, options) {
 }
 
 function parse3DTileFeatureTable(tile, arrayBuffer, byteOffset, options) {
-  const {featureTableJsonByteLength, featureTableBinaryByteLength} = tile.header;
+  const {featureTableJsonByteLength, featureTableBinaryByteLength, batchLength} = tile.header;
 
   tile.featureTableJson = {
-    BATCH_LENGTH: 0
+    BATCH_LENGTH: batchLength || 0
   };
 
   if (featureTableJsonByteLength > 0) {
