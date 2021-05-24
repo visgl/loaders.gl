62642379f7c1adaa5ce0f9f1626ae3cde1dda26b
felixpalmer <felixpalmer@gmail.com>
2021-05-24 19:28:57 +0200

    chore(mvt): Use earcut from math.gl (#1391)

--- modules/mvt/test/mvt-loader.spec.js
+++ modules/mvt/test/mvt-loader.spec.js
@@ -275,29 +275,37 @@ test('Triangulation is supported', async t => {
 });
 
 test('Rings - single ring', async t => {
-  const result = classifyRings(ringsSingleRing);
-  t.deepEqual(result, [[0]]);
+  const geom = {...ringsSingleRing};
+  const classified = classifyRings(geom);
+  t.deepEqual(classified.areas, [[-0.02624368667602539]]);
+  t.deepEqual(classified.lines, [[0]]);
   t.end();
 });
 
 test('Rings - ring and hole', async t => {
-  const result = classifyRings(ringsRingAndHole);
-  t.deepEqual(result, [[0, 10]]);
+  const geom = {...ringsRingAndHole};
+  const classified = classifyRings(geom);
+  t.deepEqual(classified.areas, [[-0.02624368667602539, 0.001363515853881836]]);
+  t.deepEqual(classified.lines, [[0, 10]]);
   t.end();
 });
 
 test('Rings - two rings', async t => {
-  const result = classifyRings(ringsTwoRings);
-  t.deepEqual(result, [[0], [10]]);
+  const geom = {...ringsTwoRings};
+  const classified = classifyRings(geom);
+  t.deepEqual(classified.areas, [[-0.02624368667602539], [-0.001363515853881836]]);
+  t.deepEqual(classified.lines, [[0], [10]]);
   t.end();
 });
 
 test('Rings - zero sized hole', async t => {
   // In addition to checking the result,
   // verify that the data array is shortened
-  t.equal(ringsZeroSizeHole.data.length, 20);
-  const result = classifyRings(ringsZeroSizeHole);
-  t.deepEqual(result, [[0]]);
-  t.equal(ringsZeroSizeHole.data.length, 12);
+  const geom = {...ringsZeroSizeHole};
+  t.equal(geom.data.length, 20);
+  const classified = classifyRings(geom);
+  t.deepEqual(classified.areas, [[-0.44582176208496094]]);
+  t.deepEqual(classified.lines, [[0]]);
+  t.equal(classified.data.length, 12);
   t.end();
 });
