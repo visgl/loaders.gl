89a0d43ce5342dea23c8bbba91b8b5e37e5c3488
Ib Green <ib@unfolded.ai>
2021-06-06 16:51:03 -0700

    feat(zarr): Add new zarr module (#1460)
    Co-authored-by: Trevor Manz <trevor.j.manz@gmail.com>

---
+++ modules/zarr/src/index.ts
@@ -0,0 +1,4 @@
+export { loadZarr } from './lib/load-zarr';
+export { loadBioformatsZarr } from './lib/load-bioformats-zarr';
+
+export { default as ZarrPixelSource } from './lib/zarr-pixel-source';
---
+++ modules/zarr/src/lib/load-zarr.ts
@@ -0,0 +1,47 @@
+import type { ZarrArray } from 'zarr';
+import { loadMultiscales, guessTileSize } from './utils/zarr-utils';
+import ZarrPixelSource from './zarr-pixel-source';
+import type { Labels } from '../types';
+
+interface Channel {
+  active: boolean;
+  color: string;
+  label: string;
+  window: {
+    min?: number;
+    max?: number;
+    start: number;
+    end: number;
+  };
+}
+
+interface Omero {
+  channels: Channel[];
+  rdefs: {
+    defaultT?: number;
+    defaultZ?: number;
+    model: string;
+  };
+  name?: string;
+}
+
+interface Multiscale {
+  datasets: { path: string }[];
+  version?: string;
+}
+
+export interface RootAttrs {
+  omero: Omero;
+  multiscales: Multiscale[];
+}
+
+export async function loadZarr(store: ZarrArray['store']) {
+  const { data, rootAttrs } = await loadMultiscales(store);
+  const labels = ['t', 'c', 'z', 'y', 'x'] as Labels<['t', 'c', 'z']>;
+  const tileSize = guessTileSize(data[0]);
+  const pyramid = data.map(arr => new ZarrPixelSource(arr, labels, tileSize));
+  return {
+    data: pyramid,
+    metadata: rootAttrs
+  };
+}
