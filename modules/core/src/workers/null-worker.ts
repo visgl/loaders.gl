cfbef41fd7cfadd88faf65d325a200991fe5142b
Avnerus <avnerus@gmail.com>
2021-06-25 12:57:34 +0300

    Move the null worker to core

---
+++ modules/core/src/workers/null-worker.ts
@@ -0,0 +1,4 @@
+import {createLoaderWorker} from '@loaders.gl/loader-utils';
+import {NullLoader} from '../null-loader';
+
+createLoaderWorker(NullLoader);
--- modules/loader-utils/src/workers/null-worker.ts
+++
@@ -1,4 +0,0 @@
-import {createLoaderWorker} from '../lib/worker-loader-utils/create-loader-worker';
-import {NullLoader} from '@loaders.gl/core';
-
-createLoaderWorker(NullLoader);
