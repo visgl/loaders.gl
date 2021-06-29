9e401a154edfacd7ff629cccc3098ea42879508f
Ib Green <ib@unfolded.ai>
2021-06-28 18:05:16 -0700

    wip

--- modules/worker-utils/src/lib/env-utils/version.ts
+++ modules/worker-utils/src/lib/env-utils/version.ts
@@ -1,7 +1,7 @@
 // Version constant cannot be imported, it needs to correspond to the build version of **this** module.
 // __VERSION__ is injected by babel-plugin-version-inline
 declare let __VERSION__;
-export const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
+const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'beta';
 if (typeof __VERSION__ === 'undefined') {
   // eslint-disable-next-line
   console.error(
