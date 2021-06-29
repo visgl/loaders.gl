0a045e7d10d3241eaf84e5c5045f8a53ea17f636
Ib Green <ib@unfolded.ai>
2021-06-28 18:18:31 -0700

    wip

--- modules/core/src/lib/loader-utils/option-defaults.ts
+++ modules/core/src/lib/loader-utils/option-defaults.ts
@@ -5,7 +5,6 @@ export const DEFAULT_LOADER_OPTIONS: LoaderOptions = {
   // baseUri
   fetch: null,
   log: new ConsoleLog(), // A probe.gl compatible (`log.log()()` syntax) that just logs to console
-  nothrow: false,
 
   CDN: 'https://unpkg.com/@loaders.gl',
   worker: true, // By default, use worker if provided by loader.
