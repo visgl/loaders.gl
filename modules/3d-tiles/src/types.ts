2f7dd5630fd06715ab7f70418e59702390f885c0
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2021-07-06 11:46:10 +0300

    Move types from i3s-converter to particular modules (#1609)

---
+++ modules/3d-tiles/src/types.ts
@@ -0,0 +1,36 @@
+import type {GLTFObject} from '@loaders.gl/gltf';
+import {Matrix4, Vector3} from '@math.gl/core';
+
+export type BatchTableJson = {
+  [key: string]: any[];
+};
+
+export type B3DMContent = {
+  batchTableJson?: BatchTableJson;
+  byteLength: number;
+  byteOffset: number;
+  cartesianModelMatrix: Matrix4;
+  cartesianOrigin: Vector3;
+  cartographicModelMatrix: Matrix4;
+  cartographicOrigin: Vector3;
+  featureIds?: number[] | null;
+  featureTableBinary?: Uint8Array;
+  featureTableJson?: BatchTableJson;
+  gltf?: GLTFObject;
+  gltfUpAxis: string;
+  header: GLTFHeader;
+  magic: number;
+  modelMatrix: Matrix4;
+  rotateYtoZ: boolean;
+  rtcCenter: [number, number, number];
+  type: string;
+  version: number;
+};
+
+export type GLTFHeader = {
+  batchLength?: number;
+  batchTableBinaryByteLength: number;
+  batchTableJsonByteLength: number;
+  featureTableBinaryByteLength: number;
+  featureTableJsonByteLength: number;
+};
