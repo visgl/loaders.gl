05d95feb791d2049a920d1a3495fa43830c1b2e6
Ib Green <ib@unfolded.ai>
2021-07-17 09:58:01 -0700

    fix(textures): compressed texture mime types (#1678)

--- modules/textures/src/workers/crunch-worker.ts
+++ modules/textures/src/workers/crunch-worker.ts
@@ -1,13 +1,13 @@
 import {createLoaderWorker} from '@loaders.gl/loader-utils';
-import {CrunchWorkerLoader} from '../crunch-loader';
+import {CrunchLoader} from '../crunch-loader';
 import {parseCrunch} from '../lib/parsers/parse-crunch';
 
 /**
  * Loader for the Crunch compressed texture container format
  */
-export const CrunchLoader = {
-  ...CrunchWorkerLoader,
+export const CrunchLoaderWithParser = {
+  ...CrunchLoader,
   parse: parseCrunch
 };
 
-createLoaderWorker(CrunchLoader);
+createLoaderWorker(CrunchLoaderWithParser);
