9aec8c1ecc496eac62dc33db0e4c1d7b38d8e56c
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2021-08-02 10:37:53 +0300

    Ranaming

--- modules/parquet/src/parquetjs/codecs/dictionary.ts
+++ modules/parquet/src/parquetjs/codecs/dictionary.ts
@@ -1,9 +1,9 @@
-import {decodeValues as decodeDictionaryValues} from './rle';
+import {decodeValues as decodeRleValues} from './rle';
 
 export function decodeValues(type, cursor, count, opts) {
   opts.bitWidth = cursor.buffer.slice(cursor.offset, cursor.offset + 1).readInt8(0);
   cursor.offset += 1;
-  return decodeDictionaryValues(type, cursor, count, {...opts, disableEnvelope: true});
+  return decodeRleValues(type, cursor, count, {...opts, disableEnvelope: true});
 }
 
 export function encodeValues(type, cursor, count, opts) {
