a85dc33ff6a32dac69f37f072cbb8026952e5a15
Evin-K <ksenia.evin@actionengine.com>
2021-08-09 12:51:20 +0300

    feat(examples) created website example with geopkg loader

---
+++ examples/website/geopkg/components/control-panel.js
@@ -0,0 +1,186 @@
+import styled from 'styled-components';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-template-rows: 30px 25px 35px;
+  grid-template-areas: 
+    "tileset tileset tileset" 
+    "mapstyle mapstyle mapstyle"
+    "frame frame memory";
+  position: absolute;
+  padding: 5px;
+  top: 0;
+  right: 0;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const TilesetDropDown = styled.select`
+  grid-area: tileset;
+  background: rgba(36,39,48,0.7);
+  padding: 5px;
+  font-weight: 800;
+  cursor: pointer;
+  font-size: 13px;
+  text-transform: uppercase;
+  color: #00ADE6;
+  text-shadow: 1px 1px 1px #212529;
+  border: none;
+    option {
+      color: black;
+      background: white;
+      font-weight: small;
+      display: flex;
+      white-space: pre;
+      min-height: 20px;
+      padding: 0px 2px 1px;
+    }
+  &:hover {
+    background: #212529;
+  }
+`;
+
+const DropDown = styled.select`
+  grid-area: mapstyle;
+  background: rgba(0, 0, 0, 0.7);
+  padding: 5px;
+  text-transform: uppercase;
+  border: none;
+  font-size: 11px;
+  color: #adb5bd;
+  cursor: pointer;
+    option {
+      color: black;
+      background: white;
+      display: flex;
+      white-space: pre;
+      min-height: 20px;
+      padding: 0px 2px 1px;
+    }
+  &:hover {
+    background: #212529;
+  }
+`;
+
+const FrameWrap = styled.div`
+  grid-area: frame;
+  height: fit-content;
+  overflow: hidden;
+`;
+
+const FrameControl = styled.div`
+  width: 100%;
+  z-index: 20;
+  margin-top: 5px;
+`;
+
+const FrameButton = styled.button`
+  border: none;
+  background: rgba(0, 0, 0, 0.5);
+  color: #f2e9e4;
+  font-size: 9px;
+  height: 30px;
+  width: 100px;
+  text-transform: uppercase;
+  align-items: center;
+  justify-content: center;
+  transition: all 1s;
+  border-radius: 2px;
+  cursor: pointer;
+  &:hover {
+    color: #242730;
+    background: #00ADE6;
+  }
+`;
+
+const LinkButton = styled.button`
+  grid-area: link;
+  background: rgba(0, 0, 0, 0.5);
+  color: #f2e9e4;
+  align-items: center;
+  margin-left: 10px;
+  font-size: 9px;
+  height: 30px;
+  width: 100px;
+  padding: 10px;
+  text-transform: uppercase;
+  text-decoration: none;
+  transition: all 1s;
+  border-radius: 2px;
+  cursor: pointer;
+  &:hover {
+    color: #242730;
+    background: #00ADE6;
+  }
+`;
+
+const propTypes = {
+  name: PropTypes.string,
+  tileset: PropTypes.object,
+  mapStyles: PropTypes.object,
+  metadata: PropTypes.object,
+  token: PropTypes.string,
+  onExampleChange: PropTypes.func,
+  selectedMapStyle: PropTypes.string,
+  onMapStyleChange: PropTypes.func
+};
+
+const defaultProps = {
+  droppedFile: null,
+  onChange: () => {}
+};
+
+export default class ControlPanel extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFullInfo: false
+    };
+  }
+
+  _renderExamples() {
+    const {name, onExampleChange} = this.props;
+    const value = name || CUSTOM_EXAMPLE;
+
+    return (
+      <TilesetDropDown
+        value={value}
+        onChange={(evt) => {
+          const selected = evt.target.value;
+          this.setState({selected});
+          onExampleChange(EXAMPLES[selected]);
+        }}
+      >
+        {!name && (
+          <option key={'custom-example'} value={'custom-example'}>
+            {CUSTOM_EXAMPLE}
+          </option>
+        )}
+        {Object.keys(EXAMPLES).map((key) => {
+          const example = EXAMPLES[key];
+          return (
+            <option key={key} value={example.name}>
+              {example.name}
+            </option>
+          );
+        })}
+      </TilesetDropDown>
+    );
+  }
+
+  render() {
+    return (
+      <Container>
+        {/* {this._renderExamples()} */}
+        {this.props.children}
+      </Container>
+    );
+  }
+}
+
+ControlPanel.propTypes = propTypes;
+ControlPanel.defaultProps = defaultProps;
