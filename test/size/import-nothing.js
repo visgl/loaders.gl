59a37cf1e0ef8dd5676801907e5901c7d73cdd45
morphoix <morphoix@gmail.com>
2021-08-24 16:36:05 +0300

    fix(test): remove unexisting import

--- test/size/import-nothing.js
+++ test/size/import-nothing.js
@@ -1,3 +1,3 @@
-import {Schema, Field, Float32Vector, Float32} from '@loaders.gl/tables';
+import {Schema, Field, Float32Vector, Float32} from '@loaders.gl/schema';
 
 console.log(Schema, Field); // eslint-disable-line
