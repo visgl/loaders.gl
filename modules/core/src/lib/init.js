c98e2081af04280fcf28b732f2b004056eb1b6ee
Ib Green <ib@unfolded.ai>
2021-09-04 12:48:50 -0700

    chore: Use globalThis (#1826)

--- modules/core/src/lib/init.js
+++ modules/core/src/lib/init.js
@@ -1,11 +1,10 @@
-import {global} from '@loaders.gl/loader-utils';
 // @ts-ignore TS2304: Cannot find name '__VERSION__'.
 const version = typeof __VERSION__ !== 'undefined' ? __VERSION__ : '';
 
 // @ts-ignore
-global.loaders = Object.assign(global.loaders || {}, {
+globalThis.loaders = Object.assign(globalThis.loaders || {}, {
   VERSION: version
 });
 
 // @ts-ignore
-export default global.loaders;
+export default globalThis.loaders;
