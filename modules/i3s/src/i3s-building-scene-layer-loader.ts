f3efbb6a4637d1f6870a42d8d671e9791252b9b3
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2021-09-14 09:23:08 +0300

    Move parsing code to the lib folder + fix commets

--- modules/i3s/src/i3s-building-scene-layer-loader.ts
+++ modules/i3s/src/i3s-building-scene-layer-loader.ts
@@ -1,12 +1,12 @@
 import type {LoaderWithParser, LoaderOptions, LoaderContext} from '@loaders.gl/loader-utils';
-import type {BuildingSceneLayerTileset, BuildingSceneSublayer} from './types';
+import type {BuildingSceneLayerTileset} from './types';
+
+import {parseBuildingSceneLayer} from './lib/parsers/parse-i3s-building-scene-layer';
 
 // __VERSION__ is injected by babel-plugin-version-inline
 // @ts-ignore TS2304: Cannot find name '__VERSION__'.
 
 const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'beta';
-const OBJECT_3D_LAYER_TYPE = '3DObject';
-
 /**
  * Loader for I3S - Building Scene Layer
  */
@@ -26,46 +26,9 @@ async function parse(
   options?: LoaderOptions,
   context?: LoaderContext
 ): Promise<BuildingSceneLayerTileset> {
-  const layers: BuildingSceneSublayer[] = [];
-  const layer0 = JSON.parse(new TextDecoder().decode(data));
-  const {sublayers} = layer0;
-  const url = context?.url;
-
-  if (!url) {
+  if (!context?.url) {
     throw new Error('Url is not provided');
   }
 
-  parseSublayersTree(sublayers, url, layers);
-
-  return {
-    header: layer0,
-    sublayers: layers
-  };
-}
-
-function parseSublayersTree(
-  sublayers: BuildingSceneSublayer[],
-  url: string,
-  layers: BuildingSceneSublayer[]
-): void {
-  for (let index = 0; index < sublayers.length; index++) {
-    const subLayer = sublayers[index];
-    const {id, layerType, ...rest} = subLayer;
-
-    // Add support only for 3DObject layer type for I3S purposes.
-    if (layerType === OBJECT_3D_LAYER_TYPE) {
-      const sublayerUrl = `${url}/sublayers/${id}`;
-
-      layers.push({
-        url: sublayerUrl,
-        id,
-        layerType,
-        ...rest
-      });
-    }
-
-    if (subLayer?.sublayers?.length) {
-      parseSublayersTree(subLayer.sublayers, url, layers);
-    }
-  }
+  return parseBuildingSceneLayer(data, context.url);
 }
---
+++ modules/i3s/src/lib/parsers/parse-i3s-building-scene-layer.ts
@@ -0,0 +1,57 @@
+import type {BuildingSceneLayerTileset, BuildingSceneSublayer} from '../../types';
+
+const OBJECT_3D_LAYER_TYPE = '3DObject';
+
+/**
+ * Parses Builiding Scene Layer and creates tileset
+ * @param data
+ * @param options
+ * @param context
+ */
+export async function parseBuildingSceneLayer(
+  data: ArrayBuffer,
+  url: string
+): Promise<BuildingSceneLayerTileset> {
+  const layer0 = JSON.parse(new TextDecoder().decode(data));
+  const {sublayers} = layer0;
+
+  return {
+    header: layer0,
+    sublayers: parseSublayersTree(sublayers, url)
+  };
+}
+
+/**
+ * Recursively parses Building Scene Layer sublayers.
+ * @param sublayers
+ * @param url
+ */
+function parseSublayersTree(
+  sublayers: BuildingSceneSublayer[],
+  url: string
+): BuildingSceneSublayer[] {
+  let layers: BuildingSceneSublayer[] = [];
+
+  for (let index = 0; index < sublayers.length; index++) {
+    const subLayer = sublayers[index];
+    const {id, layerType, ...rest} = subLayer;
+
+    // Add support only for 3DObject layer type for I3S purposes.
+    if (layerType === OBJECT_3D_LAYER_TYPE) {
+      const sublayerUrl = `${url}/sublayers/${id}`;
+
+      layers.push({
+        url: sublayerUrl,
+        id,
+        layerType,
+        ...rest
+      });
+    }
+
+    if (subLayer?.sublayers?.length) {
+      layers = [...layers, ...parseSublayersTree(subLayer.sublayers, url)];
+    }
+  }
+
+  return layers;
+}
