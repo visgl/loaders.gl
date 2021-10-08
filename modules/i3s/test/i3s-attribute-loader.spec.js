3b01bf326c10170bdc666d56c18cae1e53563e55
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2021-10-08 17:44:35 +0300

    fix(i3s-attributes): fix picking attributes parsing

--- modules/i3s/test/i3s-attribute-loader.spec.js
+++ modules/i3s/test/i3s-attribute-loader.spec.js
@@ -8,9 +8,17 @@ const heightRoofUrl = '@loaders.gl/i3s/test/data/attributes/f_2/0/index.bin';
 const wrongNamesUrl = '@loaders.gl/i3s/test/data/attributes/f_1/0/wrong.bin';
 const wrongBufferNamesUrl = '@loaders.gl/i3s/test/data/attributes/f_1/0/wrong-buffer.bin';
 
+const objectIdsWithCodeValuesUrl =
+  '@loaders.gl/i3s/test/data/BuildingSceneLayer/attributes/f_0/0/index.bin';
+const attributesWithCodeValuesUrl =
+  '@loaders.gl/i3s/test/data/BuildingSceneLayer/attributes/f_27/0/index.bin';
+const shortInt16AttributesUrl =
+  '@loaders.gl/i3s/test/data/BuildingSceneLayer/attributes/f_7/0/index.bin';
+
 const name602 = 'West End Building\0';
 const objecId0 = 979297;
 const heightRoof0 = 32.18;
+const codeValuesFeatureId = 5;
 
 const TILE_WITHOUT_URLS = {
   tileset: {
@@ -62,6 +70,76 @@ const TILE_WITH_WRONG_BUFFER_NAME_ATTRIBUTE = {
   },
   header: {
     attributeUrls: [objectIdsUrl, wrongBufferNamesUrl, heightRoofUrl]
+  }
+};
+
+const TILE_WITH_CODE_VALUES_ATTRIBUTE = {
+  tileset: {
+    tileset: {
+      attributeStorageInfo: [
+        {key: 'f_0', name: 'OBJECTID_1', objectIds: []},
+        {key: 'f_27', name: 'BaseLevel', attributeValues: {valueType: 'Int32'}}
+      ],
+      fields: [
+        {
+          name: 'BaseLevel',
+          type: 'esriFieldTypeInteger',
+          alias: 'BaseLevel',
+          domain: {
+            type: 'codedValue',
+            name: 'Levels_Admin_BLDG_DA_vr13_2018_rvt',
+            description: 'Levels_Admin_BLDG_DA_vr13_2018_rvt',
+            codedValues: [
+              {
+                name: 'SUBBASEMENT',
+                code: 0
+              },
+              {
+                name: 'BASEMENT',
+                code: 1
+              },
+              {
+                name: '1ST FLOOR',
+                code: 2
+              },
+              {
+                name: '2ND FLOOR',
+                code: 3
+              },
+              {
+                name: '3RD FLOOR',
+                code: 4
+              },
+              {
+                name: '4TH FLOOR',
+                code: 5
+              },
+              {
+                name: 'ROOF',
+                code: 6
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  header: {
+    attributeUrls: [objectIdsWithCodeValuesUrl, attributesWithCodeValuesUrl]
+  }
+};
+
+const TILE_WITH_INT_16_ATTRIBUTES = {
+  tileset: {
+    tileset: {
+      attributeStorageInfo: [
+        {key: 'f_0', name: 'OBJECTID_1', objectIds: []},
+        {key: 'f_7', name: 'BldgLevel_IsBuildingStory', attributeValues: {valueType: 'Int16'}}
+      ]
+    }
+  },
+  header: {
+    attributeUrls: [objectIdsWithCodeValuesUrl, shortInt16AttributesUrl]
   }
 };
 
@@ -230,5 +308,32 @@ test('I3SAttributeLoader#loadFeatureAttributes should work with fetch options', 
 
   const attributes = await loadFeatureAttributes(tile, featureId, options);
   t.deepEqual(attributes, {OBJECTID: '979297', NAME: ''});
+  t.end();
+});
+
+test('I3SAttributeLoader#loadFeatureAttributes should work with code values if tilesetFields is provided', async (t) => {
+  const tile = TILE_WITH_CODE_VALUES_ATTRIBUTE;
+  const featureId = codeValuesFeatureId;
+  const options = {
+    attributeName: 'BaseLevel',
+    attributeType: 'Int32'
+  };
+
+  const attributes = await loadFeatureAttributes(tile, featureId, options);
+  t.deepEqual(attributes, {OBJECTID_1: '5', BaseLevel: '1ST FLOOR'});
+  t.end();
+});
+
+test('I3SAttributeLoader#loadFeatureAttributes should work with Int16 attribute type', async (t) => {
+  const tile = TILE_WITH_INT_16_ATTRIBUTES;
+  const featureId = codeValuesFeatureId;
+  const options = {
+    attributeName: 'BldgLevel_IsBuildingStory',
+    attributeType: 'Int16'
+  };
+
+  const attributes = await loadFeatureAttributes(tile, featureId, options);
+  // eslint-disable-next-line camelcase
+  t.deepEqual(attributes, {OBJECTID_1: '5', BldgLevel_IsBuildingStory: '-1'});
   t.end();
 });
