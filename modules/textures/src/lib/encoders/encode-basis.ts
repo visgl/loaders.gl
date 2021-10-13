c6e6ab5e93006f9cd6a857cc2756db43e278bc2e
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2021-10-13 17:17:55 +0300

    change tests + minor fixes

--- modules/textures/src/lib/encoders/encode-basis.ts
+++ modules/textures/src/lib/encoders/encode-basis.ts
@@ -4,18 +4,17 @@ import {loadBasisEncoderModule} from '../parsers/basis-module-loader';
 /**
  * Encodes image to basis texture
  * Code example is taken from here - https://github.com/BinomialLLC/basis_universal/blob/master/webgl/encode_test/index.html#L413
- * BasisEncoder API is taken from here - https://github.com/BinomialLLC/basis_universal/blob/master/webgl/transcoder/basis_wrappers.cpp#L1712
+ * BasisEncoder API - https://github.com/BinomialLLC/basis_universal/blob/master/webgl/transcoder/basis_wrappers.cpp#L1712
  * @param image
  * @param options
  */
 export default async function encodeBasis(image: ImageDataType, options: any): Promise<Uint8Array> {
   const {useSRGB = false, qualityLevel = 10, encodeUASTC = false, mipmaps = false} = options;
+  const {BasisEncoder} = await loadBasisEncoderModule(options);
+  const basisEncoder = new BasisEncoder();
 
   try {
-    const {BasisEncoder} = await loadBasisEncoderModule(options);
-
-    const basisFileData = new Uint8Array(image.width * image.height * 4);
-    const basisEncoder = new BasisEncoder();
+    const basisFileData = new Uint8Array(image.width * image.height * 10);
 
     basisEncoder.setSliceSourceImage(0, image.data, image.width, image.height, false);
     basisEncoder.setPerceptual(useSRGB);
@@ -27,12 +26,12 @@ export default async function encodeBasis(image: ImageDataType, options: any): P
     const numOutputBytes = basisEncoder.encode(basisFileData);
     const actualBasisFileData = new Uint8Array(basisFileData.buffer, 0, numOutputBytes);
 
-    basisEncoder.delete();
-
     return actualBasisFileData;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Basis Encoder Error: ', error);
     throw error;
+  } finally {
+    basisEncoder.delete();
   }
 }
