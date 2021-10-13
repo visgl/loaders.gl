e6257ec22e7c8d667e99b92a1f469b11789ffaf6
Victor Belomestnov <belom88@yandex.ru>
2021-10-13 14:29:38 +0300

    fix(tiles): i3s lod logic (#1870)

--- modules/tiles/src/tileset/helpers/i3s-lod.ts
+++ modules/tiles/src/tileset/helpers/i3s-lod.ts
@@ -1,52 +1,23 @@
-import {toRadians} from '@math.gl/core';
+import {Vector3} from '@math.gl/core';
+import {Ellipsoid} from '@math.gl/geospatial';
+import {Tile3D} from '../..';
+import {FrameState} from './frame-state';
 
-const WGS84_RADIUS_X = 6378137.0;
-// use this to bias the lod switching  (1+ results in increasing the LOD quality)
-const qualityFactor = Math.PI / 2; // empirical derived bias factor
-/* eslint-disable max-statements */
-export function lodJudge(tile, frameState) {
-  const viewport = frameState.viewport;
-  const metersPerPixel = viewport.metersPerPixel;
-
-  const mbsLat = tile.header.mbs[1];
-  const mbsLon = tile.header.mbs[0];
-  const mbsZ = tile.header.mbs[2];
-  const mbsR = tile.header.mbs[3];
-
-  const {height, width, latitude, longitude} = viewport;
-
-  const viewportCenter = [longitude, latitude];
-  const mbsCenter = [mbsLon, mbsLat, mbsZ];
-  const mbsLatProjected = [longitude, mbsLat];
-  const mbsLonProjected = [mbsLon, latitude];
-
-  const diagonalInMeters = Math.sqrt(height * height + width * width) * metersPerPixel[0];
-  const distanceInMeters = getDistanceFromLatLon(viewportCenter, mbsCenter);
-
-  const visibleHeight = height * 0.5 + mbsR / WGS84_RADIUS_X;
-  const visibleWidth = width * 0.5 + mbsR / WGS84_RADIUS_X;
-
-  if (distanceInMeters > diagonalInMeters + mbsR / WGS84_RADIUS_X) {
-    return 'OUT';
-  }
-  if (getDistanceFromLatLon(viewportCenter, mbsLatProjected) > visibleHeight) {
-    return 'OUT';
-  }
-  if (getDistanceFromLatLon(viewportCenter, mbsLonProjected) > visibleWidth) {
-    return 'OUT';
-  }
-
-  if (tile.lodMetricValue === 0) {
+/**
+ * For the maxScreenThreshold error metric, maxError means that you should replace the node with it's children
+   as soon as the nodes bounding sphere has a screen radius larger than maxError pixels.
+   In this sense a value of 0 means you should always load it's children,
+   or if it's a leaf node, you should always display it.
+ * @param tile 
+ * @param frameState 
+ * @returns 
+ */
+export function getLodStatus(tile: Tile3D, frameState: FrameState): 'DIG' | 'OUT' | 'DRAW' {
+  if (tile.lodMetricValue === 0 || isNaN(tile.lodMetricValue)) {
     return 'DIG';
   }
-
-  // For the maxScreenThreshold error metric, maxError means that you should replace the node with it's children
-  // as soon as the nodes bounding sphere has a screen radius larger than maxError pixels.
-  // In this sense a value of 0 means you should always load it's children,
-  // or if it's a leaf node, you should always display it.
-  let screenSize = getI3ScreenSize(tile, frameState); // in pixels
-  screenSize *= qualityFactor;
-  if (screenSize < 0.5) {
+  const screenSize = 2 * getProjectedRadius(tile, frameState);
+  if (screenSize < 2) {
     return 'OUT';
   }
   if (!tile.header.children || screenSize <= tile.lodMetricValue) {
@@ -57,58 +28,40 @@ export function lodJudge(tile, frameState) {
   return 'OUT';
 }
 
-/* eslint-enable max-statements */
-
-function projectVertexToSphere([x, y, z]) {
-  const azim = toRadians(x);
-  const incl = toRadians(y);
-  const radius = 1.0 + z / WGS84_RADIUS_X;
-  const radCosInc = radius * Math.cos(incl);
-  x = radCosInc * Math.cos(azim);
-  y = radCosInc * Math.sin(azim);
-  z = radius * Math.sin(incl);
-  return [x, y, z];
-}
-
-function getDistanceFromLatLon(observer: number[], center: number[]) {
-  const [observerLon, observerLat, observerZ = 0.0] = observer;
-  const [centerLon, centerLat, centerZ = 0.0] = center;
-
-  const projectedCenter = projectVertexToSphere([centerLon, centerLat, centerZ]);
-  const projectedObserver = projectVertexToSphere([observerLon, observerLat, observerZ]);
-  const dx = projectedObserver[0] - projectedCenter[0];
-  const dy = projectedObserver[1] - projectedCenter[1];
-  const dz = projectedObserver[2] - projectedCenter[2];
-  return dx * dx + dy * dy + dz * dz;
-}
-
-export function getI3ScreenSize(tile, frameState) {
+/**
+ * Calculate size of MBS radius projected on the screen plane
+ * @param tile
+ * @param frameState
+ * @returns
+ */
+export function getProjectedRadius(tile: Tile3D, frameState: FrameState): number {
   const viewport = frameState.viewport;
   const mbsLat = tile.header.mbs[1];
   const mbsLon = tile.header.mbs[0];
   const mbsZ = tile.header.mbs[2];
   const mbsR = tile.header.mbs[3];
+  const mbsCenterCartesian = [...tile.boundingVolume.center];
+  const cameraPositionCartesian = [...frameState.camera.position];
 
-  const mbsCenter = [mbsLon, mbsLat, mbsZ];
-  const cameraPositionCartographic = viewport.unprojectPosition(viewport.cameraPosition);
-  const dSquared = getDistanceFromLatLon(cameraPositionCartographic, mbsCenter);
-  const mbsRNormalized = mbsR / WGS84_RADIUS_X;
-  const d = dSquared - mbsRNormalized * mbsRNormalized;
-  const fltMax = 3.4028235e38; // convert from 0x7f7fffff which is the maximum
-  if (d <= 0.0) {
-    return 0.5 * fltMax;
-  }
-  // https://stackoverflow.com/questions/21648630/radius-of-projected-sphere-in-screen-space
-  // There is a formula there to calculate projected radius:
-  // return 1.0 / Math.tan(fov) * r / Math.sqrt(d * d - r * r); // Right
-  // Hack: 300 is a Magic number to get the correct LoD. Possibly, d and r are calculated in a wrong way.
-  const screenSizeFactor =
-    ((getTanOfHalfVFAngle(frameState) * mbsRNormalized) / Math.sqrt(d)) * 300;
-  return screenSizeFactor;
-}
+  // ---------------------------
+  // Calculate mbs border vertex
+  // ---------------------------
+  const toEye = new Vector3(cameraPositionCartesian).subtract(mbsCenterCartesian).normalize();
+  // Add extra vector to form plane
+  const topVector = new Vector3([0, 1, 0]).normalize();
+  // We need radius vector orthogonal to toEye vector
+  const radiusVector = toEye.cross(topVector).normalize().scale(mbsR);
+  const sphereMbsBorderVertexCartesian = new Vector3(mbsCenterCartesian).add(radiusVector);
+  const sphereMbsBorderVertexCartographic = Ellipsoid.WGS84.cartesianToCartographic(
+    sphereMbsBorderVertexCartesian
+  );
+  // ---------------------------
 
-function getTanOfHalfVFAngle(frameState) {
-  const {projectionMatrix} = frameState.viewport;
-  const t = projectionMatrix[5];
-  return t;
+  // Project center vertex and border vertex and calculate projected radius of MBS
+  const projectedOrigin = viewport.project([mbsLon, mbsLat, mbsZ]);
+  const projectedMbsBorderVertex = viewport.project(sphereMbsBorderVertexCartographic);
+  const projectedRadius = new Vector3(projectedOrigin)
+    .subtract(projectedMbsBorderVertex)
+    .magnitude();
+  return projectedRadius;
 }
