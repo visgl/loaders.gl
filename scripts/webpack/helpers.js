e7c777053ddb2785ec5a025e55710bb53045028e
Ib Green <ib@unfolded.ai>
2021-10-14 12:22:10 -0700

    chore: TS remove implicit any

--- scripts/webpack/helpers.js
+++ scripts/webpack/helpers.js
@@ -2,8 +2,10 @@
  * peerDependencies are excluded using `externals`
  * https://webpack.js.org/configuration/externals/
  * e.g. @deck.gl/core is not bundled with @deck.gl/geolayers
+ * @param {{peerDependencies: any, browser: any}} packageInfo
  */
 module.exports.getExternals = function getExternals(packageInfo) {
+  /** @type {Record<string, string>} */
   const externals = {
     // Hard coded externals
   };
