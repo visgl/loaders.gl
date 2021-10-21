a7a6945421c9808dd00849c71171281cf81219ee
Victor Belomestnov <belom88@yandex.ru>
2021-10-21 08:04:09 +0300

    fix(tiles): update i3s lod logic (#1891)

--- modules/tiles/test/data/viewport-opts-examples.js
+++ modules/tiles/test/data/viewport-opts-examples.js
@@ -1,9 +1,9 @@
 export const VIEWPORT_DEFAULT = {
   width: 1848,
   height: 980,
-  latitude: 37.79236722394597,
-  longitude: -122.40126134069325,
-  zoom: 14.079936531679898,
+  latitude: 37.79592258631093,
+  longitude: -122.40750202607084,
+  zoom: 13.902576534944492,
   bearing: 0,
   pitch: 45,
   altitude: 1.5,
@@ -20,19 +20,16 @@ export const VIEWPORT_DEFAULT = {
       smooth: true
     }
   },
-  projectionMatrix: null,
-  fovy: 50,
+  viewMatrix: [
+    15.626671346078382, 0, 0, 0, 0, 11.049725276185539, -11.049725276185537, 0, 0,
+    11.049725276185537, 11.049725276185539, 0, 0, 0, -1.5, 1
+  ],
+  fov: 0.6435011087932844,
+  aspect: 1.8857142857142857,
+  focalDistance: 1.5,
   near: 0.1,
   far: 2.2725,
-  modelMatrix: null,
-  viewMatrix: [
-    17.670838486143424, 0, 0, 0, 0, 12.495169722804242, -12.49516972280424, 0, 0, 12.49516972280424,
-    12.495169722804242, 0, 0, 0, -1.5, 1
-  ],
-  orthographic: false,
-  fovyRadians: 0.6435011087932844,
-  aspect: 1.8857142857142857,
-  focalDistance: 1.5
+  fovy: 36.86989764584402
 };
 
 export const VIEWPORT_ZOOM_OPTS = {
@@ -72,51 +69,14 @@ export const VIEWPORT_ZOOM_OPTS = {
   focalDistance: 1.5
 };
 
-export const VIEWPORT_TOP_RIGHT_CORNER_OPTS = {
-  width: 1848,
-  height: 980,
-  latitude: 37.785888431386894,
-  longitude: -122.42101334588308,
-  zoom: 14.219746634522798,
-  bearing: 0,
-  pitch: 45,
-  altitude: 1.5,
-  maxZoom: 30,
-  minZoom: 2,
-  maxPitch: 60,
-  minPitch: 0,
-  id: 'main',
-  controller: {
-    maxPitch: 60,
-    inertia: true,
-    scrollZoom: {
-      speed: 0.01,
-      smooth: true
-    }
-  },
-  projectionMatrix: null,
-  fovy: 50,
-  near: 0.1,
-  far: 2.2725,
-  modelMatrix: null,
-  viewMatrix: [
-    19.469024518005774, 0, 0, 0, 0, 13.766679259769038, -13.766679259769036, 0, 0,
-    13.766679259769036, 13.766679259769038, 0, 0, 0, -1.5, 1
-  ],
-  orthographic: false,
-  fovyRadians: 0.6435011087932844,
-  aspect: 1.8857142857142857,
-  focalDistance: 1.5
-};
-
 export const VIEWPORT_ROTATED_OPTS = {
   width: 1848,
   height: 980,
-  latitude: 37.79399734821857,
-  longitude: -122.40483899015148,
-  zoom: 13.98515749509245,
-  bearing: -56.405529953917046,
-  pitch: 46.91398827917058,
+  latitude: 37.794091235120504,
+  longitude: -122.4053107273273,
+  zoom: 13.730290492938975,
+  bearing: 77.87337662337663,
+  pitch: 32.869565217391305,
   altitude: 1.5,
   maxZoom: 30,
   minZoom: 2,
@@ -124,24 +84,24 @@ export const VIEWPORT_ROTATED_OPTS = {
   minPitch: 0,
   transitionDuration: 0,
   viewMatrix: [
-    9.155777951142982, -9.415405444299118, 10.066446641794837, 0, 13.783439616896793,
-    6.254270629394156, -6.686730799496388, 0, 0, 12.084937599219788, 11.30335175008899, 0, 0, 0,
+    2.9132194874526927, 11.387656195436083, -7.358427548228088, 0, -13.558214100978631,
+    2.446837149633242, -1.5810868873179618, 0, 0, 7.526373079230847, 11.647563078270027, 0, 0, 0,
     -1.5, 1
   ],
-  orthographic: false,
-  fovyRadians: 0.6435011087932844,
+  fov: 0.6435011087932844,
   aspect: 1.8857142857142857,
   focalDistance: 1.5,
   near: 0.1,
-  far: 2.35388118064461
+  far: 1.930900340159826,
+  fovy: 36.86989764584402
 };
 
 export const VIEWPORT_ZOOM_OUT_OPTS = {
   width: 1848,
-  height: 649,
-  latitude: 38.90449555738972,
-  longitude: -118.90439808306486,
-  zoom: 4.975614492080811,
+  height: 980,
+  latitude: 40.63908134445145,
+  longitude: -121.62588580625729,
+  zoom: 5.159799080775016,
   bearing: 0,
   pitch: 45,
   altitude: 1.5,
@@ -158,17 +118,49 @@ export const VIEWPORT_ZOOM_OUT_OPTS = {
       smooth: true
     }
   },
-  projectionMatrix: null,
-  fovy: 50,
+  viewMatrix: [
+    0.03647776483696709, 0, 0, 0, 0, 0.025793674878747626, -0.025793674878747622, 0, 0,
+    0.025793674878747622, 0.025793674878747626, 0, 0, 0, -1.5, 1
+  ],
+  fov: 0.6435011087932844,
+  aspect: 1.8857142857142857,
+  focalDistance: 1.5,
   near: 0.1,
   far: 2.2725,
-  modelMatrix: null,
+  fovy: 36.86989764584402
+};
+
+export const VIEWPORT_NEW_YORK_OPTS = {
+  width: 1848,
+  height: 980,
+  latitude: 40.6880313516013,
+  longitude: -74.04598836028212,
+  zoom: 15.491678919886628,
+  bearing: 96.59439752702683,
+  pitch: 45.13036023378121,
+  altitude: 1.5,
+  maxZoom: 30,
+  minZoom: 2,
+  maxPitch: 60,
+  minPitch: 0,
+  id: 'main',
+  controller: {
+    maxPitch: 60,
+    inertia: true,
+    scrollZoom: {
+      speed: 0.01,
+      smooth: true
+    }
+  },
   viewMatrix: [
-    0.048480212209835644, 0, 0, 0, 0, 0.034280686806937646, -0.03428068680693764, 0, 0,
-    0.03428068680693764, 0.034280686806937646, 0, 0, 0, -1.5, 1
+    -5.3991725620119455, 32.94926718847123, -33.09954271015278, 0, -46.70368225181495,
+    -3.8090953596165535, 3.8264679399846373, 0, 0, 33.319987762857274, 33.168711396675015, 0, 0, 0,
+    -1.5, 1
   ],
-  orthographic: false,
-  fovyRadians: 0.6435011087932844,
-  aspect: 2.847457627118644,
-  focalDistance: 1.5
+  fov: 0.6435011087932844,
+  aspect: 1.8857142857142857,
+  focalDistance: 1.5,
+  near: 0.1,
+  far: 2.2776940709756763,
+  fovy: 36.86989764584402
 };
