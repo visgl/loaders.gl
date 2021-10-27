922dc2458fcfdcc2d3bb83df51d9d7522ada6c7a
Victor Belomestnov <belom88@yandex.ru>
2021-10-28 02:48:17 +0300

    feat(i3s): coordinate system option (#1899)

--- modules/i3s/src/lib/parsers/constants.ts
+++ modules/i3s/src/lib/parsers/constants.ts
@@ -48,3 +48,24 @@ export const STRING_ATTRIBUTE_TYPE = 'String';
 export const OBJECT_ID_ATTRIBUTE_TYPE = 'Oid32';
 export const FLOAT_64_TYPE = 'Float64';
 export const INT_16_ATTRIBUTE_TYPE = 'Int16';
+
+// https://github.com/visgl/deck.gl/blob/9548f43cba2234a1f4877b6b17f6c88eb35b2e08/modules/core/src/lib/constants.js#L27
+// Describes the format of positions
+export const COORDINATE_SYSTEM = {
+  // `LNGLAT` if rendering into a geospatial viewport, `CARTESIAN` otherwise
+  DEFAULT: -1,
+  // Positions are interpreted as [lng, lat, elevation]
+  // lng lat are degrees, elevation is meters. distances as meters.
+  LNGLAT: 1,
+
+  // Positions are interpreted as meter offsets, distances as meters
+  METER_OFFSETS: 2,
+
+  // Positions are interpreted as lng lat offsets: [deltaLng, deltaLat, elevation]
+  // deltaLng, deltaLat are delta degrees, elevation is meters.
+  // distances as meters.
+  LNGLAT_OFFSETS: 3,
+
+  // Non-geospatial
+  CARTESIAN: 0
+};
