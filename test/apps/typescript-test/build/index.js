d8d455b6868d1661eb614ca45f4bcf642e8233b0
Ib Green <ib@unfolded.ai>
2021-11-05 08:58:30 -0700

    chore: Add typescript tests (#1908)

---
+++ test/apps/typescript-test/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const shapefile_1 = require("@loaders.gl/shapefile");
+(() => {
+    if (shapefile_1.SHPLoader) {
+        console.log('compiled');
+    }
+})();
