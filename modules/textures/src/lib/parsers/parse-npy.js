73ab4976b10e532b84f1221cf1b9f35d2e9c6e98
Ib Green <ib@unfolded.ai>
2021-11-08 16:11:43 -0800

    wip

--- modules/textures/src/lib/parsers/parse-npy.js
+++ modules/textures/src/lib/parsers/parse-npy.js
@@ -3,9 +3,7 @@ function systemIsLittleEndian() {
   const b = new Uint8Array(a.buffer, a.byteOffset, a.byteLength);
   return !(b[0] === 0x12);
 }
-
 const LITTLE_ENDIAN_OS = systemIsLittleEndian();
-
 // The basic string format consists of 3 characters:
 // 1. a character describing the byteorder of the data (<: little-endian, >: big-endian, |: not-relevant)
 // 2. a character code giving the basic type of the array
@@ -24,15 +22,12 @@ const DTYPES = {
   f4: Float32Array,
   f8: Float64Array
 };
-
 export function parseNPY(arrayBuffer, options) {
   if (!arrayBuffer) {
     return null;
   }
-
   const view = new DataView(arrayBuffer);
   const {header, headerEndOffset} = parseHeader(view);
-
   const numpyType = header.descr;
   const ArrayType = DTYPES[numpyType.slice(1, 3)];
   if (!ArrayType) {
@@ -40,34 +35,28 @@ export function parseNPY(arrayBuffer, options) {
     console.warn(`Decoding of npy dtype not implemented: ${numpyType}`);
     return null;
   }
-
-  const nArrayElements = header.shape.reduce((a, b) => a * b);
+  const nArrayElements = header.shape?.reduce((a, b) => a * b);
   const arrayByteLength = nArrayElements * ArrayType.BYTES_PER_ELEMENT;
-
   const data = new ArrayType(arrayBuffer.slice(headerEndOffset, headerEndOffset + arrayByteLength));
-
   // Swap endianness if needed
   if ((numpyType[0] === '>' && LITTLE_ENDIAN_OS) || (numpyType[0] === '<' && !LITTLE_ENDIAN_OS)) {
     // eslint-disable-next-line no-console, no-undef
     console.warn('Data is wrong endianness, byte swapping not yet implemented.');
   }
-
   return {
     data,
     header
   };
 }
-
 /**
  * Parse NPY header
  *
- * @param  {DataView} view
- * @return {Object}
+ * @param  view
+ * @return
  */
 function parseHeader(view) {
   const majorVersion = view.getUint8(6);
   // const minorVersion = view.getUint8(7);
-
   let offset = 8;
   let headerLength;
   if (majorVersion >= 2) {
@@ -77,13 +66,11 @@ function parseHeader(view) {
     headerLength = view.getUint16(8, true);
     offset += 2;
   }
-
   const encoding = majorVersion <= 2 ? 'latin1' : 'utf-8';
   const decoder = new TextDecoder(encoding);
   const headerArray = new Uint8Array(view.buffer, offset, headerLength);
   const headerText = decoder.decode(headerArray);
   offset += headerLength;
-
   const header = JSON.parse(
     headerText
       .replace(/'/g, '"')
@@ -91,6 +78,5 @@ function parseHeader(view) {
       .replace('(', '[')
       .replace(/,*\),*/g, ']')
   );
-
   return {header, headerEndOffset: offset};
 }
