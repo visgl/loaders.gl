fa45a65af4b8647f19b468b6c7a6e61a76955839
Felix Palmer <felixpalmer@gmail.com>
2021-12-08 18:27:22 +0100

    Cover all geometry types in test

--- modules/gis/test/geojson-to-flatGeojson.spec.js
+++ modules/gis/test/geojson-to-flatGeojson.spec.js
@@ -20,5 +20,75 @@ test.only('gis#geojson-to-flatGeojson', async (t) => {
     );
   });
 
+  // Point
+  t.deepEquals(point.geometry.data, [100, 0], 'flat Point data should be equivalent');
+  t.deepEquals(point.geometry.lines, [0], 'flat Point lines should be equivalent');
+
+  // MultiPoint
+  t.deepEquals(
+    multiPoint.geometry.data,
+    [100, 0, 101, 1],
+    'flat MultiPoint data should be equivalent'
+  );
+  t.deepEquals(multiPoint.geometry.lines, [0, 2], 'flat MultiPoint lines should be equivalent');
+
+  // LineString
+  t.deepEquals(
+    lineString.geometry.data,
+    [100, 0, 101, 1],
+    'flat LineString data should be equivalent'
+  );
+  t.deepEquals(lineString.geometry.lines, [0], 'flat LineString lines should be equivalent');
+
+  // MultiLineString
+  t.deepEquals(
+    multiLineString.geometry.data,
+    [100, 0, 101, 1, 102, 2, 103, 3],
+    'flat MultiLineString data should be equivalent'
+  );
+  t.deepEquals(
+    multiLineString.geometry.lines,
+    [0, 4],
+    'flat MultiLineString data should be equivalent'
+  );
+
+  // Polygon
+  t.deepEquals(
+    polygon.geometry.data,
+    [100, 0, 101, 0, 101, 1, 100, 1, 100, 0],
+    'flat Polygon data should be equivalent'
+  );
+  t.deepEquals(polygon.geometry.lines, [0], 'flat Polygon data should be equivalent');
+
+  // Polygon (hole)
+  t.deepEquals(
+    polygonWithHole.geometry.data,
+    [
+      100, 0, 101, 0, 101, 1, 100, 1, 100, 0, 100.8, 0.8, 100.8, 0.2, 100.2, 0.2, 100.2, 0.8, 100.8,
+      0.8
+    ],
+    'flat Polygon (hole) data should be equivalent'
+  );
+  t.deepEquals(
+    polygonWithHole.geometry.lines,
+    [0, 10],
+    'flat Polygon (hole) data should be equivalent'
+  );
+
+  // MultiPolygon
+  t.deepEquals(
+    multiPolygon.geometry.data,
+    [
+      102, 2, 103, 2, 103, 3, 102, 3, 102, 2, 100, 0, 101, 0, 101, 1, 100, 1, 100, 0, 100.2, 0.2,
+      100.2, 0.8, 100.8, 0.8, 100.8, 0.2, 100.2, 0.2
+    ],
+    'flat MultiPolygon data should be equivalent'
+  );
+  t.deepEquals(
+    multiPolygon.geometry.lines,
+    [0, 10, 20],
+    'flat MultiPolygon data should be equivalent'
+  );
+
   t.end();
 });
