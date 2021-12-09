fb2f91545ad4f3d5b39124b2756a0dd777b47a1a
Felix Palmer <felixpalmer@gmail.com>
2021-12-09 11:02:00 +0100

    Comparison test for new/old geojsonToBinary

--- modules/gis/src/index.ts
+++ modules/gis/src/index.ts
@@ -1,7 +1,7 @@
 // Types from `@loaders.gl/schema`
 
 // Functions
-export {geojsonToBinary} from './lib/geojson-to-binary';
+export {geojsonToBinary, geojsonToBinaryOld} from './lib/geojson-to-binary';
 export {geojsonToFlatGeojson} from './lib/geojson-to-flatGeojson';
 export {binaryToGeojson, binaryToGeoJson, binaryToGeometry} from './lib/binary-to-geojson';
 export {transformBinaryCoords, transformGeoJsonCoords} from './lib/transform';
--- modules/mvt/src/index.ts
+++ modules/mvt/src/index.ts
@@ -1,1 +1,3 @@
 export {MVTLoader, MVTWorkerLoader} from './mvt-loader';
+export {featuresToBinary} from './lib/binary-vector-tile/features-to-binary';
+export {classifyRings} from './helpers/binary-util-functions';
