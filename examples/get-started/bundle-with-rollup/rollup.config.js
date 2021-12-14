f58e2ecd2595cac7ff314a104710835506984769
Ib Green <ib@unfolded.ai>
2021-12-13 17:09:21 -0800

    rollup fixes (#2011)

--- examples/get-started/bundle-with-rollup/rollup.config.js
+++ examples/get-started/bundle-with-rollup/rollup.config.js
@@ -1,7 +1,21 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import alias from '@rollup/plugin-alias';
+import typescript from '@rollup/plugin-typescript';
+import fs from 'fs';
 
-export default {
+/** Run against local source */
+const getAliases = async (frameworkName, frameworkRootDir) => {
+  const modules = await fs.promises.readdir(`${frameworkRootDir}/modules`)
+  const aliases = {}
+  modules.forEach(module => {
+    aliases[`${frameworkName}/${module}`] = `${frameworkRootDir}/modules/${module}/src`;
+  })
+  console.log(aliases);
+  return aliases
+}
+
+export default async () => ({
   input: 'app.js',
   output: {
     file: 'dist/bundle.js',
@@ -9,10 +23,14 @@ export default {
     sourcemap: true
   },
   plugins: [
+    typescript(),
+    alias({
+      entries: {} // await getAliases('@loaders.gl', `${__dirname}/../../..`)
+    }),
     resolve({
       browser: true,
       preferBuiltins: false
     }),
     commonjs()
   ]
-};
+});
--- examples/get-started/bundle-with-vite/app.ts
+++ examples/get-started/bundle-with-vite/app.ts
@@ -1,20 +1,20 @@
 import {parse} from '@loaders.gl/core';
-import {OBJLoader} from '@loaders.gl/obj';
+import {GLTFLoader} from '@loaders.gl/gltf';
 import {CompressedTextureLoader} from '@loaders.gl/textures';
 
 const TEXTURE_URL =
-  'https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/textures/test/data/shannon-astc-12x12.pvr';
+  'https://raw.githubusercontent.com/visgl/loaders.gl/master/modules/textures/test/data/shannon-astc-12x12.pvr';
 
-const OBJ_URL =
-  'https://raw.githubusercontent.com/uber-web/loaders.gl/master/modules/obj/test/data/cube.obj';
+const GLTF_URL =
+  'https://raw.githubusercontent.com/visgl/loaders.gl/master/modules/gltf/test/data/glb/DamagedHelmet.glb';
 
 const getTextureBytes = (texture) => texture.map(level => level.data.byteLength).join(',')
 
 async function main() {
   const texture = await parse(fetch(TEXTURE_URL), CompressedTextureLoader);
-  const data = await parse(fetch(OBJ_URL), OBJLoader);
+  const data = await parse(fetch(GLTF_URL), GLTFLoader, {gltf: {postProcess: false}});
   document.getElementById('content').innerHTML = 
-    `Texture: [${getTextureBytes(texture)}] bytes <br/> <br/>OBJ Mesh = ` + JSON.stringify(data, null, 2);
+    `Texture: [${getTextureBytes(texture)}] bytes <br/> <br/>GLTF = ` + JSON.stringify(data.json, null, 2);
 }
 
 main();
