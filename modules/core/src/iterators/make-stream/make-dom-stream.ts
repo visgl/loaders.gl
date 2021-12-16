1af70ab2be42902e0d86331d4d88cbc3b630881e
Ib Green <ib@unfolded.ai>
2021-12-16 10:20:32 -0800

    fix: Alternate approach to bundler issues (#2026)
    Co-authored-by: Felix Palmer <felixpalmer@gmail.com>

--- modules/core/src/iterators/make-stream/make-dom-stream.ts
+++ modules/core/src/iterators/make-stream/make-dom-stream.ts
@@ -1,14 +1,19 @@
-export type MakeDOMStreamOptions = {
+export type MakeStreamOptions = {
   /** Stream allocates an arrayBuffer. Enables use of a default reader. */
   autoAllocateChunkSize?: number;
   /** Total number of chunks in queue before back pressure is applied */
   highWaterMark?: number;
 };
 
-/** Builds a DOM stream from an iterator */
-export function makeDOMStream<ArrayBuffer>(
+/**
+ * Builds a DOM stream from an iterator
+ * This stream is currently used in browsers only,
+ * but note that Web stream support is present in Node from Node 16
+ * https://nodejs.org/api/webstreams.html#webstreams_web_streams_api
+ */
+export function makeStream<ArrayBuffer>(
   source: Iterable<ArrayBuffer> | AsyncIterable<ArrayBuffer>,
-  options?: MakeDOMStreamOptions
+  options?: MakeStreamOptions
 ): ReadableStream {
   const iterator = source[Symbol.asyncIterator]
     ? (source as AsyncIterable<ArrayBuffer>)[Symbol.asyncIterator]()
