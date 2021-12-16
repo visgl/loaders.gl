fa135807935f4329ccd9108c4a38cff036d835a9
Ib Green <ib@unfolded.ai>
2021-12-16 05:59:14 -0800

    wip

--- modules/core/src/iterators/make-stream/make-dom-stream.ts
+++ modules/core/src/iterators/make-stream/make-dom-stream.ts
@@ -5,7 +5,12 @@ export type MakeStreamOptions = {
   highWaterMark?: number;
 };
 
-/** Builds a DOM stream from an iterator */
+/** 
+ * Builds a DOM stream from an iterator 
+ * This stream is currently used in browsers only, 
+ * but note that Web stream support is present in Node from Node 16
+ * https://nodejs.org/api/webstreams.html#webstreams_web_streams_api
+ */
 export function makeStream<ArrayBuffer>(
   source: Iterable<ArrayBuffer> | AsyncIterable<ArrayBuffer>,
   options?: MakeStreamOptions
