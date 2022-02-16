734665c3c60a6c69a7c4b1003cc13e891503e695
Igor Dykhta <igorDykhta@users.noreply.github.com>
2022-02-16 12:27:09 +0200

    fix(terrain): swap winding order of skirts to match winding order of terrain (#2085)

--- modules/terrain/test/lib/helpers/skirt.spec.js
+++ modules/terrain/test/lib/helpers/skirt.spec.js
@@ -12,7 +12,7 @@ test('TerrainLoader-skirting#addSkirt', (t) => {
     }
   };
   const triangles = new Uint16Array([0, 1, 2, 0, 2, 3]);
-  addSkirt(attributes, triangles, 20);
+  const addSkirtResult = addSkirt(attributes, triangles, 20);
   // prettier-ignore
   t.deepEqual(attributes.POSITION.value, 
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, -17, 4, 5, -14, 10, 11, -8, 1, 2, -17, 4, 5, -14, 7, 8, -11, 7, 8, -11, 10, 11, -8], 
@@ -21,6 +21,11 @@ test('TerrainLoader-skirting#addSkirt', (t) => {
     attributes.TEXCOORD_0.value,
     [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 7, 8, 1, 2, 3, 4, 5, 6, 5, 6, 7, 8],
     'Make correct TEXCOORD_0 attribute'
+  );
+  t.deepEqual(
+    addSkirtResult.triangles,
+    [0, 1, 2, 0, 2, 3, 0, 5, 1, 5, 0, 4, 3, 7, 0, 7, 3, 6, 1, 9, 2, 9, 1, 8, 2, 11, 3, 11, 2, 10],
+    'Make correct indices array of the mesh geometry'
   );
   t.end();
 });
