604dc13744ed97148d969d6f38ae279f5ef7aa16
Kyle Barron <kbarron@foursquare.com>
2022-03-01 13:25:56 -0700

    Prettier

--- modules/schema/src/category/table/table-types.ts
+++ modules/schema/src/category/table/table-types.ts
@@ -43,7 +43,7 @@ export interface ArrowTable extends Table {
 
 /** A collection of tables */
 export type Tables<TableType extends Table = Table> = {
-  shape: 'tables',
+  shape: 'tables';
   tables: TableType[];
 };
 
