99c6ea4fd2f5bae78284701f71a07079677ab8b9
Kyle Barron <kbarron@foursquare.com>
2022-03-03 15:27:28 -0700

    Add Geopackage to geospatial example (#2100)

--- modules/geopackage/src/lib/parse-geopackage.ts
+++ modules/geopackage/src/lib/parse-geopackage.ts
@@ -30,7 +30,8 @@ import {
   DataColumnsRow,
   DataColumnsMapping,
   PragmaTableInfoRow,
-  SQLiteTypes
+  SQLiteTypes,
+  GeoPackageGeometryTypes
 } from './types';
 
 // https://www.geopackage.org/spec121/#flags_layout
@@ -47,7 +48,7 @@ const ENVELOPE_BYTE_LENGTHS = {
 };
 
 // Documentation: https://www.geopackage.org/spec130/index.html#table_column_data_types
-const SQL_TYPE_MAPPING: {[type in SQLiteTypes]: typeof DataType} = {
+const SQL_TYPE_MAPPING: {[type in SQLiteTypes | GeoPackageGeometryTypes]: typeof DataType} = {
   BOOLEAN: Bool,
   TINYINT: Int8,
   SMALLINT: Int16,
@@ -61,7 +62,14 @@ const SQL_TYPE_MAPPING: {[type in SQLiteTypes]: typeof DataType} = {
   BLOB: Binary,
   DATE: Utf8,
   DATETIME: Utf8,
-  GEOMETRY: Binary
+  GEOMETRY: Binary,
+  POINT: Binary,
+  LINESTRING: Binary,
+  POLYGON: Binary,
+  MULTIPOINT: Binary,
+  MULTILINESTRING: Binary,
+  MULTIPOLYGON: Binary,
+  GEOMETRYCOLLECTION: Binary
 };
 
 export default async function parseGeoPackage(
@@ -463,7 +471,8 @@ function getArrowSchema(db: Database, tableName: string): Schema {
   while (stmt.step()) {
     const pragmaTableInfo = stmt.getAsObject() as unknown as PragmaTableInfoRow;
     const {name, type, notnull} = pragmaTableInfo;
-    const field = new Field(name, new SQL_TYPE_MAPPING[type](), !notnull);
+    const schemaType = SQL_TYPE_MAPPING[type] && new SQL_TYPE_MAPPING[type]();
+    const field = new Field(name, schemaType, !notnull);
     fields.push(field);
   }
 
