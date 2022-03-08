26317f7a066032a1c833810e0224d3c22a988b4c
Kyle Barron <kbarron@foursquare.com>
2022-03-08 14:55:15 -0700

    Update parquet wasm tests

--- modules/parquet/test/data/files.js
+++ modules/parquet/test/data/files.js
@@ -1,47 +1,55 @@
 const PARQUET_FILES = [
   // Parquet seems to use LZ4 compression without header so we need to find a way to e.g. add a dummy header.
-  {supported: true, title: 'lz4_raw_compressed', path: 'good/lz4_raw_compressed.parquet'},
-  {supported: true, title: 'lz4_raw_compressed_larger', path: 'good/lz4_raw_compressed_larger.parquet'},
-  {supported: true, title: 'non_hadoop_lz4_compressed', path: 'good/non_hadoop_lz4_compressed.parquet'},
-  {supported: true, title: 'alltypes_dictionary', path: 'good/alltypes_dictionary.parquet'},
-  {supported: true, title: 'alltypes_plain', path: 'good/alltypes_plain.parquet'},
-  {supported: true, title: 'alltypes_plain_snappy', path: 'good/alltypes_plain.snappy.parquet'},
-  {supported: true, title: 'binary', path: 'good/binary.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'lz4_raw_compressed', path: 'good/lz4_raw_compressed.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'lz4_raw_compressed_larger', path: 'good/lz4_raw_compressed_larger.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'non_hadoop_lz4_compressed', path: 'good/non_hadoop_lz4_compressed.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'alltypes_dictionary', path: 'good/alltypes_dictionary.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'alltypes_plain', path: 'good/alltypes_plain.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'alltypes_plain_snappy', path: 'good/alltypes_plain.snappy.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'binary', path: 'good/binary.parquet'},
   // Specialized Dict for very large dictionaries: https://github.com/apache/parquet-format/blob/master/BloomFilter.md
-  {supported: false, title: 'bloom_filter', path: 'good/bloom_filter.bin'}, 
-  {supported: true, title: 'byte_array_decimal', path: 'good/byte_array_decimal.parquet'},
-  {supported: false, title: 'datapage_v2', path: 'good/datapage_v2.snappy.parquet'}, // Doesn't work on parquet-tools
-  {supported: true, title: 'dict', path: 'good/dict-page-offset-zero.parquet'},
-  {supported: true, title: 'fixed_length_decimal', path: 'good/fixed_length_decimal.parquet'},
-  {supported: true, title: 'fixed_length_decimal_legacy', path: 'good/fixed_length_decimal_legacy.parquet'},
-  {supported: false, title: 'hadoop_lz4_compressed', path: 'good/hadoop_lz4_compressed.parquet'},
-  {supported: false, title: 'hadoop_lz4_compressed_larger', path: 'good/hadoop_lz4_compressed_larger.parquet'},
-  {supported: true, title: 'int32_decimal', path: 'good/int32_decimal.parquet'},
-  {supported: true, title: 'int64_decimal', path: 'good/int64_decimal.parquet'},
-  {supported: true, title: 'list_columns', path: 'good/list_columns.parquet'},
-  {supported: true, title: 'nation', path: 'good/nation.dict-malformed.parquet'}, // Partially works, issue with indices for dictionary values.
-  {supported: true, title: 'nested_lists', path: 'good/nested_lists.snappy.parquet'},
-  {supported: true, title: 'nested_maps', path: 'good/nested_maps.snappy.parquet'},
-  {supported: false, title: 'nested_structs', path: 'good/nested_structs.rust.parquet'},
-  {supported: true, title: 'nonnullable', path: 'good/nonnullable.impala.parquet'},
-  {supported: true, title: 'nullable', path: 'good/nullable.impala.parquet'},
-  {supported: true, title: 'nulls', path: 'good/nulls.snappy.parquet'},
-  {supported: true, title: 'repeated_no_annotation', path: 'good/repeated_no_annotation.parquet'},
-  {supported: false, title: 'single_nan', path: 'good/single_nan.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'bloom_filter', path: 'good/bloom_filter.bin'},
+  {supportedJs: true, supportedWasm: true, title: 'byte_array_decimal', path: 'good/byte_array_decimal.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'datapage_v2', path: 'good/datapage_v2.snappy.parquet'}, // Doesn't work on parquet-tools
+  {supportedJs: true, supportedWasm: false, title: 'dict', path: 'good/dict-page-offset-zero.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'fixed_length_decimal', path: 'good/fixed_length_decimal.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'fixed_length_decimal_legacy', path: 'good/fixed_length_decimal_legacy.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'hadoop_lz4_compressed', path: 'good/hadoop_lz4_compressed.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'hadoop_lz4_compressed_larger', path: 'good/hadoop_lz4_compressed_larger.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'int32_decimal', path: 'good/int32_decimal.parquet'},
+  {supportedJs: true, supportedWasm: true, title: 'int64_decimal', path: 'good/int64_decimal.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'list_columns', path: 'good/list_columns.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'nation', path: 'good/nation.dict-malformed.parquet'}, // Partially works, issue with indices for dictionary values.
+  {supportedJs: true, supportedWasm: false, title: 'nested_lists', path: 'good/nested_lists.snappy.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'nested_maps', path: 'good/nested_maps.snappy.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'nested_structs', path: 'good/nested_structs.rust.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'nonnullable', path: 'good/nonnullable.impala.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'nullable', path: 'good/nullable.impala.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'nulls', path: 'good/nulls.snappy.parquet'},
+  {supportedJs: true, supportedWasm: false, title: 'repeated_no_annotation', path: 'good/repeated_no_annotation.parquet'},
+  {supportedJs: false, supportedWasm: true, title: 'single_nan', path: 'good/single_nan.parquet'},
+
+  {supportedJs: false, supportedWasm: true, title: 'data_index_bloom_encoding_stats', path: 'good/data_index_bloom_encoding_stats.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'delta_binary_packed', path: 'good/delta_binary_packed.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'delta_byte_array', path: 'good/delta_byte_array.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'delta_encoding_optional_column', path: 'good/delta_encoding_optional_column.parquet'},
+  {supportedJs: false, supportedWasm: false, title: 'delta_encoding_required_column', path: 'good/delta_encoding_required_column.parquet'},
 
   // Encrypted
-  {supported: false, encrypted: true, title: 'uniform_encryption', path: 'good/uniform_encryption.parquet.encrypted'},
-  {supported: false, encrypted: true, title: 'encrypt_columns_and_footer', path: 'encrypted/encrypt_columns_and_footer.parquet.encrypted'},
-  {supported: false, encrypted: true, title: 'encrypt_columns_and_footer_aad', path: 'encrypted/encrypt_columns_and_footer_aad.parquet.encrypted'},
-  {supported: false, encrypted: true, title: 'encrypt_columns_and_footer_ctr', path: 'encrypted/encrypt_columns_and_footer_ctr.parquet.encrypted'},
-  {supported: false, encrypted: true, title: 'encrypt_columns_and_footer_disable_aad_storage', path: 'encrypted/encrypt_columns_and_footer_disable_aad_storage.parquet.encrypted'},
-  {supported: false, encrypted: true,  title: 'encrypt_columns_plaintext_footer', path: 'encrypted/encrypt_columns_plaintext_footer.parquet.encrypted'},
+  {supportedJs: false, encrypted: true, supportedWasm: false, title: 'uniform_encryption', path: 'good/uniform_encryption.parquet.encrypted'},
+  {supportedJs: false, encrypted: true, supportedWasm: false, title: 'encrypt_columns_and_footer', path: 'encrypted/encrypt_columns_and_footer.parquet.encrypted'},
+  {supportedJs: false, encrypted: true, supportedWasm: false, title: 'encrypt_columns_and_footer_aad', path: 'encrypted/encrypt_columns_and_footer_aad.parquet.encrypted'},
+  {supportedJs: false, encrypted: true, supportedWasm: false, title: 'encrypt_columns_and_footer_ctr', path: 'encrypted/encrypt_columns_and_footer_ctr.parquet.encrypted'},
+  {supportedJs: false, encrypted: true, supportedWasm: false, title: 'encrypt_columns_and_footer_disable_aad_storage', path: 'encrypted/encrypt_columns_and_footer_disable_aad_storage.parquet.encrypted'},
+  {supportedJs: false, encrypted: true,  supportedWasm: false, title: 'encrypt_columns_plaintext_footer', path: 'encrypted/encrypt_columns_plaintext_footer.parquet.encrypted'},
 
   // Illegal
-  {supported: false, bad: true, title: 'PARQUET-1481', path: 'illegal/PARQUET-1481.parquet'},
+  {supportedJs: false, bad: true, title: 'PARQUET-1481', path: 'illegal/PARQUET-1481.parquet'},
 ];
 
-export const SUPPORTED_FILES = PARQUET_FILES.filter(file => file.supported);
-export const UNSUPPORTED_FILES = PARQUET_FILES.filter(file => !file.supported && !file.encrypted && !file.bad);
-export const ENCRYPTED_FILES = PARQUET_FILES.filter(file => !file.supported && file.encrypted);
+export const SUPPORTED_FILES = PARQUET_FILES.filter(file => file.supportedJs);
+export const UNSUPPORTED_FILES = PARQUET_FILES.filter(file => !file.supportedJs && !file.encrypted && !file.bad);
+export const ENCRYPTED_FILES = PARQUET_FILES.filter(file => !file.supportedJs && file.encrypted);
 export const BAD_FILES = PARQUET_FILES.filter(file => file.bad)
+
+export const WASM_SUPPORTED_FILES = PARQUET_FILES.filter(file => file.supportedWasm);
--- modules/parquet/test/parquet-wasm-loader.spec.ts
+++ modules/parquet/test/parquet-wasm-loader.spec.ts
@@ -1,11 +1,12 @@
 import test from 'tape-promise/tape';
 // import {validateLoader} from 'test/common/conformance';
 
-import {ParquetWasmLoader} from '@loaders.gl/parquet';
-import {load, setLoaderOptions} from '@loaders.gl/core';
-import type {Table} from 'apache-arrow';
+import {ParquetWasmLoader, ParquetWasmWriter} from '@loaders.gl/parquet';
+import {load, encode, setLoaderOptions} from '@loaders.gl/core';
+import {Table, Builder, Utf8, Bool} from 'apache-arrow';
+import {WASM_SUPPORTED_FILES} from './data/files';
 
-const PARQUET_DIR = '@loaders.gl/parquet/test/data/geoparquet';
+const PARQUET_DIR = '@loaders.gl/parquet/test/data/apache';
 
 setLoaderOptions({
   _workerType: 'test'
@@ -19,7 +20,7 @@ test('ParquetLoader#loader objects', (t) => {
 });
 
 test('Load Parquet file', async (t) => {
-  const url = `${PARQUET_DIR}/example.parquet`;
+  const url = '@loaders.gl/parquet/test/data/geoparquet/example.parquet';
   const table: Table = await load(url, ParquetWasmLoader, {
     geoparquet: {
       cdn: null
@@ -31,3 +32,46 @@ test('Load Parquet file', async (t) => {
     [ 'pop_est', 'continent', 'name', 'iso_a3', 'gdp_md_est', 'geometry' ]);
   t.end();
 })
+
+test('ParquetWasmLoader#load', async (t) => {
+  t.comment('SUPPORTED FILES');
+  for (const {title, path} of WASM_SUPPORTED_FILES) {
+    const url = `${PARQUET_DIR}/${path}`;
+    const data = await load(url, ParquetWasmLoader, {});
+    t.ok(data, `GOOD(${title})`);
+  }
+
+  t.end();
+})
+
+test.only('ParquetWasmWriterLoader round trip', async (t) => {
+  const table = createArrowTable();
+  const parquetBuffer = await encode(table, ParquetWasmWriter);
+  const newTable = await load(parquetBuffer, ParquetWasmLoader);
+
+  t.deepEqual(table.schema, newTable.schema);
+})
+
+function createArrowTable() {
+  const utf8Builder = Builder.new({
+    type: new Utf8(),
+  });
+  utf8Builder.append('a').append('b').append('c').append('d');
+
+  const utf8Vector = utf8Builder.finish().toVector();
+
+  const boolBuilder = Builder.new({
+    type: new Bool(),
+  });
+  boolBuilder.append(true).append(true).append(false).append(false);
+
+  const boolVector = boolBuilder.finish().toVector();
+
+  const table = Table.from({
+    str: utf8Vector,
+    uint8: new Uint8Array([1, 2, 3, 4]),
+    int32: new Int32Array([0, -2147483638, 2147483637, 1]),
+    bool: boolVector,
+  });
+  return table;
+}
