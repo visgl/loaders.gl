97bf0e77033ca3c1c1aa1128d8711ff5688351e0
Kyle Barron <kbarron@foursquare.com>
2022-03-07 19:16:29 -0700

    wip

--- modules/schema/src/category/common.ts
+++ modules/schema/src/category/common.ts
@@ -14,11 +14,38 @@ export type Batch = {
   recordBatch?: RecordBatch;
   length: number;
   schema?: Schema;
+
+  /** Bytes processed in all batches up to and including this batch */
   bytesUsed?: number;
+
+  /**
+   * Total number of bytes in all batches
+   *
+   * This field is not always available, for instance when fetching compressed data
+   */
   bytesTotal?: number;
+
+  /**
+   * Total number of rows parsed up to and including this batch
+   */
+  rowsUsed?: number;
+
+  /** Total number of rows in all batches
+   *
+   * This field is not always available
+   */
+  rowsTotal?: number;
+
+  /** @deprecated Use bytesUsed or rowsUsed */
   count?: number;
+
+  /** @deprecated */
   cursor?: number;
 };
+
+type ArrowBatch = Batch & {
+  recordBatch: RecordBatch
+}
 
 /*
 export type Batch = {
