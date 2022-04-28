ede990ab47e4cb35227facdefc1a47f8acac8311
Kyle Barron <kbarron@foursquare.com>
2022-04-28 13:07:31 -0600

    Remove ts-ignore

--- modules/core/src/iterators/make-stream/make-dom-stream.ts
+++ modules/core/src/iterators/make-stream/make-dom-stream.ts
@@ -34,7 +34,6 @@ export function makeStream<ArrayBuffer>(
             controller.close();
           } else {
             // TODO - ignores controller.desiredSize
-            // @ts-ignore
             controller.enqueue(new Uint8Array(value));
           }
         } catch (error) {
