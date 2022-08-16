944e360de6ca38425768c502a64227db40b45080
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2022-08-16 14:19:25 +0300

    feat(tile-converter): add feature-attributes helper

--- modules/tile-converter/src/i3s-converter/helpers/geometry-converter.ts
+++ modules/tile-converter/src/i3s-converter/helpers/geometry-converter.ts
@@ -35,6 +35,7 @@ import {
 import {B3DMAttributesData /*transformI3SAttributesOnWorker */} from '../../i3s-attributes-worker';
 import {prepareDataForAttributesConversion} from './gltf-attributes';
 import {handleBatchIdsExtensions} from './batch-ids-extensions';
+import {checkPropertiesLength, flattenPropertyTableByFeatureIds} from './feature-attributes';
 
 // Spec - https://github.com/Esri/i3s-spec/blob/master/docs/1.7/pbrMetallicRoughness.cmn.md
 const DEFAULT_ROUGHNESS_FACTOR = 1;
@@ -1096,67 +1097,6 @@ function convertPropertyTableToAttributeBuffers(
   }
 
   return attributeBuffers;
-}
-
-/**
- * Check that all attributes in propertyTable have the same length as FeatureIds.
- * If there are differencies between lengths we should flatten property table based on exiesting featureIds.
- * @param featureIds
- * @param propertyTable
- * @returns
- */
-function checkPropertiesLength(featureIds: number[], propertyTable: FeatureTableJson): boolean {
-  let needFlatten = false;
-
-  for (const attribute of Object.values(propertyTable)) {
-    if (featureIds.length !== attribute.length) {
-      needFlatten = true;
-    }
-  }
-
-  return needFlatten;
-}
-
-/**
- * Takes attributes from property table based on featureIds.
- * If there is no property value for particular featureId (index) the property will be null.
- * Example:
- * Initial data:
- *   OBJECTID: [0, 1, 5]
- *   component: ['Windows', 'Frames', 'Wall', 'Roof', 'Skylight']
- * Result:
- *   OBJECTID: [0, 1, 5]
- *   component: ['Windows', 'Frames', 'null']
- * @param featureIds
- * @param propertyTable
- */
-function flattenPropertyTableByFeatureIds(
-  featureIds: number[],
-  propertyTable: FeatureTableJson
-): FeatureTableJson {
-  const resultPropertyTable: FeatureTableJson = {};
-  for (const propertyName in propertyTable) {
-    const properties = propertyTable[propertyName];
-    resultPropertyTable[propertyName] = getPropertiesByFeatureIds(properties, featureIds);
-  }
-
-  return resultPropertyTable;
-}
-
-/**
- * Getting properties by featureId index
- * @param properties
- * @param featureIds
- */
-function getPropertiesByFeatureIds(properties: any[], featureIds: number[]): any[] {
-  const resultProperties: any = [];
-
-  for (const featureId of featureIds) {
-    const property = properties[featureId] || null;
-    resultProperties.push(property);
-  }
-
-  return resultProperties;
 }
 
 /**
