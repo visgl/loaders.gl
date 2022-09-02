e855d0f946a56d391bc51b09675f694b5fd16dcf
Victor Belomestnov <belom88@yandex.ru>
2022-09-02 11:35:54 +0200

    fix(tile-converter): install deps test

--- modules/tile-converter/test/deps-installer/deps-installer.spec.js
+++ modules/tile-converter/test/deps-installer/deps-installer.spec.js
@@ -7,10 +7,14 @@ import {cleanUpPath, isFileExists} from '../utils/file-utils';
 test('tile-converter Install dependencies', async (t) => {
   if (!isBrowser) {
     const depsInstaller = new DepsInstaller();
-    await depsInstaller.install('tmp');
+    await depsInstaller.install('tmp', 'tmp-workers');
 
     t.ok(await isFileExists('tmp/egm2008-5.pgm'));
+    t.ok(await isFileExists('tmp-workers/modules/i3s/dist/i3s-content-nodejs-worker.js'));
+    t.ok(await isFileExists('tmp-workers/modules/draco/dist/draco-nodejs-worker.js'));
+    t.ok(await isFileExists('tmp-workers/modules/textures/dist/basis-nodejs-worker.js'));
   }
   await cleanUpPath('tmp');
+  await cleanUpPath('tmp-workers');
   t.end();
 });
