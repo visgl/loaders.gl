254ef076b11967cee25cea91e213bfba9a35ddef
Ib Green <ib@unfolded.ai>
2022-11-17 14:19:13 -0500

    feat(mvt): automatic tile generation from GeoJSON (geojson-vt fork).

--- modules/mvt/src/index.ts
+++ modules/mvt/src/index.ts
@@ -1,1 +1,8 @@
+// loaders.gl, MIT license
+
 export {MVTLoader, MVTWorkerLoader} from './mvt-loader';
+
+// GeoJSONTiler
+
+export type {GeoJSONTilerOptions} from './lib/geojson-vt/geojson-tiler';
+export {GeoJSONTiler} from './lib/geojson-vt/geojson-tiler';
