318cb3a1f153a517aa18ea1c0bde5e352097e304
mspivak-actionengine <michael.spivak@actionengine.com>
2022-12-18 15:09:08 +0300

    fix(tile-converter): instant writing in queue

--- modules/tile-converter/src/lib/utils/write-queue.ts
+++ modules/tile-converter/src/lib/utils/write-queue.ts
@@ -6,6 +6,22 @@ const MEMORY_LIMIT = 4 * 1024 * 1024 * 1024; // 4GB
 
 export type WriteQueueItem = {
   archiveKey?: string;
+  /**
+   * writePromise() returns a Promise that will be awaited in Promise.allSettled(promises);
+   * Arguments for this call are specified in writeQueue.enqueue call like this:
+   * await writeQueue.enqueue({
+   *     archiveKey: `nodePages/xxx.json.gz`,
+   *     writePromise: () => writeFileForSlpk(slpkPath, data, `xxx.json`)
+   * });
+   * Note, a function like writeFileForSlpk should NOT be called when initializing the object for enqueue().
+   * If he function is called, the promise will be created
+   * and the function will allocate resources (file descriptors) for file writing.
+   * It will be done for ALL items in the queue, which is not supposed to happen.
+   * That's why the function should be passed as
+   *   writePromise: () => writeFileForSlpk(slpkPath, content, `xxx.json`)
+   * instead of
+   *  writePromise: writeFileForSlpk(slpkPath, content, `xxx.json`) // INCORRECT !
+   */
   writePromise: () => Promise<string>;
 };
 
@@ -65,20 +81,6 @@ export default class WriteQueue<T extends WriteQueueItem> extends Queue<T> {
         const {archiveKey, writePromise} = item as WriteQueueItem;
         archiveKeys.push(archiveKey);
         const promise = writePromise();
-        // writePromise() returns a Promise that will be awaited in Promise.allSettled(promises);
-        // Arguments for this call are specified in writeQueue.enqueue call like this:
-        // await writeQueue.enqueue({
-        //   archiveKey: `nodePages/xxx.json.gz`,
-        //   writePromise: () => writeFileForSlpk(slpkPath, data, `xxx.json`)
-        // });
-        // Note, a function like writeFileForSlpk should NOT be called when initializing the object for enqueue().
-        // If he function is called, the promise will be created
-        // and the function will allocate resources (file descriptors) for file writing.
-        // It will be done for ALL items in the queue, which is not supposed to happen.
-        // That's why the function should be passed as
-        //   writePromise: () => writeFileForSlpk(slpkPath, content, `xxx.json`)
-        // instead of
-        //   writePromise: writeFileForSlpk(slpkPath, content, `xxx.json`) // INCORRECT !
         promises.push(promise);
       }
       const writeResults = await Promise.allSettled(promises);
