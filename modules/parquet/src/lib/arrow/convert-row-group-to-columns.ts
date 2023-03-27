c9434794a09ddea9a924900faeb17a2c64de9ba8
Ib Green <ib@unfolded.ai>
2023-03-27 11:34:28 -0400

    wip

--- modules/parquet/src/lib/arrow/convert-row-group-to-columns.ts
+++ modules/parquet/src/lib/arrow/convert-row-group-to-columns.ts
@@ -3,7 +3,10 @@
 import {Schema} from '@loaders.gl/schema';
 import {ParquetBuffer} from '@loaders.gl/parquet/parquetjs/schema/declare';
 
-export function convertParquetRowGroupToColumns(schema: Schema, rowGroup: ParquetBuffer): Record<string, any[]> {
+export function convertParquetRowGroupToColumns(
+  schema: Schema,
+  rowGroup: ParquetBuffer
+): Record<string, any[]> {
   const columns: Record<string, any[]> = {};
   for (const [columnName, data] of Object.entries(rowGroup.columnData)) {
     columns[columnName] = columns[columnName] || data.values;
