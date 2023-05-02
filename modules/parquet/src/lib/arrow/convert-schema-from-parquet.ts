30957c2bc8127a79b00a4688d943f5673cf74fc6
Ib Green <ib@foursquare.com>
2023-05-02 09:46:19 -0400

    feat: Typed loaders (#2448)

--- modules/parquet/src/lib/arrow/convert-schema-from-parquet.ts
+++ modules/parquet/src/lib/arrow/convert-schema-from-parquet.ts
@@ -4,7 +4,7 @@ import {Schema, Field, DataType} from '@loaders.gl/schema';
 
 import type {ParquetSchema} from '../../parquetjs/schema/schema';
 import type {FieldDefinition, ParquetField, ParquetType} from '../../parquetjs/schema/declare';
-import {FileMetaData} from '@loaders.gl/parquet/parquetjs/parquet-thrift';
+import {FileMetaData} from '../../parquetjs/parquet-thrift';
 
 export const PARQUET_TYPE_MAPPING: {[type in ParquetType]: DataType} = {
   BOOLEAN: 'bool',
