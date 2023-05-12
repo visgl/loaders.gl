5c0c51cf01ec09d8a69b1296fbf4a516dcb120d8
Ib Green <ib@unfolded.ai>
2023-05-12 07:19:59 -0400

    lint

--- modules/i3s/src/index.ts
+++ modules/i3s/src/index.ts
@@ -40,4 +40,5 @@ export {SLPKLoader} from './i3s-slpk-loader';
 export {I3SContentLoader} from './i3s-content-loader';
 export {I3SAttributeLoader, loadFeatureAttributes} from './i3s-attribute-loader';
 export {I3SBuildingSceneLayerLoader} from './i3s-building-scene-layer-loader';
+export {I3SNodePageLoader} from './i3s-node-page-loader';
 export {ArcGisWebSceneLoader} from './arcgis-webscene-loader';
