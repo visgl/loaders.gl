64ba21289ef890093c93aaa6010b9a58fdfa2a05
Ib Green <ib@unfolded.ai>
2023-05-20 19:13:26 -0400

    tests

--- modules/polyfills/test/index.ts
+++ modules/polyfills/test/index.ts
@@ -5,8 +5,7 @@ import './fetch-node/headers.node.spec';
 import './fetch-node/response.node.spec';
 import './fetch-node/fetch.node.spec';
 
-import './images-node/images-node.spec';
-
 import './file/blob-polyfill.spec';
 import './file/file-polyfill.spec';
+
 import './promise/all-settled.spec';
