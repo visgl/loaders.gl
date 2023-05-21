9a07253290a5feb831375ac4fff87e2074ab6ece
Ib Green <ib@unfolded.ai>
2023-05-21 07:23:20 -0400

    wip

--- modules/kml/src/index.ts
+++ modules/kml/src/index.ts
@@ -5,7 +5,7 @@ import {isBrowser} from '@loaders.gl/loader-utils';
 import xmldom from '@xmldom/xmldom';
 
 if (!isBrowser && !('DOMParser' in globalThis) && xmldom.DOMParser) {
-  globalThis['DOMParser'] = xmldom.DOMParser;
+  globalThis.DOMParser = xmldom.DOMParser;
 }
 
 export type {GPXLoaderOptions} from './gpx-loader';
