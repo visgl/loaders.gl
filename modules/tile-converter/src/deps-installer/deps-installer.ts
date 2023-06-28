b4dbff40da041bf51f567e7b16c6eaace2dd6caa
Viktor Belomestnov <belom88@yandex.ru>
2023-06-28 09:36:32 +0200

    fix(worker-utils): child process - ignore warnings (#2530)

--- modules/tile-converter/src/deps-installer/deps-installer.ts
+++ modules/tile-converter/src/deps-installer/deps-installer.ts
@@ -50,8 +50,9 @@ export class DepsInstaller {
       // `npm install sharp join-images` works unstable. It fails because installed `sharp` version
       // may be different from the version required by `join-images`. Pointing to specific versions
       // resolve this issue
-      arguments: ['install', 'sharp@^0.30.4', 'join-images@^1.1.3'],
-      wait: 0
+      arguments: ['install', 'sharp@0.30.4', 'join-images@1.1.3'],
+      wait: 0,
+      ignoreStderr: true
     });
 
     console.log('All dependencies were installed succesfully.'); // eslint-disable-line no-console
