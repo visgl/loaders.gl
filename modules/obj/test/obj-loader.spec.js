d1d8ea74fe3f9fb6af17d64f990d0d7340c4a18a
Chris Gervang <chris.gervang@joby.aero>
2023-07-27 14:07:39 -0700

    docs and tests

--- modules/obj/test/obj-loader.spec.js
+++ modules/obj/test/obj-loader.spec.js
@@ -4,11 +4,12 @@ import {validateLoader, validateMeshCategoryData} from 'test/common/conformance'
 
 import {OBJLoader, OBJWorkerLoader} from '@loaders.gl/obj';
 import {setLoaderOptions, load} from '@loaders.gl/core';
+import {equals} from '@math.gl/core';
 
 const OBJ_ASCII_URL = '@loaders.gl/obj/test/data/bunny.obj';
 const OBJ_NORMALS_URL = '@loaders.gl/obj/test/data/cube.obj';
 const OBJ_MULTI_PART_URL = '@loaders.gl/obj/test/data/magnolia.obj';
-const OBJ_VERTEX_COLOR_URL = '@loaders.gl/obj/test/data/cube_vertex_colors.obj';
+const OBJ_VERTEX_COLOR_URL = '@loaders.gl/obj/test/data/cube-vertex-colors.obj';
 
 setLoaderOptions({
   _workerType: 'test'
@@ -87,6 +88,23 @@ test('OBJLoader#parseText - object with vertex colors', async (t) => {
   t.equal(data.attributes.NORMAL.size, 3, 'NORMAL attribute was found');
   t.equal(data.attributes.COLOR_0.value.length, 108, 'COLOR_0 attribute was found');
   t.equal(data.attributes.COLOR_0.size, 3, 'COLOR_0 attribute was found');
+
+  // Test two verticies with different colors.
+  const vertex1Color = [0.2801, 0.4429, 0.8987];
+  t.ok(
+    vertex1Color.every((value, index) =>
+      equals(data.attributes.COLOR_0.value[index], value, 0.0001)
+    ),
+    'vertex 1 color parsed as float rgb'
+  );
+
+  const vertex2Color = [0.6907, 0.2524, 0.8987];
+  t.ok(
+    vertex2Color.every((value, index) =>
+      equals(data.attributes.COLOR_0.value[index + 18], value, 0.0001)
+    ),
+    'vertex 2 color parsed as float rgb'
+  );
   t.end();
 });
 
