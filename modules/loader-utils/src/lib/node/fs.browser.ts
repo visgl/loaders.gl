764b0c1419939a8f289ce011bf89496ad1d411d9
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2023-08-28 15:21:36 -0700

    Try fix bundling error (#2605)

--- modules/loader-utils/src/lib/node/fs.browser.ts
+++ modules/loader-utils/src/lib/node/fs.browser.ts
@@ -1,1 +1,14 @@
+export const readdir = null;
+export const stat = null;
+export const readFile = null;
+export const readFileSync = null;
+export const writeFile = null;
+export const writeFileSync = null;
+export const open = null;
+export const close = null;
+export const read = null;
+export const fstat = null;
+export const createWriteStream = null;
+export const _readToArrayBuffer = null;
+
 export const isSupported = false;
