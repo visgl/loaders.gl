9a66df9b3258eb39ec29456332a0bcae9635b906
dariaterekhovaae <98411986+dariaterekhovaae@users.noreply.github.com>
2023-08-29 13:42:14 +0300

    feat(3d-tiles): 3tz loader (#2578)

---
+++ modules/3d-tiles/src/tz3/tz3-archive.ts
@@ -0,0 +1,82 @@
+import md5 from 'md5';
+import {FileProvider, parseZipLocalFileHeader, HashElement, findBin} from '@loaders.gl/zip';
+import {DeflateCompression} from '@loaders.gl/compression';
+
+type CompressionHandler = (compressedFile: ArrayBuffer) => Promise<ArrayBuffer>;
+
+/**
+ * Handling different compression types in zip
+ */
+const COMPRESSION_METHODS: {[key: number]: CompressionHandler} = {
+  /** No compression */
+  0: async (compressedFile) => compressedFile,
+
+  /** Deflation */
+  8: async (compressedFile) => {
+    const compression = new DeflateCompression({raw: true});
+    const decompressedData = await compression.decompress(compressedFile);
+    return decompressedData;
+  }
+};
+
+/**
+ * Class for handling information about 3tz file
+ */
+export class TZ3Archive {
+  private tz3Archive: FileProvider;
+  private hashArray: HashElement[];
+  constructor(tz3Archive: FileProvider, hashFile: HashElement[]) {
+    this.tz3Archive = tz3Archive;
+    this.hashArray = hashFile;
+  }
+
+  /**
+   * Returns file with the given path from 3tz archive
+   * @param path - path inside the 3tz
+   * @returns buffer with ready to use file
+   */
+  async getFile(path: string): Promise<Buffer> {
+    // sometimes paths are not in lower case when hash file is created,
+    // so first we're looking for lower case file name and then for original one
+    let data = await this.getFileBytes(path.toLocaleLowerCase());
+    if (!data) {
+      data = await this.getFileBytes(path);
+    }
+    if (!data) {
+      throw new Error('No such file in the archieve');
+    }
+    const decompressedFile = Buffer.from(data);
+
+    return decompressedFile;
+  }
+
+  /**
+   * Trying to get raw file data by adress
+   * @param path - path inside the archive
+   * @returns buffer with the raw file data
+   */
+  private async getFileBytes(path: string): Promise<ArrayBuffer | null> {
+    const nameHash = Buffer.from(md5(path), 'hex');
+    const fileInfo = findBin(nameHash, this.hashArray); // implement binary search
+    if (!fileInfo) {
+      return null;
+    }
+
+    const localFileHeader = await parseZipLocalFileHeader(fileInfo.offset, this.tz3Archive);
+    if (!localFileHeader) {
+      return null;
+    }
+
+    const compressedFile = await this.tz3Archive.slice(
+      localFileHeader.fileDataOffset,
+      localFileHeader.fileDataOffset + localFileHeader.compressedSize
+    );
+
+    const compressionMethod = COMPRESSION_METHODS[localFileHeader.compressionMethod];
+    if (!compressionMethod) {
+      throw Error('Only Deflation compression is supported');
+    }
+
+    return compressionMethod(compressedFile);
+  }
+}
---
+++ modules/3d-tiles/src/tz3/tz3-parser.ts
@@ -0,0 +1,52 @@
+import {
+  FileProvider,
+  HashElement,
+  cdSignature as cdHeaderSignature,
+  generateHashInfo,
+  parseHashFile,
+  parseZipCDFileHeader,
+  parseZipLocalFileHeader,
+  searchFromTheEnd
+} from '@loaders.gl/zip';
+import {TZ3Archive} from './tz3-archive';
+
+/**
+ * Creates 3tz file handler from raw file
+ * @param fileProvider raw file data
+ * @param cb is called with information message during parsing
+ * @returns 3tz file handler
+ */
+export const parse3tz = async (
+  fileProvider: FileProvider,
+  cb?: (msg: string) => void
+): Promise<TZ3Archive> => {
+  const hashCDOffset = await searchFromTheEnd(fileProvider, cdHeaderSignature);
+
+  const cdFileHeader = await parseZipCDFileHeader(hashCDOffset, fileProvider);
+
+  let hashData: HashElement[];
+  if (cdFileHeader?.fileName !== '@3dtilesIndex1@') {
+    cb?.('3tz doesnt contain hash file');
+    hashData = await generateHashInfo(fileProvider);
+    cb?.('hash info has been composed according to central directory records');
+  } else {
+    cb?.('3tz contains hash file');
+    const localFileHeader = await parseZipLocalFileHeader(
+      cdFileHeader.localHeaderOffset,
+      fileProvider
+    );
+    if (!localFileHeader) {
+      throw new Error('corrupted 3tz');
+    }
+
+    const fileDataOffset = localFileHeader.fileDataOffset;
+    const hashFile = await fileProvider.slice(
+      fileDataOffset,
+      fileDataOffset + localFileHeader.compressedSize
+    );
+
+    hashData = parseHashFile(hashFile);
+  }
+
+  return new TZ3Archive(fileProvider, hashData);
+};
