cc9cbd3b2d44ed49c0fa173692bd197641a2ab79
dariaterekhovaae <98411986+dariaterekhovaae@users.noreply.github.com>
2023-09-01 15:21:20 +0300

    feat(3d-tiles): some improvements (#2610)

--- modules/3d-tiles/test/parse-tz3.spec.js
+++ modules/3d-tiles/test/parse-tz3.spec.js
@@ -1,17 +1,13 @@
 import test from 'tape-promise/tape';
 import {load} from '@loaders.gl/core';
-import {TZ3Loader} from '../src';
+import {Tiles3DArchiveFileLoader} from '../src';
 
-const testUrl = '@loaders.gl/3d-tiles/test/data/bridges.3tz';
+const testUrl = '@loaders.gl/3d-tiles/test/data/test.3tz';
 
 test('tz3Loader#load uncompressed file', async (t) => {
-  const uncompressedFile = await load(testUrl, TZ3Loader, {tz3: {path: 'tileset.json'}});
-  t.deepEqual(uncompressedFile.byteLength, 2420, 'tileset.json has the correct length');
-  t.end();
-});
-
-test('tz3Loader#load deflated file', async (t) => {
-  const uncompressedFile = await load(testUrl, TZ3Loader, {tz3: {path: '13/5081/4739.glb'}});
-  t.deepEqual(uncompressedFile.byteLength, 36032, 'tileset.json has the correct length');
+  const uncompressedFile = await load(testUrl, Tiles3DArchiveFileLoader, {
+    '3d-tiles-archive': {path: 'tileset.json'}
+  });
+  t.deepEqual(uncompressedFile.byteLength, 2339, 'tileset.json has the correct length');
   t.end();
 });
