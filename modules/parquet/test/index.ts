42771d2e8dea275e9bb89298dc6d875f96efda81
Ib Green <ib@unfolded.ai>
2023-09-13 07:41:50 -0400

    fixes

--- modules/parquet/test/index.ts
+++ modules/parquet/test/index.ts
@@ -3,23 +3,23 @@ import './init';
 import './buffer-polyfill';
 
 // parquetjs unit test suite
-// import './parquetjs/codec-plain.spec';
-// import './parquetjs/codec-rle.spec';
-// import './parquetjs/schema.spec';
-// import './parquetjs/shred.spec';
-// import './parquetjs/thrift.spec';
-// import './parquetjs/reader.spec';
+import './parquetjs/codec-plain.spec';
+import './parquetjs/codec-rle.spec';
+import './parquetjs/schema.spec';
+import './parquetjs/shred.spec';
+import './parquetjs/thrift.spec';
+import './parquetjs/reader.spec';
 
 // // The integration spec runs tens of thousands of detailed tests. Too slow for CI, uncomment to run.
-// // import './parquetjs/integration.spec';
+// import './parquetjs/integration.spec';
 
 // // loader/writer
-// import './parquet-loader.spec';
-// import './parquet-writer.spec';
+import './parquet-loader.spec';
+import './parquet-writer.spec';
 
 
 // import './parquet-wasm-loader.spec';
 
-// import './parquet-columnar-loader.spec';
+import './parquet-columnar-loader.spec';
 
-// import './geoparquet-loader.spec';
+import './geoparquet-loader.spec';
