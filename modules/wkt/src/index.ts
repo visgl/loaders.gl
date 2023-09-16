382f2ca9c8fcde1244c21a1cb85eecf973f8e960
Ib Green <ib@foursquare.com>
2023-09-16 10:24:49 -0400

    feat(wkb): New HexWKBLoader for hex encoded WKB (#2652)

--- modules/wkt/src/index.ts
+++ modules/wkt/src/index.ts
@@ -3,8 +3,13 @@
 export {WKBLoader, WKBWorkerLoader} from './wkb-loader';
 export {WKBWriter} from './wkb-writer';
 
+export {HexWKBLoader} from './hex-wkb-loader';
+
 export {WKTLoader, WKTWorkerLoader} from './wkt-loader';
 export {WKTWriter} from './wkt-writer';
 
 export {WKTCRSLoader} from './wkt-crs-loader';
 export {WKTCRSWriter} from './wkt-crs-writer';
+
+// EXPERIMENTAL EXPORTS
+export {encodeHex, decodeHex} from './lib/utils/hex-transcoder';
