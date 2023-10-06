24f60bd328c851d1701e4e4a00ab2b7c6afc27c5
Ib Green <ib@unfolded.ai>
2023-10-06 11:58:19 +0200

    Remove Buffer in zip module

--- modules/zip/src/parse-zip/end-of-central-directory.ts
+++ modules/zip/src/parse-zip/end-of-central-directory.ts
@@ -1,4 +1,4 @@
-import {FileProvider} from '@loaders.gl/loader-utils';
+import {FileProvider, compareArrayBuffers} from '@loaders.gl/loader-utils';
 import {ZipSignature, searchFromTheEnd} from './search-from-the-end';
 
 /**
@@ -12,9 +12,9 @@ export type ZipEoCDRecord = {
   cdRecordsNumber: bigint;
 };
 
-const eoCDSignature: ZipSignature = [0x50, 0x4b, 0x05, 0x06];
-const zip64EoCDLocatorSignature = [0x50, 0x4b, 0x06, 0x07];
-const zip64EoCDSignature = [0x50, 0x4b, 0x06, 0x06];
+const eoCDSignature: ZipSignature = new Uint8Array([0x50, 0x4b, 0x05, 0x06]);
+const zip64EoCDLocatorSignature = new Uint8Array([0x50, 0x4b, 0x06, 0x07]);
+const zip64EoCDSignature = new Uint8Array([0x50, 0x4b, 0x06, 0x06]);
 
 // offsets accroding to https://en.wikipedia.org/wiki/ZIP_(file_format)
 const CD_RECORDS_NUMBER_OFFSET = 8n;
@@ -25,43 +25,33 @@ const ZIP64_CD_START_OFFSET_OFFSET = 48n;
 
 /**
  * Parses end of central directory record of zip file
- * @param fileProvider - FileProvider instance
+ * @param file - FileProvider instance
  * @returns Info from the header
  */
-export const parseEoCDRecord = async (fileProvider: FileProvider): Promise<ZipEoCDRecord> => {
-  const zipEoCDOffset = await searchFromTheEnd(fileProvider, eoCDSignature);
+export const parseEoCDRecord = async (file: FileProvider): Promise<ZipEoCDRecord> => {
+  const zipEoCDOffset = await searchFromTheEnd(file, eoCDSignature);
 
   let cdRecordsNumber = BigInt(
-    await fileProvider.getUint16(zipEoCDOffset + CD_RECORDS_NUMBER_OFFSET)
+    await file.getUint16(zipEoCDOffset + CD_RECORDS_NUMBER_OFFSET)
   );
-  let cdStartOffset = BigInt(await fileProvider.getUint32(zipEoCDOffset + CD_START_OFFSET_OFFSET));
+  let cdStartOffset = BigInt(await file.getUint32(zipEoCDOffset + CD_START_OFFSET_OFFSET));
 
   if (cdStartOffset === BigInt(0xffffffff) || cdRecordsNumber === BigInt(0xffffffff)) {
     const zip64EoCDLocatorOffset = zipEoCDOffset - 20n;
 
-    if (
-      Buffer.from(
-        await fileProvider.slice(zip64EoCDLocatorOffset, zip64EoCDLocatorOffset + 4n)
-      ).compare(Buffer.from(zip64EoCDLocatorSignature)) !== 0
-    ) {
+    const magicBytes = await file.slice(zip64EoCDLocatorOffset, zip64EoCDLocatorOffset + 4n);
+    if (!compareArrayBuffers(magicBytes, zip64EoCDLocatorSignature)) {
       throw new Error('zip64 EoCD locator not found');
     }
-    const zip64EoCDOffset = await fileProvider.getBigUint64(
-      zip64EoCDLocatorOffset + ZIP64_EOCD_START_OFFSET_OFFSET
-    );
+    const zip64EoCDOffset = await file.getBigUint64(zip64EoCDLocatorOffset + ZIP64_EOCD_START_OFFSET_OFFSET);
 
-    if (
-      Buffer.from(await fileProvider.slice(zip64EoCDOffset, zip64EoCDOffset + 4n)).compare(
-        Buffer.from(zip64EoCDSignature)
-      ) !== 0
-    ) {
+    const endOfCDMagicBytes = await file.slice(zip64EoCDOffset, zip64EoCDOffset + 4n);
+    if (!compareArrayBuffers(endOfCDMagicBytes, zip64EoCDSignature.buffer)) {
       throw new Error('zip64 EoCD not found');
     }
 
-    cdRecordsNumber = await fileProvider.getBigUint64(
-      zip64EoCDOffset + ZIP64_CD_RECORDS_NUMBER_OFFSET
-    );
-    cdStartOffset = await fileProvider.getBigUint64(zip64EoCDOffset + ZIP64_CD_START_OFFSET_OFFSET);
+    cdRecordsNumber = await file.getBigUint64(zip64EoCDOffset + ZIP64_CD_RECORDS_NUMBER_OFFSET);
+    cdStartOffset = await file.getBigUint64(zip64EoCDOffset + ZIP64_CD_START_OFFSET_OFFSET);
   }
 
   return {
