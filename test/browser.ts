7554711ca992cce6b9e50be3c5aa056eb3cf9995
Xiaoji Chen <cxiaoji@gmail.com>
2023-10-09 22:23:33 -0700

    Fix browser test

--- test/browser.ts
+++ test/browser.ts
@@ -18,20 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-/* eslint-disable @typescript-eslint/no-var-requires */
-import test from 'tape';
-import {version} from '../lerna.json';
-
-// @ts-ignore TS2339: Property does not exist on type 'Window & typeof globalThis'.
-test.onFinish(window.browserTestDriver_finish);
-// @ts-ignore TS2339: Property does not exist on type 'Window & typeof globalThis'.
-test.onFailure(window.browserTestDriver_fail);
-
-// This constant will be inlined by babel plugin.
-// To test source without transpilation, set a fallback here.
-// @ts-ignore TS2339: Property does not exist on type 'Global'
-window.__VERSION__ = version;
-
+import './init-browser-test';
 import './modules';
 // Render tests too flaky - disable for now
 // import './render';
