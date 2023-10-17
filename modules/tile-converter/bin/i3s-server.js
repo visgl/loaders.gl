522eca192a5f4a019ab906cdca0e3cd70f8eb6f8
Viktor Belomestnov <belom88@yandex.ru>
2023-10-17 22:23:21 +0200

    fix(tile-converter): cli tools (#2707)

--- modules/tile-converter/bin/i3s-server.js
+++ modules/tile-converter/bin/i3s-server.js
@@ -1,4 +1,2 @@
 #!/usr/bin/env node
-// @ts-nocheck
-/* eslint-disable import/no-extraneous-dependencies */
-require('@loaders.gl/tile-converter/dist/esm/i3s-server/bin/i3s-server.min');
+import '../dist/i3s-server/bin/i3s-server.min.cjs';
