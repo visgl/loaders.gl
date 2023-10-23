e63a68f16eee2d80e7f2ba89909739d2a09cde5a
Ib Green <ib@unfolded.ai>
2023-10-23 15:51:21 -0400

    feat(lerc): Break out LERCLoader into its own module (size and bundling issues)

---
+++ modules/lerc/src/index.ts
@@ -0,0 +1,5 @@
+// loaders.gl, MIT license
+
+// LERC - Limited Error Raster Compression
+export type {LERCData} from './lib/parsers/lerc/lerc-types';
+export {LERCLoader} from './lerc-loader';
---
+++ modules/lerc/src/workers/lerc-worker.ts
@@ -0,0 +1,6 @@
+// loaders.gl, MIT license
+
+import {createLoaderWorker} from '@loaders.gl/loader-utils';
+import {LERCLoader} from '../lerc-loader';
+
+createLoaderWorker(LERCLoader);
