7d9545c924ef452fe08c86da5e9e0c074f210a0b
Viktor Belomestnov <belom88@yandex.ru>
2023-10-27 13:38:42 +0200

    fix for bundle

--- modules/tile-converter/src/i3s-server/app.ts
+++ modules/tile-converter/src/i3s-server/app.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import path from 'path';
 import logger from 'morgan';
 import cors from 'cors';
-import {fileURLToPath} from 'url';
+// For local debug
+// import {fileURLToPath} from 'url';
 import {loadArchive} from './controllers/slpk-controller';
 import {router as indexRouter} from './routes';
 import {sceneServerRouter, router} from './routes/slpk-router';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// For local debug
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const I3S_LAYER_PATH = process.env.I3sLayerPath || ''; // eslint-disable-line no-process-env, no-undef
 const FULL_LAYER_PATH = path.join(process.cwd(), I3S_LAYER_PATH); // eslint-disable-line no-undef
