203e871856cc60d55b6c32e06d3854915667583a
Giovanni Cimolin <giovannicimolin@gmail.com>
2023-12-12 13:24:06 +0100

    feat: WIP version of RGB geotiff loader

--- modules/geotiff/src/index.ts
+++ modules/geotiff/src/index.ts
@@ -2,5 +2,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
+export {RGBGeoTiffLoader} from './loaders';
 export {loadGeoTiff} from './lib/load-geotiff';
 export {default as TiffPixelSource} from './lib/tiff-pixel-source';
---
+++ modules/geotiff/src/loaders.ts
@@ -0,0 +1,68 @@
+import { fromArrayBuffer } from "geotiff";
+import type {LoaderWithParser, LoaderOptions} from '@loaders.gl/loader-utils';
+
+// __VERSION__ is injected by babel-plugin-version-inline
+// @ts-ignore TS2304: Cannot find name '__VERSION__'.
+const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
+
+// Type definitions
+type GeoTiffData =  {
+    bounds: number[],
+    width: number,
+    height: number,
+    imageData: ImageData,
+}
+
+interface RGBGeoTiffLoaderOptions extends LoaderOptions {
+    enableAlpha: boolean;
+}
+
+/**
+ * Loads a GeoTIFF file containing a RGB image.
+ */
+const loadRgbGeoTiff = async (
+    data: ArrayBuffer, 
+    options?: RGBGeoTiffLoaderOptions
+): Promise<GeoTiffData> => {
+    // Load using Geotiff.js
+    const tiff = await fromArrayBuffer(data);
+
+    // Assumes we only have one image inside TIFF
+    const image = await tiff.getImage();
+
+    // Read image and size 
+    // TODO: Add support for worker pools here.
+    // TODO: Add support for more image formats.
+    const rgbData = await image.readRGB({ 
+        enableAlpha: options?.enableAlpha, 
+    });
+    const width = image.getWidth();
+    const height = image.getHeight();
+
+    // Create a new ImageData object
+    const imageData = new ImageData(width, height);
+    imageData.data.set(new Uint8ClampedArray(rgbData as unknown as Uint8Array));
+
+    // Return GeoJSON data
+    return {
+        // TODO: Add bounds here
+        bounds: [],
+        imageData,
+        width,
+        height,
+    };
+};
+
+// Export loader
+export const RGBGeoTiffLoader: LoaderWithParser<GeoTiffData, never, RGBGeoTiffLoaderOptions> = {
+    id: "geotiff",
+    name: "GeoTIFF",
+    module: "geotiff",
+    version: VERSION,
+    options: {
+        enableAlpha: true,
+    },
+    mimeTypes: ["image/tiff", "image/geotiff"],
+    extensions: ["geotiff", "tiff", "geotif", "tif"],
+    parse: loadRgbGeoTiff,
+};
