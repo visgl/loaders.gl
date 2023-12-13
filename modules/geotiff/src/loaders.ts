d9744ecabdd29c1e12de298c6a468d5f7cc55a0a
Ib Green <ib@foursquare.com>
2023-12-13 07:31:44 -0500

    docs

--- modules/geotiff/src/loaders.ts
+++ modules/geotiff/src/loaders.ts
@@ -1,4 +1,4 @@
-import { fromArrayBuffer } from "geotiff";
+import {fromArrayBuffer} from 'geotiff';
 import type {LoaderWithParser, LoaderOptions} from '@loaders.gl/loader-utils';
 
 // __VERSION__ is injected by babel-plugin-version-inline
@@ -6,63 +6,63 @@ import type {LoaderWithParser, LoaderOptions} from '@loaders.gl/loader-utils';
 const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
 
 // Type definitions
-type GeoTiffData =  {
-    bounds: number[],
-    width: number,
-    height: number,
-    imageData: ImageData,
-}
+type GeoTiffData = {
+  bounds: number[];
+  width: number;
+  height: number;
+  imageData: ImageData;
+};
 
 interface GeoTiffLoaderOptions extends LoaderOptions {
-    enableAlpha: boolean;
+  enableAlpha: boolean;
 }
 
 /**
  * Loads a GeoTIFF file containing a RGB image.
  */
 const loadGeoTiff = async (
-    data: ArrayBuffer, 
-    options?: GeoTiffLoaderOptions
+  data: ArrayBuffer,
+  options?: GeoTiffLoaderOptions
 ): Promise<GeoTiffData> => {
-    // Load using Geotiff.js
-    const tiff = await fromArrayBuffer(data);
+  // Load using Geotiff.js
+  const tiff = await fromArrayBuffer(data);
 
-    // Assumes we only have one image inside TIFF
-    const image = await tiff.getImage();
+  // Assumes we only have one image inside TIFF
+  const image = await tiff.getImage();
 
-    // Read image and size 
-    // TODO: Add support for worker pools here.
-    // TODO: Add support for more image formats.
-    const rgbData = await image.readRGB({ 
-        enableAlpha: options?.enableAlpha, 
-    });
-    const width = image.getWidth();
-    const height = image.getHeight();
+  // Read image and size
+  // TODO: Add support for worker pools here.
+  // TODO: Add support for more image formats.
+  const rgbData = await image.readRGB({
+    enableAlpha: options?.enableAlpha
+  });
+  const width = image.getWidth();
+  const height = image.getHeight();
 
-    // Create a new ImageData object
-    const imageData = new ImageData(width, height);
-    imageData.data.set(new Uint8ClampedArray(rgbData as unknown as Uint8Array));
+  // Create a new ImageData object
+  const imageData = new ImageData(width, height);
+  imageData.data.set(new Uint8ClampedArray(rgbData as unknown as Uint8Array));
 
-    // Return GeoJSON data
-    return {
-        // TODO: Add bounds here
-        bounds: [],
-        imageData,
-        width,
-        height,
-    };
+  // Return GeoJSON data
+  return {
+    // TODO: Add bounds here
+    bounds: [],
+    imageData,
+    width,
+    height
+  };
 };
 
 // Export loader
 export const GeoTiffLoader: LoaderWithParser<GeoTiffData, never, GeoTiffLoaderOptions> = {
-    id: "geotiff",
-    name: "GeoTIFF",
-    module: "geotiff",
-    version: VERSION,
-    options: {
-        enableAlpha: true,
-    },
-    mimeTypes: ["image/tiff", "image/geotiff"],
-    extensions: ["geotiff", "tiff", "geotif", "tif"],
-    parse: loadGeoTiff,
+  id: 'geotiff',
+  name: 'GeoTIFF',
+  module: 'geotiff',
+  version: VERSION,
+  options: {
+    enableAlpha: true
+  },
+  mimeTypes: ['image/tiff', 'image/geotiff'],
+  extensions: ['geotiff', 'tiff', 'geotif', 'tif'],
+  parse: loadGeoTiff
 };
