19e62e518367f1d5825228c110eb0e4b68f021f2
mspivak-actionengine <michael.spivak@actionengine.com>
2024-01-31 17:03:28 +0300

    fix dev review issues

--- modules/tile-converter/src/3d-tiles-converter/3d-tiles-converter.ts
+++ modules/tile-converter/src/3d-tiles-converter/3d-tiles-converter.ts
@@ -115,15 +115,13 @@ export default class Tiles3DConverter {
         box: i3sObbTo3dTilesObb(rootNode.obb, this.geoidHeightModel)
       },
       geometricError: convertScreenThresholdToGeometricError(rootNode),
-      children: []
+      children: [],
+      refine: 'REPLACE'
     };
 
     await this._addChildren(rootNode, rootTile, 1);
 
     const tileset = transform({root: rootTile}, tilesetTemplate());
-    if (!tileset.root.refine) {
-      tileset.root.refine = 'REPLACE';
-    }
     await writeFile(this.tilesetPath, JSON.stringify(tileset), 'tileset.json');
 
     this._finishConversion({slpk: false, outputPath, tilesetName});
