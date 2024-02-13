9b5813ae1811d6968b806a443cbb53de9f6f2dc9
Ib Green <ib@foursquare.com>
2024-02-13 09:30:55 -0500

    fix(flatgeobuf): Add missing index files (#2884)

---
+++ modules/flatgeobuf/src/flatgeobuf/3.27.2/generic/index.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: BSD-2-Clause AND ISC
+// Copyright (c) 2018, Björn Harrtell
+
+export * from './feature';
+export * from './featurecollection';
+export * from './geometry';
+export * from './header';
---
+++ modules/flatgeobuf/src/flatgeobuf/3.27.2/geojson/index.ts
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSD-2-Clause AND ISC
+// Copyright (c) 2018, Björn Harrtell
+
+export * from './feature';
+export * from './featurecollection';
+export * from './geometry';
