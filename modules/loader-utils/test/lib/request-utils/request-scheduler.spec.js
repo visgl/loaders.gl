65005eff1e29992795cd12359dc4d856fe216026
Don McCurdy <donmccurdy@cartodb.com>
2024-02-29 11:21:37 -0500

    Add unit tests for RequestScheduler debounce

--- modules/loader-utils/test/lib/request-utils/request-scheduler.spec.js
+++ modules/loader-utils/test/lib/request-utils/request-scheduler.spec.js
@@ -1,6 +1,8 @@
 import {RequestScheduler} from '@loaders.gl/loader-utils';
 import test from 'tape-promise/tape';
 
+const sleep = (t: number) => new Promise((resolve) => setTimeout(resolve, t));
+
 test('RequestScheduler#constructor', (t) => {
   const requestScheduler = new RequestScheduler();
   t.ok(requestScheduler);
@@ -12,19 +14,21 @@ test('RequestScheduler#scheduleRequest', async (t) => {
   t.ok(requestScheduler);
 
   let requestToken = await requestScheduler.scheduleRequest({id: 1});
-  t.ok(requestToken, 'should issue request');
-  if (requestToken) {
-    t.is(requestScheduler.activeRequestCount, 1, 'active request count');
-    requestToken.done();
-    t.is(requestScheduler.activeRequestCount, 0, 'active request count');
-
-    requestToken.done();
-    t.is(requestScheduler.activeRequestCount, 0, 'active request count');
-
-    requestToken = await requestScheduler.scheduleRequest({id: 2}, () => -1);
-    t.notOk(requestToken, 'should not issue request with negative priority');
-    t.is(requestScheduler.activeRequestCount, 0, 'active request count');
+  if (!requestToken) {
+    t.fail('should issue request');
+    return;
   }
+
+  t.is(requestScheduler.activeRequestCount, 1, 'active request count');
+  requestToken.done();
+  t.is(requestScheduler.activeRequestCount, 0, 'active request count');
+
+  requestToken.done();
+  t.is(requestScheduler.activeRequestCount, 0, 'active request count');
+
+  requestToken = await requestScheduler.scheduleRequest({id: 2}, () => -1);
+  t.notOk(requestToken, 'should not issue request with negative priority');
+  t.is(requestScheduler.activeRequestCount, 0, 'active request count');
 
   // The following test checks that request#4 is only issued AFTER request#3 is resolved
   // By modifying request#4's priority during request#3
@@ -35,11 +39,13 @@ test('RequestScheduler#scheduleRequest', async (t) => {
 
   const result = await Promise.all([
     requestScheduler.scheduleRequest({id: 3}).then(async (reqToken) => {
+      if (!reqToken) {
+        t.fail('should issue request');
+        return;
+      }
       t.is(requestScheduler.activeRequestCount, 1, 'active request count');
       await request3();
-      if (reqToken) {
-        reqToken.done();
-      }
+      reqToken.done();
       t.is(requestScheduler.activeRequestCount, 0, 'active request count');
     }),
     // priority callback should be called after the previous one is fully resolved
@@ -48,6 +54,51 @@ test('RequestScheduler#scheduleRequest', async (t) => {
 
   t.notOk(result[1]);
   t.is(requestScheduler.activeRequestCount, 0, 'active request count');
+
+  t.end();
+});
+
+test('RequestScheduler#debounce', async (t) => {
+  const scheduler = new RequestScheduler({debounceMs: 0, maxRequests: 1});
+  const schedulerDebounced = new RequestScheduler({debounceMs: 10, maxRequests: 1});
+
+  const request1 = scheduler.scheduleRequest({id: 1});
+  const request2 = schedulerDebounced.scheduleRequest({id: 2});
+  const request3 = schedulerDebounced.scheduleRequest({id: 3});
+
+  await sleep(0);
+
+  t.is(scheduler.activeRequestCount, 1, 'issues default request');
+  t.is(schedulerDebounced.activeRequestCount, 0, 'delays debounced requests');
+
+  await sleep(50);
+
+  t.is(schedulerDebounced.activeRequestCount, 1, 'issues first debounced request after delay');
+
+  const token1 = await request1;
+  const token2 = await request2;
+
+  if (token1 && token2) {
+    token1.done();
+    token2.done();
+  } else {
+    t.fail('should issue both initial requests');
+  }
+
+  t.is(scheduler.activeRequestCount, 0, 'no active requests on scheduler #1');
+  t.is(schedulerDebounced.activeRequestCount, 0, 'no active requests on scheduler #2');
+
+  await sleep(50);
+
+  t.is(schedulerDebounced.activeRequestCount, 1, 'issues final debounced request after delay');
+
+  const token3 = await request3;
+
+  if (token3) {
+    token3.done();
+  } else {
+    t.fail('should issue final request');
+  }
 
   t.end();
 });
