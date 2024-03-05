fadfeb58d49f9f7f88abb4fb097fd211718fab07
Ib Green <ib@foursquare.com>
2024-03-05 09:24:31 -0500

    wip

--- modules/lerc/src/index.ts
+++ modules/lerc/src/index.ts
@@ -3,5 +3,6 @@
 // Copyright (c) vis.gl contributors
 
 // LERC - Limited Error Raster Compression
+export const LERC_ERROR = 'lerc module is not esbuild compatible';
 // export type {LERCData} from './lib/parsers/lerc/lerc-types';
 // export {LERCLoader} from './lerc-loader';
