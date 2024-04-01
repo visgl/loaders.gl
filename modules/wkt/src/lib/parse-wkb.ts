91c6402da3c42ad45563ae8ab95171bf14cd8c3b
Viktor Belomestnov <belom88@yandex.ru>
2024-04-01 17:41:45 +0200

    fix(wkt): multipolygons UInt16 overflow (#2935)

--- modules/wkt/src/lib/parse-wkb.ts
+++ modules/wkt/src/lib/parse-wkb.ts
@@ -307,11 +307,13 @@ function concatenateBinaryPolygonGeometries(
   // Combine primitivePolygonIndices from each individual polygon
   const primitivePolygonIndices = [0];
   for (const primitivePolygon of primitivePolygons) {
-    primitivePolygonIndices.push(
-      ...primitivePolygon
-        .filter((x: number) => x > 0)
-        .map((x: number) => x + primitivePolygonIndices[primitivePolygonIndices.length - 1])
-    );
+    for (const value of primitivePolygon) {
+      if (value > 0) {
+        primitivePolygonIndices.push(
+          value + primitivePolygonIndices[primitivePolygonIndices.length - 1]
+        );
+      }
+    }
   }
 
   return {
