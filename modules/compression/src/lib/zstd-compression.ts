f00f2cce5a0827fa56ed37984377fbc7c670b105
Viktor Belomestnov <belom88@yandex.ru>
2024-04-02 13:37:50 +0200

    fix(parquet): zstd decompression

--- modules/compression/src/lib/zstd-compression.ts
+++ modules/compression/src/lib/zstd-compression.ts
@@ -3,6 +3,8 @@ import type {CompressionOptions} from './compression';
 import {Compression} from './compression';
 // import {ZstdCodec} from 'zstd-codec'; // https://bundlephobia.com/package/zstd-codec
 
+const CHUNK_SIZE = 1000000; // Tested value
+
 let ZstdCodec;
 let zstd;
 
@@ -49,5 +51,19 @@ export class ZstdCompression extends Compression {
     // var jsonBytes = simpleZstd.decompressUsingDict(jsonZstData, ddict);
     const inputArray = new Uint8Array(input);
     return simpleZstd.decompress(inputArray).buffer;
+  }
+
+  async decompress(input: ArrayBuffer, size?: number): Promise<ArrayBuffer> {
+    await this.preload();
+    const simpleZstd = new zstd.Streaming();
+    const inputArray = new Uint8Array(input);
+
+    const chunks: ArrayBuffer[] = [];
+    for (let i = 0; i <= inputArray.length; i += CHUNK_SIZE) {
+      chunks.push(input.slice(i, i + CHUNK_SIZE));
+    }
+
+    const decompressResult = await simpleZstd.decompressChunks(chunks);
+    return decompressResult.buffer;
   }
 }
