223b138e070c12d6769bcd11f322276d7bfae567
Yury Fedorov <yura_f@mail.ru>
2024-05-07 17:11:00 +0300

    feat(tile-converter): Add absolute path support for i3s-server (#2970)
    Co-authored-by: Yury Fedorov <yury.fedorov@actionengine.com>

--- modules/tile-converter/src/i3s-server/controllers/index-controller.ts
+++ modules/tile-converter/src/i3s-server/controllers/index-controller.ts
@@ -3,18 +3,24 @@ import fs from 'fs';
 
 const {promises} = fs;
 
-const I3S_LAYER_PATH = process.env.I3sLayerPath || '';
-
 /**
  * Get local file name by input HTTP URL
  * @param url - I3S HTTP service url
+ * @param i3sLayerPath - I3S layer path
  * @returns - local file name
  */
-export async function getFileNameByUrl(url: string): Promise<string | null> {
+export async function getFileNameByUrl(url: string, i3sLayerPath = ''): Promise<string | null> {
+  i3sLayerPath = i3sLayerPath || process.env.I3sLayerPath || '';
   const extensions = ['json', 'bin', 'jpg', 'jpeg', 'png', 'bin.dds', 'ktx2'];
-  const FULL_LAYER_PATH = path.join(process.cwd(), I3S_LAYER_PATH);
+  let filePath = process.cwd();
+  // Checks if the first character is not a point to indicate absolute path
+  const absolutePath = /^[^.]/.exec(i3sLayerPath);
+  if (absolutePath) {
+    filePath = '';
+  }
+  const FULL_LAYER_PATH = path.join(filePath, i3sLayerPath, url);
   for (const ext of extensions) {
-    const fileName = `${FULL_LAYER_PATH}${url}/index.${ext}`;
+    const fileName = `${FULL_LAYER_PATH}/index.${ext}`;
     try {
       await promises.access(fileName);
       return fileName;
