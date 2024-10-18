4784e4b45d1486e7010f892daab0e81b392194fa
Ib Green <ib@foursquare.com>
2024-10-18 16:11:28 -0400

    feat(csv): papaparse cleanup (#3143)

--- modules/csv/test/papaparse/csv-test-cases.ts
+++ modules/csv/test/papaparse/csv-test-cases.ts
@@ -1,12 +1,12 @@
-// This is a fork of papaparse under MIT License
+// loaders.gl
+// SPDX-License-Identifier: MIT
+// Copyright (c) vis.gl contributors
+// Copyright (c) 2015 Matthew Holt
+
+// This is a fork of papaparse v5.0.0-beta.0 under MIT license
 // https://github.com/mholt/PapaParse
 
-/* @license
-Papa Parse
-v5.0.0-beta.0
-https://github.com/mholt/PapaParse
-License: MIT
-*/
+import type {CSVParserConfig} from '../../src/papaparse/papaparse';
 
 /* eslint-disable camelcase, quotes, max-len, prefer-template, wrap-regex */
 
@@ -18,8 +18,20 @@ const UNIT_SEP = String.fromCharCode(31);
 const FILES_ENABLED = false;
 const XHR_ENABLED = false;
 
+type Test = {
+  description: string;
+  notes?: string;
+  input: any;
+  config?: CSVParserConfig;
+  expected: {
+    data: any[];
+    errors: any[];
+  };
+  disabled?: boolean;
+};
+
 // Tests for the core parser using new Papa.Parser().parse() (CSV to JSON)
-export const CORE_PARSER_TESTS = [
+export const CORE_PARSER_TESTS: Test[] = [
   {
     description: 'One row',
     input: 'A,b,c',
@@ -995,7 +1007,7 @@ export const PARSE_TESTS = [
   {
     description: 'Dynamic typing by indices can be determined by function',
     input: '001,002,003',
-    config: {dynamicTyping: (field) => field % 2 === 0},
+    config: {dynamicTypingFunction: (field) => field % 2 === 0},
     expected: {
       data: [[1, '002', 3]],
       errors: []
@@ -1004,7 +1016,7 @@ export const PARSE_TESTS = [
   {
     description: 'Dynamic typing by headers can be determined by function',
     input: 'A_as_int,B,C_as_int\r\n001,002,003',
-    config: {header: true, dynamicTyping: (field) => /_as_int$/.test(field)},
+    config: {header: true, dynamicTypingFunction: (field) => /_as_int$/.test(field)},
     expected: {
       data: [{A_as_int: 1, B: '002', C_as_int: 3}],
       errors: []
