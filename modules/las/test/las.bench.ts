efd79107ec136ea899ed7f5cd99f77139f960f33
Oleksii Vykaliuk <alekzvik@gmail.com>
2024-12-03 12:52:03 +0100

    clean up the bench file

--- modules/las/test/las.bench.ts
+++ modules/las/test/las.bench.ts
@@ -6,19 +6,18 @@ import {LAZPerfLoader, LAZRsLoader} from '@loaders.gl/las';
 import {fetchFile, load} from '@loaders.gl/core';
 
 const LAZ_URL = '@loaders.gl/las/test/data/indoor.laz';
-// const GEO_PARQUET_URL = '@loaders.gl/parquet/test/data/geoparquet/airports.parquet';
 
 export async function lazBench(suite) {
-  suite.group('ParquetLoader');
+  suite.group('LasLoader');
 
   const response = await fetchFile(LAZ_URL);
   const arrayBuffer = await response.arrayBuffer();
 
-  suite.addAsync('load(LAZPerfLoader) - LAZ load', {multiplier: 40000, unit: 'rows'}, async () => {
+  suite.addAsync('load(LAZPerfLoader) - LAZ load', {}, async () => {
     await load(arrayBuffer, LAZPerfLoader, {worker: false});
   });
 
-  suite.addAsync('load(LAZRsLoader) - LAZ load', {multiplier: 40000, unit: 'rows'}, async () => {
+  suite.addAsync('load(LAZRsLoader) - LAZ load', {}, async () => {
     await load(arrayBuffer, LAZRsLoader, {worker: false});
   });
 }
