47c70ccbcf5cc6607c5d75c300df624a9cb6e729
Ib Green <7025232+ibgreen@users.noreply.github.com>
2025-12-19 16:19:28 -0500

    Jclaessens97 fix/pcd sizesum calc (#3270)
    Co-authored-by: Jeroen Claessens <jeroen.claessens@jcss.be>

--- modules/pcd/test/pcd-loader.spec.ts
+++ modules/pcd/test/pcd-loader.spec.ts
@@ -16,6 +16,49 @@ setLoaderOptions({
   _workerType: 'test'
 });
 
+function createBinaryArrayBufferWithCountedField(): ArrayBuffer {
+  const headerText = [
+    '# .PCD v0.7 - Point Cloud Data file format',
+    'VERSION .7',
+    'FIELDS padding x y z',
+    'SIZE 4 4 4 4',
+    'TYPE F F F F',
+    'COUNT 2 1 1 1',
+    'WIDTH 2',
+    'HEIGHT 1',
+    'VIEWPOINT 0 0 0 1 0 0 0',
+    'POINTS 2',
+    'DATA binary',
+    ''
+  ].join('\n');
+  const headerBytes = new TextEncoder().encode(headerText);
+  const pointCount = 2;
+  const rowByteSize = 20;
+  const binaryData = new ArrayBuffer(pointCount * rowByteSize);
+  const binaryDataView = new DataView(binaryData);
+  const littleEndian = true;
+  const positionValues = [
+    {padding: [10, 20], position: [1, 2, 3]},
+    {padding: [30, 40], position: [4, 5, 6]}
+  ];
+
+  for (let pointIndex = 0; pointIndex < positionValues.length; pointIndex++) {
+    const rowOffset = pointIndex * rowByteSize;
+    const pointValues = positionValues[pointIndex];
+    binaryDataView.setFloat32(rowOffset + 0, pointValues.padding[0], littleEndian);
+    binaryDataView.setFloat32(rowOffset + 4, pointValues.padding[1], littleEndian);
+    binaryDataView.setFloat32(rowOffset + 8, pointValues.position[0], littleEndian);
+    binaryDataView.setFloat32(rowOffset + 12, pointValues.position[1], littleEndian);
+    binaryDataView.setFloat32(rowOffset + 16, pointValues.position[2], littleEndian);
+  }
+
+  const pcdArrayBuffer = new ArrayBuffer(headerBytes.length + binaryData.byteLength);
+  const pcdBytes = new Uint8Array(pcdArrayBuffer);
+  pcdBytes.set(headerBytes, 0);
+  pcdBytes.set(new Uint8Array(binaryData), headerBytes.length);
+  return pcdArrayBuffer;
+}
+
 test('PCDLoader#loader conformance', (t) => {
   validateLoader(t, PCDLoader, 'PCDLoader');
   validateLoader(t, PCDWorkerLoader, 'PCDWorkerLoader');
@@ -68,6 +111,26 @@ test('PCDLoader#parse(binary)', async (t) => {
   t.notOk(data.attributes.COLOR_0, 'COLOR_0 attribute was not preset');
   t.notOk(data.attributes.NORMAL, 'NORMAL attribute was not preset');
   t.equal(data.attributes.POSITION.value.length, 179250, 'POSITION attribute was found');
+
+  t.end();
+});
+
+test('PCDLoader#parse(binary with counted fields)', async (t) => {
+  const binaryArrayBuffer = createBinaryArrayBufferWithCountedField();
+  const data = await parse(binaryArrayBuffer, PCDLoader, {
+    core: {worker: false}
+  });
+  validateMeshCategoryData(t, data);
+
+  t.equal(data.loaderData.rowSize, 20, 'row size accounts for count values');
+  t.equal(data.loaderData.offset.x, 8, 'offset for x accounts for count values');
+  t.equal(data.loaderData.offset.y, 12, 'offset for y accounts for count values');
+  t.equal(data.loaderData.offset.z, 16, 'offset for z accounts for count values');
+  t.deepEqual(
+    Array.from(data.attributes.POSITION.value),
+    [1, 2, 3, 4, 5, 6],
+    'positions read correctly'
+  );
 
   t.end();
 });
