68766a51e20a417fbf9c27ea40a96888612e3343
Ib Green <ib@unfolded.ai>
2026-04-16 07:57:27 -0400

    coverage

--- modules/sql/test/index.ts
+++ modules/sql/test/index.ts
@@ -2,4 +2,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 import './duckdb-sql-source.node.spec';
-import './snowflake-sql-source.node.spec';
+import './snowflake-sql-source.spec';
+import './sql-source.spec';
+import './sql-utils.spec';
