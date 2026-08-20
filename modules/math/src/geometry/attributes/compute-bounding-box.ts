4eb9bf3abfa7f42e14b94e3a4c37832985a845fd
Ib Green <7025232+ibgreen@users.noreply.github.com>
2026-08-20 19:05:18 -0400

    chore: add SPDX headers to math module (#3613)

--- modules/math/src/geometry/attributes/compute-bounding-box.ts
+++ modules/math/src/geometry/attributes/compute-bounding-box.ts
@@ -1,3 +1,7 @@
+// loaders.gl
+// SPDX-License-Identifier: MIT
+// Copyright (c) vis.gl contributors
+
 import {makeAttributeIterator} from '../iterators/attribute-iterator';
 import {assert} from '../utils/assert';
 
