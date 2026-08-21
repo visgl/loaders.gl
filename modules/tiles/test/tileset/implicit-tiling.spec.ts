76a1073e814d4215786a1180678a79f45411f556
Ib Green <ib@unfolded.ai>
2026-08-21 11:06:36 -0400

    test(tiles): use vitest implicit tiling harness

--- modules/tiles/test/tileset/implicit-tiling.spec.ts
+++ modules/tiles/test/tileset/implicit-tiling.spec.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright vis.gl contributors
 
-import test from 'tape-promise/tape';
+import test from 'test/utils/vitest-tape';
 import {
   createImplicitSubtreeReference,
   LOD_METRIC_TYPE,
