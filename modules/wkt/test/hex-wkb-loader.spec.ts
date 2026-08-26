6566be0331b5837a8216a498b904776d6a3c4967
Ib Green <ib@unfolded.ai>
2026-08-26 15:24:14 -0400

    test: expand WKT and Lance coverage

--- modules/wkt/test/hex-wkb-loader.spec.ts
+++ modules/wkt/test/hex-wkb-loader.spec.ts
@@ -1,21 +1,12 @@
-// loaders.gl
-// SPDX-License-Identifier: MIT
-// Copyright (c) vis.gl contributors
-
-/* eslint-disable no-continue */
-
-import test from 'test/utils/vitest-tape';
+import {expect, test} from 'vitest';
 import {fetchFile, parseSync} from '@loaders.gl/core';
 import {HexWKBLoader} from '@loaders.gl/wkt/bundled';
 import {parseTestCases} from '@loaders.gl/gis/test/data/wkt/parse-test-cases';
-
 const WKB_2D_TEST_CASES = '@loaders.gl/gis/test/data/wkt/wkb-testdata2d.json';
 const WKB_Z_TEST_CASES = '@loaders.gl/gis/test/data/wkt/wkb-testdataZ.json';
-
-test('HexWKBLoader#2D', async t => {
+test('HexWKBLoader#2D', async () => {
   const response = await fetchFile(WKB_2D_TEST_CASES);
   const TEST_CASES = parseTestCases(await response.json());
-
   // TODO parseWKB outputs TypedArrays; testCase contains regular arrays
   for (const [title, testCase] of Object.entries(TEST_CASES)) {
     // TODO - remove and fix empty handling
@@ -24,47 +15,34 @@ test('HexWKBLoader#2D', async t => {
     }
     // Little endian
     if (testCase.wkbHex && testCase.geoJSON) {
-      t.deepEqual(parseSync(testCase.wkbHex, HexWKBLoader), testCase.geoJSON, title);
+      expect(parseSync(testCase.wkbHex, HexWKBLoader), title).toEqual(testCase.geoJSON);
     }
-
     // Big endian
     if (testCase.wkbHexXdr && testCase.geoJSON) {
-      t.deepEqual(parseSync(testCase.wkbHexXdr, HexWKBLoader), testCase.geoJSON, title);
+      expect(parseSync(testCase.wkbHexXdr, HexWKBLoader), title).toEqual(testCase.geoJSON);
     }
   }
-
-  t.end();
 });
-
-test('HexWKBLoader#Z', async t => {
+test('HexWKBLoader#Z', async () => {
   const response = await fetchFile(WKB_Z_TEST_CASES);
   const TEST_CASES = parseTestCases(await response.json());
-
   // TODO parseWKB outputs TypedArrays; testCase contains regular arrays
   for (const [title, testCase] of Object.entries(TEST_CASES)) {
     // TODO - remove and fix empty handling
     if (title.startsWith('empty') || title.includes('One') || title.includes('eometryCollection')) {
       continue;
     }
-
     // Little endian
     if (testCase.wkbHex && testCase.geoJSON) {
-      t.deepEqual(
-        parseSync(testCase.wkbHex, HexWKBLoader),
-        testCase.geoJSON,
-        testCase.wkbHex.slice(0, 60)
+      expect(parseSync(testCase.wkbHex, HexWKBLoader), testCase.wkbHex.slice(0, 60)).toEqual(
+        testCase.geoJSON
       );
     }
-
     // Big endian
     if (testCase.wkbHexXdr && testCase.geoJSON) {
-      t.deepEqual(
-        parseSync(testCase.wkbHexXdr, HexWKBLoader),
-        testCase.geoJSON,
-        testCase.wkbHexXdr.slice(0, 60)
+      expect(parseSync(testCase.wkbHexXdr, HexWKBLoader), testCase.wkbHexXdr.slice(0, 60)).toEqual(
+        testCase.geoJSON
       );
     }
   }
-
-  t.end();
 });
