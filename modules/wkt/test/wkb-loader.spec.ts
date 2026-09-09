f682e886dc25ed1f5e021267c9ace3c326ddccac
Ib Green <7025232+ibgreen@users.noreply.github.com>
2026-09-09 15:13:35 -0400

    test(wkt): correct WKB worker fixture

--- modules/wkt/test/wkb-loader.spec.ts
+++ modules/wkt/test/wkb-loader.spec.ts
@@ -55,7 +55,11 @@ test('WKBLoader#Z', async () => {
 
 test('WKBLoader#worker', async () => {
   const result = await parse(
-    new Uint8Array([1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 240, 63, 0, 0, 0, 0, 0, 0, 64]).buffer,
+    new Uint8Array([
+      1, 1, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 240, 63,
+      0, 0, 0, 0, 0, 0, 0, 64
+    ]).buffer,
     WKBLoader,
     {core: {worker: true, _workerType: 'test'}}
   );
