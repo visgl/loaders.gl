3cbded861b1fcb35913f70ef3fc2429c7bc9a26c
Ib Green <7025232+ibgreen@users.noreply.github.com>
2026-09-16 08:22:38 -0400

    style(tiles): format renderer contract declarations

--- modules/tiles/src/tileset-3d/common/tile-3d.ts
+++ modules/tiles/src/tileset-3d/common/tile-3d.ts
@@ -9,7 +9,11 @@ import {Vector3, Matrix4} from '@math.gl/core';
 import {CullingVolume} from '@math.gl/culling';
 import type {CullingResult} from '@math.gl/culling';
 import {getTile3DFeatureIdSets} from './tile-3d-contracts';
-import type {Tile3DContent, Tile3DFeatureIdSet, Tile3DMetadataContext} from './tile-3d-contracts';
+import type {
+  Tile3DContent,
+  Tile3DFeatureIdSet,
+  Tile3DMetadataContext
+} from './tile-3d-contracts';
 
 // Note: circular dependency
 import type {Tileset3D} from './tileset-3d';
@@ -799,7 +803,8 @@ export class Tile3D {
       const normalizedContentHeader = contentHeader || {};
       return {
         index,
-        uri: this.contentUrls[index] || normalizedContentHeader.uri || normalizedContentHeader.url,
+        uri:
+          this.contentUrls[index] || normalizedContentHeader.uri || normalizedContentHeader.url,
         type: normalizedContentHeader.type,
         payload: null,
         metadata: normalizedContentHeader.metadata || null,
