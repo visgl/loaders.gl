64523715a38f77f5180d4a7690cca048a889dab4
Ib Green <7025232+ibgreen@users.noreply.github.com>
2026-09-25 10:17:49 -0400

    Use math.gl alpha.9 WKT metadata parser (#4039)

--- modules/wkt/test/wkt-loader.spec.ts
+++ modules/wkt/test/wkt-loader.spec.ts
@@ -1,7 +1,8 @@
 import {expect, test} from 'vitest';
 import {validateLoader} from 'test/common/conformance';
 import {WKTLoader, WKTWorkerLoader} from '@loaders.gl/wkt/bundled';
-import {setLoaderOptions, fetchFile, parseSync} from '@loaders.gl/core';
+import {setLoaderOptions, fetchFile, parseSync, encodeTextSync} from '@loaders.gl/core';
+import {WKTWriter} from '@loaders.gl/wkt';
 import fuzzer from 'fuzzer';
 const GEOMETRYCOLLECTION_WKT_URL = '@loaders.gl/gis/test/data/wkt/geometrycollection.wkt';
 const GEOMETRYCOLLECTION_GEOJSON_URL = '@loaders.gl/gis/test/data/wkt/geometrycollection.geojson';
@@ -118,6 +119,37 @@ test('WKTLoader', async () => {
       geometryCollectionWithMeasure.geometries[0],
       '__geoarrowDimension'
     )?.value
+  ).toBe('xym');
+  const inheritedMeasureCollection = parseSync('GEOMETRYCOLLECTION M (POINT (1 2 3))', WKTLoader);
+  expect(
+    Object.getOwnPropertyDescriptor(inheritedMeasureCollection.geometries[0], '__geoarrowDimension')
+      ?.value
+  ).toBe('xym');
+  expect(encodeTextSync(inheritedMeasureCollection, WKTWriter)).toBe(
+    'GEOMETRYCOLLECTION (POINT M (1 2 3))'
+  );
+  const mixedDimensionCollection = parseSync(
+    'GEOMETRYCOLLECTION ZM (POINT (1 2), POINT (3 4 5), POINT M (6 7 8))',
+    WKTLoader
+  );
+  expect(mixedDimensionCollection).toEqual({
+    type: 'GeometryCollection',
+    geometries: [
+      {type: 'Point', coordinates: [1, 2]},
+      {type: 'Point', coordinates: [3, 4, 5]},
+      {type: 'Point', coordinates: [6, 7, 8]}
+    ]
+  });
+  expect(
+    Object.getOwnPropertyDescriptor(mixedDimensionCollection, '__geoarrowDimension')?.value
+  ).toBe('xyzm');
+  expect(
+    Object.getOwnPropertyDescriptor(mixedDimensionCollection.geometries[1], '__geoarrowDimension')
+      ?.value
+  ).toBe('xyz');
+  expect(
+    Object.getOwnPropertyDescriptor(mixedDimensionCollection.geometries[2], '__geoarrowDimension')
+      ?.value
   ).toBe('xym');
   expect(parseSync('SRID=3857;LINESTRING (30 10, 10 30, 40 40)', WKTLoader)).toEqual({
     type: 'LineString',
